80d60c0835e59f582f4813a27874c04de4a1b1a6
Lorenzo Tilve <ltilve@igalia.com>
2009-05-28 14:05:37 +0200

    ItEr10S10CUCreacionDeOrganizacionsDeTraballo: Created basic structure for work organizations page
     Javier Moran Rua <jmoran@igalia.com>: Deleted IWorkOrganizationController.java, it is not necessary.

---
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/workorders/WorkOrganizationController.java
@@ -0,0 +1,15 @@
+package org.navalplanner.web.workorders;
+
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+
+
+/**
+ * Controller for {@link WorkOrganization} view of WorkOrder entitites <br />
+ * @author Lorenzo Tilve Álvaro <ltilve@igalia.com>
+ */
+public class WorkOrganizationController extends GenericForwardComposer {
+
+    public WorkOrganizationController() {
+    }
+
+}
