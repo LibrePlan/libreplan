3406d2b24d1b2dc5b25fa104124afb825d4f803b
Lorenzo Tilve <ltilve@igalia.com>
2009-07-09 22:50:33 +0200

    ItEr16S07RFDesenhoGraficoPortalItEr15S09: Separated menus to a macro component and managed from a separate controller

---
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/common/CustomMenuController.java
@@ -0,0 +1,120 @@
+package org.navalplanner.web.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zul.Div;
+
+/**
+ * Controller for customMenu <br />
+ * @author Lorenzo Tilve Álvaro <ltilve@igalia.com>
+ */
+public class CustomMenuController extends Div {
+
+	private List<CustomMenuItem> firstLevel;
+
+	public class CustomMenuItem {
+
+		private final String name;
+		private final String url;
+		private final List<CustomMenuItem> children;
+
+		public String getName() {
+			return name;
+		}
+
+		public String getUrl() {
+			return url;
+		}
+
+		public List<CustomMenuItem> getChildren() {
+			return children;
+		}
+
+		public CustomMenuItem(String name, String url) {
+			this.name = name;
+			this.url = url;
+			this.children = new ArrayList<CustomMenuItem>();
+		}
+
+		public CustomMenuItem(String name, String url,
+				List<CustomMenuItem> children) {
+			this.name = name;
+			this.url = url;
+			this.children = children;
+		}
+
+		public void appendChildren(CustomMenuItem newChildren) {
+			this.children.add(newChildren);
+		}
+
+	}
+
+	public CustomMenuController() {
+		initializeMenu();
+	}
+
+	public void initializeMenu() {
+		List<CustomMenuItem> l = new ArrayList<CustomMenuItem>();
+		CustomMenuItem ci;
+
+		ci = new CustomMenuItem("mainmenu.plannification",
+				"/navalplanner-webapp/");
+		ci.appendChildren(new CustomMenuItem("mainmenu.plannification",
+				"/navalplanner-webapp/planner/main.zul"));
+		ci.appendChildren(new CustomMenuItem("mainmenu.company_overview",
+				"/navalplanner-webapp/planner/main.zul"));
+		ci.appendChildren(new CustomMenuItem("mainmenu.plannifications_list",
+				"/navalplanner-webapp/planner/main.zul"));
+		l.add(ci);
+
+		ci = new CustomMenuItem("mainmenu.resources",
+				"/navalplanner-webapp/resources/worker/worker.zul");
+		ci.appendChildren(new CustomMenuItem("mainmenu.list_workers",
+				"/navalplanner-webapp/resources/worker/worker.zul#list"));
+		ci.appendChildren(new CustomMenuItem("mainmenu.manage_criterions",
+				"/navalplanner-webapp/resources/criterions/criterions.zul"));
+		l.add(ci);
+
+		ci = new CustomMenuItem("mainmenu.orders",
+				"/navalplanner-webapp/orders/orders.zul");
+		ci.appendChildren(new CustomMenuItem("mainmenu.list_orders",
+				"/orders/orders.zul"));
+		ci.appendChildren(new CustomMenuItem("mainmenu.activity_work_types",
+				"/orders/orders.zul"));
+		ci.appendChildren(new CustomMenuItem("mainmenu.models",
+				"/orders/orders.zul"));
+		l.add(ci);
+
+		ci = new CustomMenuItem("mainmenu.work_reports",
+				"/navalplanner-webapp/");
+		l.add(ci);
+
+		ci = new CustomMenuItem("mainmenu.quality_management",
+				"/navalplanner-webapp/");
+		l.add(ci);
+
+		ci = new CustomMenuItem("mainmenu.quality_management",
+				"/navalplanner-webapp/");
+		l.add(ci);
+
+		this.firstLevel = l;
+	}
+
+	public List<CustomMenuItem> getCustomMenuItems() {
+		return this.firstLevel;
+	}
+
+	public List<CustomMenuItem> getCustomMenuSecondaryItems() {
+		String requestPath = Executions.getCurrent().getDesktop()
+				.getRequestPath();
+		for (CustomMenuItem ci : this.firstLevel) {
+			if (requestPath.contains(ci.url) || ci.url.contains(requestPath)) {
+				return ci.getChildren();
+			}
+		}
+		return this.firstLevel.get(0).getChildren();
+	}
+
+}
