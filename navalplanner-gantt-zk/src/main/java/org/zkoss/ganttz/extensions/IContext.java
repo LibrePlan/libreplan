79e3ed65f7d4f83748851c37f2547a9353962adb
Óscar González Fernández <ogonzalez@igalia.com>
2009-07-28 12:28:48 +0200

    ItEr19S08CUCreacionProxectoPlanificacionItEr18S08: Adding javadoc to IContext methods.

--- navalplanner-gantt-zk/src/main/java/org/zkoss/ganttz/extensions/IContext.java
+++ navalplanner-gantt-zk/src/main/java/org/zkoss/ganttz/extensions/IContext.java
@@ -1,21 +1,46 @@
 package org.zkoss.ganttz.extensions;
 
+import org.zkoss.ganttz.Planner;
+import org.zkoss.ganttz.TaskComponent;
+import org.zkoss.ganttz.adapters.IAdapterToTaskFundamentalProperties;
+import org.zkoss.ganttz.adapters.IStructureNavigator;
 import org.zkoss.ganttz.adapters.PlannerConfiguration;
+import org.zkoss.ganttz.data.Task;
 import org.zkoss.zk.ui.Component;
-
 
 /**
  * A facade for operations allowed to extensions <br />
  * @author Óscar González Fernández <ogonzalez@igalia.com>
  */
 public interface IContext<T> {
-
+    /**
+     * Adding domainObject to the {@link Planner}. It is transformed using
+     * {@link IAdapterToTaskFundamentalProperties} and
+     * {@link IStructureNavigator}. It appends the created {@link Task} to the
+     * top of the planner
+     * @param domainObject
+     */
     void add(T domainObject);
 
+    /**
+     * Reloading all the {@link Planner} from the configuration
+     * @param configuration
+     */
     void reload(PlannerConfiguration<?> configuration);
 
+    /**
+     * Removing the tasks associated to the domainObject
+     * @param domainObject
+     */
     void remove(T domainObject);
 
+    /**
+     * Retrieves the component associated to the action performed. Normally it
+     * is the {@link Planner}, but it can be other. For example, if the action
+     * is performed on a {@link TaskComponent} this method might return the said
+     * component.
+     * @return the component the action is relative to
+     */
     public Component getRelativeTo();
 
 }
