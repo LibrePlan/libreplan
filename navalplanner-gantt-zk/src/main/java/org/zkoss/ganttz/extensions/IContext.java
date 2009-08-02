9a58bcd899139f12fc51dfdd844c5a3e3a77ae20
Óscar González Fernández <ogonzalez@igalia.com>
2009-08-02 19:32:26 +0200

    ItEr18S09CUCreacionProxectoPlanificacionItEr17S10: Adding api comments to IContext.

--- navalplanner-gantt-zk/src/main/java/org/zkoss/ganttz/extensions/IContext.java
+++ navalplanner-gantt-zk/src/main/java/org/zkoss/ganttz/extensions/IContext.java
@@ -5,15 +5,16 @@ import org.zkoss.ganttz.TaskComponent;
 import org.zkoss.ganttz.adapters.IAdapterToTaskFundamentalProperties;
 import org.zkoss.ganttz.adapters.IStructureNavigator;
 import org.zkoss.ganttz.adapters.PlannerConfiguration;
+import org.zkoss.ganttz.data.Position;
 import org.zkoss.ganttz.data.Task;
 import org.zkoss.zk.ui.Component;
-import org.zkoss.ganttz.data.Position;
 
 /**
  * A facade for operations allowed to extensions <br />
  * @author Óscar González Fernández <ogonzalez@igalia.com>
  */
 public interface IContext<T> {
+
     /**
      * Adding domainObject to the {@link Planner}. It is transformed using
      * {@link IAdapterToTaskFundamentalProperties} and
@@ -34,7 +35,6 @@ public interface IContext<T> {
      * @param domainObject
      * @return the Position in which the domainObject's task was
      */
-
     Position remove(T domainObject);
 
     /**
@@ -46,8 +46,27 @@ public interface IContext<T> {
      */
     public Component getRelativeTo();
 
+    /**
+     * Removes the oldDomainObject, and adds newDomainObject in the position
+     * occupied by oldDomainObject.<br />
+     * This method might be useful when you modify a domainObject and you want
+     * it to be reloaded again
+     * @param oldDomainObject
+     *            the domain object associated to the task that is going to be
+     *            removed
+     * @param newDomainObject
+     *            the domain object from which a task will be created and
+     *            positioned in the place of oldDomainObject
+     */
     void replace(T oldDomainObject, T newDomainObject);
 
+    /**
+     * Inserts a new task created from domainObject on the position specified.
+     * @param position
+     *            The position in which the insertion will be done
+     * @param domainObject
+     *            the domain object from which a task will be created
+     */
     void add(Position position, T domainObject);
 
 }
