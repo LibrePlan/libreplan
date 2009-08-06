a667a528a245d42d8c1ff0d0a7de386197b8c460
Manuel Rego Casasnovas <mrego@igalia.com>
2009-08-06 17:58:37 +0200

    ItEr20S04ArquitecturaServidorItEr19S04: Added needed configuration and dependencies to test the model classes from webapp.

---
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/WebappGlobalNames.java
@@ -0,0 +1,12 @@
+package org.navalplanner.web;
+
+/**
+ * A class containing constants for global names.
+ *
+ * @author Manuel Rego Casasnovas <mrego@igalia.com>
+ */
+public class WebappGlobalNames {
+
+    public final static String WEBAPP_SPRING_CONFIG_FILE = "classpath:/navalplanner-webapp-spring-config.xml";
+
+}
---
+++ navalplanner-webapp/src/test/java/org/navalplanner/web/test/WebappGlobalNames.java
@@ -0,0 +1,12 @@
+package org.navalplanner.web.test;
+
+/**
+ * A class containing constants for global names.
+ *
+ * @author Manuel Rego Casasnovas <mrego@igalia.com>
+ */
+public class WebappGlobalNames {
+
+    public final static String WEBAPP_SPRING_CONFIG_TEST_FILE = "classpath:/navalplanner-webapp-spring-config-test.xml";
+
+}
