84252a32163db50f66f71bb0643ca644d95225d0
Susana Montes Pedreira <smpedreira@gmail.com>
2009-08-06 16:40:48 +0200

    ItEr19S14CUListaPartesTraballoDendeOrganizacionTraballoItEr18S12: Modifying of the view of the imputed hours to a order element.

--- navalplanner-webapp/src/main/java/org/navalplanner/web/orders/AsignedHoursToOrderElementController.java
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/orders/AsignedHoursToOrderElementController.java
@@ -1,15 +1,16 @@
 package org.navalplanner.web.orders;
 
 import java.util.List;
+
 import org.navalplanner.business.workreports.entities.WorkReportLine;
-import org.zkoss.zul.Window;
 import org.navalplanner.web.common.Util;
-import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Progressmeter;
+import org.zkoss.zul.Window;
 
 /**
  * Controller for show the asigned hours of the selected order element<br />
- *
  * @author Susana Montes Pedreria <smontes@wirelessgalicia.com>
  */
 public class AsignedHoursToOrderElementController extends
@@ -58,5 +59,33 @@ public class AsignedHoursToOrderElementController extends
     public void openWindow(IOrderElementModel orderElementModel) {
         this.asignedHoursToOrderElementModel.initOrderElement(orderElementModel
                 .getOrderElement());
+        viewPercentage();
     }
+
+    /**
+     * This method shows the percentage of the imputed hours with respect to the
+     * estimated hours.If the hours imputed is greater that the hours estimated
+     * then show the exceed percentage of hours.
+     */
+    private void viewPercentage() {
+
+        Progressmeter progressBar = (Progressmeter) window
+                .getFellow("HoursProgressBar");
+        Progressmeter exceedHoursProgressBar = (Progressmeter) window
+                .getFellow("exceedHoursProgressBar");
+        if (this.getProgressWork() > 100) {
+            progressBar.setValue(100);
+            exceedHoursProgressBar.setVisible(true);
+            exceedHoursProgressBar.setValue(0);
+            String exceedValue = String.valueOf(getProgressWork() - 100);
+            exceedHoursProgressBar.setWidth(exceedValue + "px");
+            exceedHoursProgressBar.setLeft("left");
+            exceedHoursProgressBar
+                    .setStyle("background:red ; border:1px solid red");
+        } else {
+            progressBar.setValue(getProgressWork());
+            exceedHoursProgressBar.setVisible(false);
+        }
+    }
+
 }
