5ee30eb5ebda51076dd5232b15836decc6ee571f
Óscar González Fernández <ogonzalez@igalia.com>
2009-08-07 18:09:31 +0200

    ItEr20S04ArquitecturaServidorItEr19S04: Erasing not used methods from Registry. They will be added as needed.

--- navalplanner-business/src/main/java/org/navalplanner/business/common/Registry.java
+++ navalplanner-business/src/main/java/org/navalplanner/business/common/Registry.java
@@ -1,18 +1,14 @@
 package org.navalplanner.business.common;
 
-import org.navalplanner.business.advance.daos.IAdvanceAssigmentDAO;
-import org.navalplanner.business.advance.daos.IAdvanceMeasurementDAO;
 import org.navalplanner.business.advance.daos.IAdvanceTypeDAO;
-import org.navalplanner.business.workreports.daos.IWorkReportDAO;
-import org.navalplanner.business.workreports.daos.IWorkReportLineDAO;
-import org.navalplanner.business.workreports.daos.IWorkReportTypeDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * A registry, AKA service locator, for objects in which dependency injection
  * (DI) is not directly supported by Spring (e.g. entities) must use this class
  * to access DAOs. For the rest of classes (e.g. services, tests, etc.), Spring
- * DI is a more convenient option.
+ * DI is a more convenient option. The DAOs or services are added to the
+ * registry as needed.
  * @author Óscar González Fernández <ogonzalez@igalia.com>
  */
 public class Registry {
@@ -20,53 +16,17 @@ public class Registry {
     private static final Registry singleton = new Registry();
 
     @Autowired
-    private IWorkReportDAO workReport;
-
-    @Autowired
-    private IWorkReportTypeDAO workReportType;
-
-    @Autowired
-    private IWorkReportLineDAO workReportLine;
-
-    @Autowired
-    private IAdvanceAssigmentDAO advanceAssigmentDao;
-
-    @Autowired
     private IAdvanceTypeDAO advanceTypeDao;
 
-    @Autowired
-    private IAdvanceMeasurementDAO advanceMeasurementDao;
-
     private Registry() {
-
     }
 
     public static Registry getInstance() {
         return singleton;
     }
 
-    public static IWorkReportDAO getWorkReportDao() {
-        return getInstance().workReport;
-    }
-
-    public static IWorkReportTypeDAO getWorkReportTypeDao() {
-        return getInstance().workReportType;
-    }
-
-    public static IWorkReportLineDAO getWorkReportLineDao() {
-        return getInstance().workReportLine;
-    }
-
-    public static IAdvanceMeasurementDAO getAdvanceMeasurementDao() {
-        return getInstance().advanceMeasurementDao;
-    }
-
     public static IAdvanceTypeDAO getAdvanceTypeDao() {
         return getInstance().advanceTypeDao;
-    }
-
-    public static IAdvanceAssigmentDAO getAdvanceAssigmentDao() {
-        return getInstance().advanceAssigmentDao;
     }
 
 }
