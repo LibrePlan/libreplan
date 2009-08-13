67f0f0dd971138e84ff853a2b6f49f897b90063f
Manuel Rego Casasnovas <mrego@igalia.com>
2009-08-13 13:00:57 +0200

    ItEr21S10CUAltaCalendarioLaboral: Added entity ResourceCalendar.

---
+++ navalplanner-business/src/main/java/org/navalplanner/business/calendars/entities/ResourceCalendar.java
@@ -0,0 +1,12 @@
+package org.navalplanner.business.calendars.entities;
+
+import org.navalplanner.business.resources.entities.Resource;
+
+/**
+ * Calendar for a {@link Resource}.
+ *
+ * @author Manuel Rego Casasnovas <mrego@igalia.com>
+ */
+public class ResourceCalendar extends BaseCalendar {
+
+}
