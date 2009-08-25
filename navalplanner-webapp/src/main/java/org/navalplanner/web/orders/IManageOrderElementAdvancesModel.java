addc2b4f100de75a9c6c4eeeed912bfa1911daf7
Óscar González Fernández <ogonzalez@igalia.com>
2009-08-25 13:27:43 +0200

    ItEr23S07CUIntroducionAvanceUnidadeTraballoItEr22S07: Fixing conflicts

--- navalplanner-webapp/src/main/java/org/navalplanner/web/orders/IManageOrderElementAdvancesModel.java
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/orders/IManageOrderElementAdvancesModel.java
@@ -17,19 +17,13 @@ public interface IManageOrderElementAdvancesModel {
     public List<AdvanceMeasurementDTO> getAdvanceMeasurements();
     public void init(OrderElement orderElement);
     public void addNewLine();
-<<<<<<< HEAD:navalplanner-webapp/src/main/java/org/navalplanner/web/orders/IManageOrderElementAdvancesModel.java
-    public void removeLine(IAdvanceMeasurementDTO advanceDTO);
-    public List<AdvanceType> getActivesAdvanceTypes();
-    public boolean isPrecisionValid(IAdvanceMeasurementDTO advanceDTO, BigDecimal value);
-    public boolean greatThanMaxValue(IAdvanceMeasurementDTO advanceDTO, BigDecimal value);
-    public boolean isGreatValidDate(IAdvanceMeasurementDTO advanceDTO, Date value);
-=======
+
     public void removeLine(AdvanceMeasurementDTO advanceDTO);
     public List<AdvanceType> getActivesAdvanceTypes();
     public boolean isPrecisionValid(AdvanceMeasurementDTO advanceDTO, BigDecimal value);
     public boolean greatThanMaxValue(AdvanceMeasurementDTO advanceDTO, BigDecimal value);
     public boolean isGreatValidDate(AdvanceMeasurementDTO advanceDTO, Date value);
->>>>>>> cb4190f... ItEr23S07CUIntroducionAvanceUnidadeTraballoItEr22S07 : Remove the interfaz IAdvanceMeasurementDTO:navalplanner-webapp/src/main/java/org/navalplanner/web/orders/IManageOrderElementAdvancesModel.java
+
     public void accept()throws InstanceNotFoundException,
             DuplicateAdvanceAssigmentForOrderElementException,
             DuplicateValueTrueReportGlobalAdvanceException;
