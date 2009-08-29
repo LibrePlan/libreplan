d81d8a4db20084420f7cbfa8c0b59ca2f241d0dd
Óscar González Fernández <ogonzalez@igalia.com>
2009-08-29 23:37:09 +0200

    ItEr23S10CUAsignacionGrupoRecursosAPlanificacionItEr22S10: TimeTrackedTable second level is now a grid.

--- ganttzk/src/main/java/org/zkoss/ganttz/timetracker/TimeTrackedTable.java
+++ ganttzk/src/main/java/org/zkoss/ganttz/timetracker/TimeTrackedTable.java
@@ -1,14 +1,33 @@
 package org.zkoss.ganttz.timetracker;
 
+import org.zkoss.zul.ListModel;
+import org.zkoss.zul.RowRenderer;
+
 public class TimeTrackedTable extends TimeTrackerComponent {
 
-    public TimeTrackedTable(TimeTracker timeTracker, String idTimeTrackerElement) {
-        super(timeTracker, "~./ganttz/zul/timetrackersecondlevel.zul",
+    private final ListModel listModel;
+
+    private final RowRenderer rowRenderer;
+
+    public TimeTrackedTable(ListModel listModel, RowRenderer rowRenderer,
+            TimeTracker timeTracker,
+            String idTimeTrackerElement) {
+        super(timeTracker, "~./ganttz/zul/timetracker/secondlevelgrid.zul",
                 idTimeTrackerElement);
+        this.listModel = listModel;
+        this.rowRenderer = rowRenderer;
     }
 
     @Override
     protected void scrollHorizontalPercentage(int pixelsDisplacement) {
+    }
+
+    public ListModel getTableModel() {
+        return listModel;
+    }
+
+    public RowRenderer getGridRenderer() {
+        return rowRenderer;
     }
 
 }
