2327d0b8cfa3d08c27d5e25f45c6930da37f006f
Óscar González Fernández <ogonzalez@igalia.com>
2009-09-01 11:30:02 +0200

    ItEr24S08CUAsignacionGrupoRecursosAPlanificacionItEr23S10: Fixing wrong script names. They were being specified as relative urls.
    This was causing problem loading page

--- ganttzk/src/main/java/org/zkoss/ganttz/ScriptsRequiredByPlanner.java
+++ ganttzk/src/main/java/org/zkoss/ganttz/ScriptsRequiredByPlanner.java
@@ -6,10 +6,10 @@ import org.zkoss.ganttz.util.script.ScriptsRequiredDeclaration;
 public class ScriptsRequiredByPlanner {
 
     public static final String SELECTOR = "/zkau/web/js/yui/2.7.0/selector/selector-min.js";
-    public static final String YAHOO_DOM_EVENT = "zkau/web/js/yui/2.7.0/yahoo-dom-event/yahoo-dom-event.js";
-    public static final String DRAGDROPMIN = "zkau/web/js/yui/2.7.0/dragdrop/dragdrop-min.js";
+    public static final String YAHOO_DOM_EVENT = "/zkau/web/js/yui/2.7.0/yahoo-dom-event/yahoo-dom-event.js";
+    public static final String DRAGDROPMIN = "/zkau/web/js/yui/2.7.0/dragdrop/dragdrop-min.js";
 
-    public static final String ELEMENT_MIN = "zkau/web/js/yui/2.7.0/element/element-min.js";
-    public static final String RESIZE_MIN = "zkau/web/js/yui/2.7.0/resize/resize-min.js";
-    public static final String LOGGER_MIN = "zkau/web/js/yui/2.7.0/logger/logger-min.js";
+    public static final String ELEMENT_MIN = "/zkau/web/js/yui/2.7.0/element/element-min.js";
+    public static final String RESIZE_MIN = "/zkau/web/js/yui/2.7.0/resize/resize-min.js";
+    public static final String LOGGER_MIN = "/zkau/web/js/yui/2.7.0/logger/logger-min.js";
 }
