265dc3519c0c1adfbd6a11342f20e1c689bb9b8b
Óscar González Fernández <ogonzalez@igalia.com>
2009-09-01 13:22:18 +0200

    ItEr24S01Coordinacion: Adding ListSorter class. It can be useful to keep a list sorted after modifications are done.

---
+++ navalplanner-business/src/main/java/org/navalplanner/business/util/ListSorter.java
@@ -0,0 +1,122 @@
+package org.navalplanner.business.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.apache.commons.lang.Validate;
+
+/**
+ * This class wraps a list that is kept sorted if and only if modify method is
+ * called as modifications are done. There cannot be elements a and b at
+ * different positions such as a == b.
+ * <p>
+ * No null elements allowed
+ * <p>
+ * This class it NOT THREAD SAFE
+ * @author Óscar González Fernández <ogonzalez@igalia.com>
+ */
+public class ListSorter<T> {
+
+    private final List<T> list;
+    private final Comparator<T> comparator;
+
+    public static <T extends Comparable<T>> ListSorter<T> create(
+            Collection<T> elements) {
+        return create(elements, new Comparator<T>() {
+
+            @Override
+            public int compare(T o1, T o2) {
+                return o1.compareTo(o2);
+            }
+        });
+    }
+
+    public static <T> ListSorter<T> create(Collection<T> elements,
+            Comparator<T> comparator) {
+        Validate.notNull(elements);
+        Validate.notNull(comparator);
+        Validate.noNullElements(elements);
+        return new ListSorter<T>(elements, comparator);
+    }
+
+    private ListSorter(Collection<? extends T> elements,
+            Comparator<T> comparator) {
+        this.comparator = comparator;
+        this.list = new ArrayList<T>(elements);
+        Collections.sort(this.list, this.comparator);
+    }
+
+    public List<T> toList() {
+        return Collections.unmodifiableList(list);
+    }
+
+    /**
+     * Repositions the element since element could have been changed
+     * @param element
+     *            the element to be repositioned
+     * @throws NoSuchElementException
+     *             if there is no element e in list such e == element
+     */
+    public void modify(T element) throws NoSuchElementException {
+        int index = indexOfElement(element);
+        if ((index == list.size()))
+            throw new NoSuchElementException("not found: " + element);
+        list.remove(index);
+        insert(element);
+    }
+
+    private void insert(T element) {
+        int position = Collections.binarySearch(list, element, comparator);
+        if (position < 0) {
+            position = (-position) - 1;
+        }
+        list.add(position, element);
+    }
+
+    private int indexOfElement(T element) {
+        int index = 0;
+        for (T t : list) {
+            if (t == element)
+                break;
+            index++;
+        }
+        return index;
+    }
+
+    /**
+     * Adds the element at the right position keeping the order.
+     * @throws IllegalArgumentException
+     *             if the element already existed
+     * @param element
+     */
+    public void add(T element) {
+        Validate.notNull(element);
+        if (exists(element))
+            throw new IllegalArgumentException(element
+                    + " already exists. Duplicateds not allowed");
+        insert(element);
+    }
+
+    /**
+     * @param element
+     * @throws IllegalArgumentException
+     *             it the element doesn't exist
+     */
+    public void remove(T element) {
+        int position = indexOfElement(element);
+        if (position == list.size()) {
+            throw new NoSuchElementException(element + " doesn't exist");
+        }
+        list.remove(position);
+    }
+
+    private boolean exists(T element) {
+        int index = indexOfElement(element);
+        return index < list.size();
+    }
+
+}
---
+++ navalplanner-business/src/test/java/org/navalplanner/business/util/ListSorterTest.java
@@ -0,0 +1,147 @@
+package org.navalplanner.business.util;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+
+public class ListSorterTest {
+
+    private ListSorter<DumbContainer> listSorter;
+    private List<DumbContainer> initialElements;
+
+    private void givenSortedList() {
+        initialElements = containers("A", "B", "C");
+        listSorter = ListSorter.create(initialElements);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void theInitialElementsMustBeNotNull() {
+        ListSorter.<String> create(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void theInitialElementsDontHaveNullElements() {
+        ListSorter.create(asList("A", "C", null));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void theComparatorCannotBeNull() {
+        ListSorter.create(Arrays.asList("A"), null);
+    }
+
+    @Test
+    public void itSortsInitialElements() {
+        givenSortedList();
+        assertThat(listSorter.toList(), equalTo(containers("A", "B", "C")));
+    }
+
+    private static List<DumbContainer> containers(String... strings) {
+        ArrayList<DumbContainer> result = new ArrayList<DumbContainer>();
+        for (String s : strings) {
+            result.add(new DumbContainer(s));
+        }
+        return result;
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void theListReturnedCannotBeModified() {
+        givenSortedList();
+        listSorter.toList().clear();
+    }
+
+    @Test
+    public void ifModifiedTheElementIsRepositioned() {
+        givenSortedList();
+        DumbContainer first = initialElements.get(0);
+        first.setData("D");
+        listSorter.modify(first);
+        assertThat(listSorter.toList(), equalTo(containers("B", "C", "D")));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void modifyCanOnlyBeCalledWithElementsWithSameReference() {
+        givenSortedList();
+        listSorter.modify(new DumbContainer("B"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void anAlreadyAddedCannotBeAdded() {
+        givenSortedList();
+        listSorter.add(initialElements.get(0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullElementCannotBeAdded() {
+        givenSortedList();
+        listSorter.add(null);
+    }
+
+    @Test
+    public void addInsertsAtRightPosition() {
+        givenSortedList();
+        listSorter.add(new DumbContainer("0"));
+        assertThat(listSorter.toList(), equalTo(containers("0", "A", "B", "C")));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void cantRemoveEqualButNotSameInstance() {
+        givenSortedList();
+        listSorter.remove(new DumbContainer("A"));
+    }
+
+    @Test
+    public void removingExistent() {
+        givenSortedList();
+        listSorter.remove(initialElements.get(0));
+        assertThat(listSorter.toList(), equalTo(containers("B", "C")));
+    }
+
+}
+
+class DumbContainer implements Comparable<DumbContainer> {
+
+    private String data;
+
+    public DumbContainer(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public int compareTo(DumbContainer o) {
+        return this.data.compareTo(o.data);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof DumbContainer) {
+            DumbContainer dumb = (DumbContainer) obj;
+            return dumb.data.equals(this.data);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.data.hashCode();
+    }
+}
