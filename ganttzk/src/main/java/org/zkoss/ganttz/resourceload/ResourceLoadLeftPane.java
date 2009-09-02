74d2d968276c7585b12333586cd756413c5a766f
Óscar González Fernández <ogonzalez@igalia.com>
2009-09-02 20:49:58 +0200

    ItEr24S09CUVistaRecursosTempoPorProxectoItEr23S11: Now it listens to the openening and collapsing of the tree nodes

--- ganttzk/src/main/java/org/zkoss/ganttz/resourceload/ResourceLoadLeftPane.java
+++ ganttzk/src/main/java/org/zkoss/ganttz/resourceload/ResourceLoadLeftPane.java
@@ -1,12 +1,12 @@
 package org.zkoss.ganttz.resourceload;
 
-import java.util.List;
-
 import org.zkoss.ganttz.data.resourceload.LoadTimeLine;
-import org.zkoss.ganttz.data.resourceload.LoadTimelinesGroup;
 import org.zkoss.ganttz.util.MutableTreeModel;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.HtmlMacroComponent;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.OpenEvent;
 import org.zkoss.zul.Div;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.Treecell;
@@ -17,12 +17,13 @@ import org.zkoss.zul.api.Tree;
 
 public class ResourceLoadLeftPane extends HtmlMacroComponent {
 
-    private final List<LoadTimelinesGroup> groups;
     private MutableTreeModel<LoadTimeLine> modelForTree;
+    private final ResourceLoadList resourceLoadList;
 
-    public ResourceLoadLeftPane(List<LoadTimelinesGroup> groups) {
-        this.groups = groups;
-        this.modelForTree = createModelForTree();
+    public ResourceLoadLeftPane(MutableTreeModel<LoadTimeLine> modelForTree,
+            ResourceLoadList resourceLoadList) {
+        this.resourceLoadList = resourceLoadList;
+        this.modelForTree = modelForTree;
     }
 
 
@@ -44,6 +45,22 @@ public class ResourceLoadLeftPane extends HtmlMacroComponent {
                 item.appendChild(row);
                 row.appendChild(cell);
                 cell.appendChild(component);
+                addExpandedListener(item, line);
+            }
+
+            private void addExpandedListener(Treeitem item,
+                    final LoadTimeLine line) {
+                item.addEventListener("onOpen", new EventListener() {
+                    @Override
+                    public void onEvent(Event event) throws Exception {
+                        OpenEvent openEvent = (OpenEvent) event;
+                        if (openEvent.isOpen()) {
+                            expand(line);
+                        } else {
+                            collapse(line);
+                        }
+                    }
+                });
             }
 
             private Component createComponent(LoadTimeLine line) {
@@ -58,16 +75,16 @@ public class ResourceLoadLeftPane extends HtmlMacroComponent {
         };
     }
 
-    private MutableTreeModel<LoadTimeLine> createModelForTree() {
-        MutableTreeModel<LoadTimeLine> result = MutableTreeModel
-                .create(LoadTimeLine.class);
-        for (LoadTimelinesGroup loadTimelinesGroup : this.groups) {
-            LoadTimeLine principal = loadTimelinesGroup.getPrincipal();
-            result.addToRoot(principal);
-            result.add(principal, loadTimelinesGroup.getChildren());
-        }
-        return result;
+    private void collapse(LoadTimeLine line) {
+        // TODO do collapse
     }
+
+    private void expand(LoadTimeLine line) {
+        // TODO do expand
+
+    }
+
+
 
     private Tree getContainerTree() {
         return (Tree) getFellow("loadsTree");
--- ganttzk/src/main/java/org/zkoss/ganttz/resourceload/ResourcesLoadPanel.java
+++ ganttzk/src/main/java/org/zkoss/ganttz/resourceload/ResourcesLoadPanel.java
@@ -3,9 +3,11 @@ package org.zkoss.ganttz.resourceload;
 import java.util.List;
 
 import org.zkoss.ganttz.Planner;
+import org.zkoss.ganttz.data.resourceload.LoadTimeLine;
 import org.zkoss.ganttz.data.resourceload.LoadTimelinesGroup;
 import org.zkoss.ganttz.timetracker.TimeTracker;
 import org.zkoss.ganttz.timetracker.TimeTrackerComponent;
+import org.zkoss.ganttz.util.MutableTreeModel;
 import org.zkoss.zk.au.out.AuInvoke;
 import org.zkoss.zk.ui.ext.AfterCompose;
 import org.zkoss.zul.impl.XulElement;
@@ -20,15 +22,29 @@ public class ResourcesLoadPanel extends XulElement implements AfterCompose {
 
     private final List<LoadTimelinesGroup> groups;
 
+    private MutableTreeModel<LoadTimeLine> treeModel;
+
     public ResourcesLoadPanel(List<LoadTimelinesGroup> groups,
             TimeTracker timeTracker) {
         this.groups = groups;
+        treeModel = createModelForTree();
         timeTrackerComponent = timeTrackerForResourcesLoadPanel(timeTracker);
         resourceLoadList = new ResourceLoadList(timeTracker, groups);
-        leftPane = new ResourceLoadLeftPane(groups);
+        leftPane = new ResourceLoadLeftPane(treeModel, resourceLoadList);
         appendChild(timeTrackerComponent);
         appendChild(leftPane);
         appendChild(resourceLoadList);
+    }
+
+    private MutableTreeModel<LoadTimeLine> createModelForTree() {
+        MutableTreeModel<LoadTimeLine> result = MutableTreeModel
+                .create(LoadTimeLine.class);
+        for (LoadTimelinesGroup loadTimelinesGroup : this.groups) {
+            LoadTimeLine principal = loadTimelinesGroup.getPrincipal();
+            result.addToRoot(principal);
+            result.add(principal, loadTimelinesGroup.getChildren());
+        }
+        return result;
     }
 
     private TimeTrackerComponent timeTrackerForResourcesLoadPanel(
