b312930cffff58ad383a95b149dce2957c577220
Diego Pino Garcia <dpino@igalia.com>
2009-09-14 09:08:04 +0200

    ItEr25S12AltaTipoDeEtiquetas: Add cancel

--- navalplanner-webapp/src/main/java/org/navalplanner/web/labels/LabelTypeCRUDController.java
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/labels/LabelTypeCRUDController.java
@@ -137,4 +137,8 @@ public class LabelTypeCRUDController extends GenericForwardComposer {
         Util.reloadBindings(editWindow);
     }
 
+    public void cancel() {
+        goToList();
+    }
+
 }
