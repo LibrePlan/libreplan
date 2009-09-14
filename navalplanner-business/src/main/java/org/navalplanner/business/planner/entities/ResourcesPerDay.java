91c57c35711fdb65c4850898ae5075775c37cfb9
Óscar González Fernández <ogonzalez@igalia.com>
2009-09-14 14:44:35 +0200

    ItEr26S07CUAsignacionGrupoRecursosAPlanificacionItEr25S07: ResourcesPerDay now stores a decimal value

--- navalplanner-business/src/main/java/org/navalplanner/business/planner/entities/ResourcesPerDay.java
+++ navalplanner-business/src/main/java/org/navalplanner/business/planner/entities/ResourcesPerDay.java
@@ -1,26 +1,35 @@
 package org.navalplanner.business.planner.entities;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import org.apache.commons.lang.Validate;
 
 public class ResourcesPerDay {
 
-    private final int amount;
+    private final BigDecimal amount;
 
     public static ResourcesPerDay amount(int amount) {
-        return new ResourcesPerDay(amount);
+        return new ResourcesPerDay(new BigDecimal(amount));
     }
 
-    private ResourcesPerDay(int amount) {
-        Validate.isTrue(amount >= 0);
-        this.amount = amount;
+    public static ResourcesPerDay amount(BigDecimal decimal) {
+        return new ResourcesPerDay(decimal);
     }
 
-    public int getAmount() {
+    private ResourcesPerDay(BigDecimal amount) {
+        Validate.isTrue(amount.intValue() >= 0);
+        this.amount = amount.setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public BigDecimal getAmount() {
         return amount;
     }
 
     public int asHoursGivenResourceWorkingDayOf(
             Integer resourceWorkingDayHours) {
-        return getAmount() * resourceWorkingDayHours;
+        return getAmount().multiply(new BigDecimal(resourceWorkingDayHours))
+                .setScale(0, RoundingMode.HALF_UP).intValue();
     }
+
 }
--- navalplanner-business/src/test/java/org/navalplanner/business/test/planner/entities/ResourcesPerDayTest.java
+++ navalplanner-business/src/test/java/org/navalplanner/business/test/planner/entities/ResourcesPerDayTest.java
@@ -3,6 +3,11 @@ package org.navalplanner.business.test.planner.entities;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import java.math.BigDecimal;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.Test;
 import org.navalplanner.business.planner.entities.ResourcesPerDay;
 
@@ -16,13 +21,69 @@ public class ResourcesPerDayTest {
     @Test
     public void theUnitsAmoutCanBeRetrieved() {
         ResourcesPerDay units = ResourcesPerDay.amount(2);
-        assertThat(units.getAmount(), equalTo(2));
+        assertThat(units.getAmount(), hasIntegerPart(2));
+    }
+
+    private Matcher<BigDecimal> hasIntegerPart(final int integer) {
+        return new BaseMatcher<BigDecimal>() {
+
+            @Override
+            public boolean matches(Object arg) {
+                if (arg instanceof BigDecimal) {
+                    BigDecimal value = (BigDecimal) arg;
+                    return value.intValue() == integer;
+                }
+                return false;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description
+                        .appendText("must have an integer part of" + integer);
+            }
+        };
+    }
+
+    @Test
+    public void theUnitsAmountCanBeADecimalValue() {
+        ResourcesPerDay resourcesPerDay = ResourcesPerDay
+                .amount(new BigDecimal(2.2));
+        assertThat(resourcesPerDay.getAmount(), hasIntegerPart(2));
+    }
+
+    @Test
+    public void theAmountIsConvertedToABigDecimalOfScale2() {
+        ResourcesPerDay resourcesPerDay = ResourcesPerDay
+                .amount(new BigDecimal(2.2));
+        assertThat(resourcesPerDay.getAmount().scale(), equalTo(2));
+    }
+
+    @Test
+    public void ifTheAmountSpecifiedHasBiggerScaleThan2ItIsRoundedHalfUp() {
+        BigDecimal[] examples = { new BigDecimal(2.236),
+                new BigDecimal(2235).movePointLeft(3), new BigDecimal(2.24),
+                new BigDecimal(2.2449) };
+        for (BigDecimal example : examples) {
+            ResourcesPerDay resourcesPerDay = ResourcesPerDay.amount(example);
+            assertThat(resourcesPerDay.getAmount().scale(), equalTo(2));
+            assertThat(
+                    resourcesPerDay.getAmount().movePointRight(2).intValue(),
+                    equalTo(224));
+        }
     }
 
     @Test
     public void canBeConvertedToHoursGivenTheWorkingDayHours() {
         ResourcesPerDay units = ResourcesPerDay.amount(2);
         assertThat(units.asHoursGivenResourceWorkingDayOf(8), equalTo(16));
+    }
+
+    @Test
+    public void ifTheAmountIsDecimalTheRoundingIs() {
+        ResourcesPerDay units = ResourcesPerDay.amount(new BigDecimal(2.4));
+        assertThat(units.asHoursGivenResourceWorkingDayOf(8), equalTo(19));
+        assertThat(units.asHoursGivenResourceWorkingDayOf(10), equalTo(24));
+        assertThat(units.asHoursGivenResourceWorkingDayOf(2), equalTo(5));
     }
 
 }
