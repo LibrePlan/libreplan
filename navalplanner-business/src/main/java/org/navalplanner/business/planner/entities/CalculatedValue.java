56bc2d8c167c79bf4d498c4cf699d7525865f9f6
Óscar González Fernández <ogonzalez@igalia.com>
2009-09-21 21:28:46 +0200

    ItEr27S06CUAsignacionGrupoRecursosAPlanificacionItEr26S07: Adding calculated value enum

---
+++ navalplanner-business/src/main/java/org/navalplanner/business/planner/entities/CalculatedValue.java
@@ -0,0 +1,9 @@
+package org.navalplanner.business.planner.entities;
+
+/**
+ * It represents the value that is calculated
+ * @author Óscar González Fernández <ogonzalez@igalia.com>
+ */
+public enum CalculatedValue {
+    NUMBER_OF_HOURS, END_DATE, RESOURCES_PER_DAY;
+}
