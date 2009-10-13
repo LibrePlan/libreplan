3a67b73be3bfdb0c41d04dc010e953090e83655d
Diego Pino Garcia <dpino@igalia.com>
2009-10-13 10:23:28 +0200

    ItEr29S14ProcuraOrganizacionsTraballo: Create BandboxSearch macro-component
    A bandboxSearch component receives a finder parameter of which must implement IBandboxSearch interface

---
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/common/components/bandboxsearch/BandboxSearch.java
@@ -0,0 +1,165 @@
+/*
+ * This file is part of ###PROJECT_NAME###
+ *
+ * Copyright (C) 2009 Fundación para o Fomento da Calidade Industrial e
+ *                    Desenvolvemento Tecnolóxico de Galicia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.navalplanner.web.common.components.bandboxsearch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.navalplanner.business.labels.entities.Label;
+import org.navalplanner.web.common.components.finders.IBandboxFinder;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.HtmlMacroComponent;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.InputEvent;
+import org.zkoss.zul.Bandbox;
+import org.zkoss.zul.ListModel;
+import org.zkoss.zul.Listhead;
+import org.zkoss.zul.Listheader;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.SimpleListModel;
+import org.zkoss.zul.api.Listbox;
+
+@SuppressWarnings("serial")
+public class BandboxSearch extends HtmlMacroComponent {
+
+    private Listbox listbox;
+
+    private Listhead listhead;
+
+    private Bandbox bandbox;
+
+    private IBandboxFinder finder;
+
+    public void afterCompose() {
+        super.afterCompose();
+        listbox = (Listbox) getFellowIfAny("listbox");
+        listbox.setModel(finder.getModel());
+        listbox.setItemRenderer(finder.getItemRenderer());
+
+        listhead = (Listhead) listbox.getFellowIfAny("listhead");
+        bandbox = (Bandbox) getFellowIfAny("bandbox");
+
+        /**
+         * Search for matching elements while typing on bandbox
+         */
+        bandbox.addEventListener("onChanging", new EventListener() {
+
+            @Override
+            public void onEvent(Event event) throws Exception {
+                clearSelectedElement();
+                final String inputText = ((InputEvent) event).getValue();
+                listbox.setModel(getSubModel(inputText));
+                listbox.invalidate();
+            }
+        });
+
+        /**
+         * Pick element from list when selecting
+         */
+        listbox.addEventListener("onSelect", new EventListener() {
+
+            @Override
+            public void onEvent(Event event) throws Exception {
+                final Object object = getSelectedItem().getValue();
+                bandbox.setValue(finder.objectToString(object));
+                setSelectedElement(object);
+                bandbox.close();
+            }
+        });
+
+        addHeaders();
+    }
+
+    private void clearSelectedElement() {
+        setSelectedElement(null);
+    }
+
+    private void setSelectedElement(Object obj) {
+        bandbox.setVariable("selectedElement", obj, true);
+    }
+
+    /**
+     * Find {@link Label} which name or type start with prefix
+     *
+     * @param inputText
+     */
+    @SuppressWarnings("unchecked")
+    private ListModel getSubModel(String inputText) {
+        List result = new ArrayList();
+
+        final SimpleListModel model = finder.getModel();
+        for (int i = 0; i < model.getSize(); i++) {
+            Object obj = model.getElementAt(i);
+            if (finder.entryMatchesText(obj, inputText)) {
+                result.add(obj);
+            }
+        }
+        return new SimpleListModel(result);
+    }
+
+    /**
+     * Append headers to listbox header list
+     */
+    @SuppressWarnings("unchecked")
+    public void addHeaders() {
+        clearHeaderIfNecessary();
+        final String[] headers = finder.getHeaders();
+        for (int i = 0; i < headers.length; i++) {
+            listhead.getChildren().add(new Listheader(headers[i]));
+        }
+    }
+
+    private void clearHeaderIfNecessary() {
+        if (listhead.getChildren() != null) {
+            listhead.getChildren().clear();
+        }
+    }
+
+    private Listitem getSelectedItem() {
+        return (Listitem) listbox.getSelectedItems().iterator().next();
+    }
+
+    public String getFinder() {
+        return finder.getClass().toString();
+    }
+
+    public void setFinder(String classname) {
+        finder = (IBandboxFinder) getBean(StringUtils.uncapitalize(classname));
+    }
+
+    private Object getBean(String classname) {
+        HttpServletRequest servletRequest = (HttpServletRequest) Executions
+                .getCurrent().getNativeRequest();
+        ServletContext servletContext = servletRequest.getSession()
+                .getServletContext();
+        WebApplicationContext webApplicationContext = WebApplicationContextUtils
+                .getWebApplicationContext(servletContext);
+        return webApplicationContext.getBean(classname);
+    }
+
+}
