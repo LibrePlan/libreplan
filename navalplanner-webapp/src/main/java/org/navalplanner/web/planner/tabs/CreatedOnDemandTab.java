c18c4affec6a6c537359b4d7ae766bb2037105f3
Óscar González Fernández <ogonzalez@igalia.com>
2009-10-13 13:02:20 +0200

    ItEr30S15RFVisualizacionMultiplesProxectosItEr29S18: Adding class to simplify the creation of tabs

---
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/planner/tabs/CreatedOnDemandTab.java
@@ -0,0 +1,72 @@
+/*
+ * This file is part of ###PROJECT_NAME###
+ *
+ * Copyright (C) 2009 Fundación para o Fomento da Calidade Industrial e
+ *                    Desenvolvemento Tecnolóxico de Galicia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.navalplanner.web.planner.tabs;
+
+import org.apache.commons.lang.Validate;
+import org.zkoss.ganttz.extensions.ITab;
+import org.zkoss.zk.ui.Component;
+
+/**
+ * @author Óscar González Fernández <ogonzalez@igalia.com>
+ */
+public class CreatedOnDemandTab implements ITab {
+
+    private final String name;
+    private Component parent;
+    private final IComponentCreator componentCreator;
+    private Component component;
+
+    public interface IComponentCreator {
+        public Component create(Component parent);
+    }
+
+    public CreatedOnDemandTab(String name, IComponentCreator componentCreator) {
+        Validate.notNull(name);
+        Validate.notNull(componentCreator);
+        this.componentCreator = componentCreator;
+        this.name = name;
+    }
+
+    @Override
+    public void addToParent(Component parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void hide() {
+        if (component != null) {
+            component.detach();
+        }
+    }
+
+    @Override
+    public void show() {
+        if (component == null) {
+            component = componentCreator.create(parent);
+        }
+        component.setParent(parent);
+    }
+
+}
