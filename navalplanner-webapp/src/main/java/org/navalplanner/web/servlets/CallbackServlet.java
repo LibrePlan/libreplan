4224aa00b30b7f77d0b15d4702f00394faad7f68
Óscar González Fernández <ogonzalez@igalia.com>
2009-10-15 14:13:52 +0200

    Adding system out traces

--- navalplanner-webapp/src/main/java/org/navalplanner/web/servlets/CallbackServlet.java
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/servlets/CallbackServlet.java
@@ -98,6 +98,9 @@ public class CallbackServlet extends HttpServlet {
 
     private static synchronized String buildURLFromKey(String generatedKey) {
         if (contextPath == null) {
+            System.out
+                    .println(CallbackServlet.class.getName()
+                            + " has not been initialized. Register it at web.xml with a load-on-startup element");
             throw new IllegalStateException(CallbackServlet.class.getName()
                             + " has not been initialized. Register it at web.xml with a load-on-startup element");
         }
@@ -139,10 +142,12 @@ public class CallbackServlet extends HttpServlet {
 
     @Override
     public void init(ServletConfig config) throws ServletException {
+        System.out.println("init");
         super.init(config);
         synchronized (CallbackServlet.class) {
             if (contextPath == null) {
                 contextPath = config.getServletContext().getContextPath();
+                System.out.println("before launching timer");
                 scheduleTimer();
             }
         }
@@ -152,7 +157,9 @@ public class CallbackServlet extends HttpServlet {
         try {
             cleaningTimer.schedule(cleaningTask(), CLEANING_PERIOD_MILLIS,
                     CLEANING_PERIOD_MILLIS);
+            System.out.println("after launching timer");
         } catch (Throwable e) {
+            e.printStackTrace();
             LOG
                     .error(
                             "can't start cleaning timer. A memory leak will be caused!",
