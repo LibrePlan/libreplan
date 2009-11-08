40d74b8784224cca58fa8156c4cef4f1e6b1cae0
Óscar González Fernández <ogonzalez@igalia.com>
2009-11-08 21:55:29 +0100

    ItEr33S14CUCreacionUnidadesPlanificacion: Now details screen can visualize current constraint

--- navalplanner-webapp/src/main/java/org/navalplanner/web/planner/taskedition/EditTaskController.java
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/planner/taskedition/EditTaskController.java
@@ -19,15 +19,25 @@
  */
 
 package org.navalplanner.web.planner.taskedition;
+import static org.navalplanner.web.I18nHelper._;
 
+import org.navalplanner.business.planner.entities.StartConstraintType;
 import org.navalplanner.business.planner.entities.Task;
 import org.navalplanner.business.planner.entities.TaskElement;
+import org.navalplanner.business.planner.entities.TaskStartConstraint;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.zkoss.ganttz.TaskEditFormComposer;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Comboitem;
 import org.zkoss.zul.Intbox;
+import org.zkoss.zul.api.Combobox;
+import org.zkoss.zul.api.Datebox;
+import org.zkoss.zul.api.Row;
 
 /**
  * Controller for edit {@link Task} popup.
@@ -38,17 +48,104 @@ import org.zkoss.zul.Intbox;
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class EditTaskController extends GenericForwardComposer {
 
-    private TaskElement currentTaskElement;
+    // This is a workaround, because in business we don't have access to
+    // I18nHelper
+    private enum WebStartConstraintType {
+        AS_SOON_AS_POSSIBLE(StartConstraintType.AS_SOON_AS_POSSIBLE) {
+            @Override
+            public String getDescription() {
+                return _("as soon as possible");
+            }
 
-    private Intbox hours;
+            @Override
+            public String getName() {
+                return _("AS_SOON_AS_POSSIBLE");
+            }
+        },
+        START_NOT_EARLIER_THAN(StartConstraintType.START_NOT_EARLIER_THAN) {
+            @Override
+            public String getDescription() {
+                return _("start not earlier than");
+            }
 
-    private Intbox duration;
+            @Override
+            public String getName() {
+                return _("START_NOT_EARLIER_THAN");
+            }
+        },
+        START_IN_FIXED_DATE(StartConstraintType.START_IN_FIXED_DATE) {
+            @Override
+            public String getDescription() {
+                return _("start in fixed date");
+            }
+
+            @Override
+            public String getName() {
+                return _("START_IN_FIXED_DATE");
+            }
+        };
+
+        public static void appendItems(Combobox combo) {
+            for (WebStartConstraintType type : WebStartConstraintType.values()) {
+                combo.appendChild(type.createCombo());
+            }
+        }
+
+        private final StartConstraintType type;
+
+        private WebStartConstraintType(StartConstraintType type) {
+            this.type = type;
+        }
+
+        public abstract String getName();
+
+        public abstract String getDescription();
+
+        private Comboitem createCombo() {
+            Comboitem result = new Comboitem();
+            result.setValue(this);
+            result.setLabel(this.getName());
+            result.setDescription(this.getDescription());
+            return result;
+        }
+
+        public static boolean representsType(Comboitem item,
+                StartConstraintType type) {
+            WebStartConstraintType webType = (WebStartConstraintType) item
+                    .getValue();
+            return webType.equivalentTo(type);
+        }
+
+        private boolean equivalentTo(StartConstraintType type) {
+            return this.type == type;
+        }
+
+        public boolean isAssociatedDateRequired() {
+            return type.isAssociatedDateRequired();
+        }
+
+        public StartConstraintType getType() {
+            return type;
+        }
+    }
 
     /**
      * Controller from the Gantt to manage common fields on edit {@link Task}
      * popup.
      */
     private TaskEditFormComposer taskEditFormComposer = new TaskEditFormComposer();
+
+    private TaskElement currentTaskElement;
+
+    private Intbox hours;
+
+    private Intbox duration;
+
+    private Combobox startConstraintTypes;
+
+    private Datebox startConstraintDate;
+
+    private Row startConstraint;
 
     public void showEditFormFor(Component openRelativeTo,
             org.zkoss.ganttz.data.Task task,
@@ -60,11 +157,51 @@ public class EditTaskController extends GenericForwardComposer {
 
     private void updateComponentValuesForTask() {
         if (currentTaskElement instanceof Task) {
-            showDurationRow((Task) currentTaskElement);
+            Task task = (Task) currentTaskElement;
+            showDurationRow(task);
+            showStartConstraintRow(task);
         } else {
             hideDurationRow();
+            hideStartConstraintRow();
         }
         hours.setValue(currentTaskElement.getWorkHours());
+    }
+
+    private void hideStartConstraintRow() {
+        startConstraint.setVisible(false);
+    }
+
+    private void showStartConstraintRow(Task task) {
+        startConstraint.setVisible(true);
+        StartConstraintType type = task.getStartConstraint()
+                .getStartConstraintType();
+        startConstraintTypes.setSelectedItemApi(findComboWithType(type));
+        startConstraintDate.setVisible(type.isAssociatedDateRequired());
+        startConstraintDate.setValue(task.getStartConstraint()
+                .getConstraintDate());
+    }
+
+    private Comboitem findComboWithType(StartConstraintType type) {
+        for (Object component : startConstraintTypes.getChildren()) {
+            if (component instanceof Comboitem) {
+                Comboitem item = (Comboitem) component;
+                if (WebStartConstraintType.representsType(item, type)) {
+                    return item;
+                }
+            }
+        }
+        return null;
+    }
+
+    private void constraintTypeChoosen(WebStartConstraintType constraint) {
+        startConstraintDate.setVisible(constraint.isAssociatedDateRequired());
+        TaskStartConstraint taskStartConstraint = currentTaskElementAsTask()
+                .getStartConstraint();
+        startConstraintDate.setValue(taskStartConstraint.getConstraintDate());
+    }
+
+    private Task currentTaskElementAsTask() {
+        return (Task) currentTaskElement;
     }
 
     private void hideDurationRow() {
@@ -80,5 +217,16 @@ public class EditTaskController extends GenericForwardComposer {
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
         taskEditFormComposer.doAfterCompose(comp);
+        WebStartConstraintType.appendItems(startConstraintTypes);
+        startConstraintTypes.addEventListener(Events.ON_SELECT,
+                new EventListener() {
+
+                    @Override
+                    public void onEvent(Event event) throws Exception {
+                        WebStartConstraintType constraint = (WebStartConstraintType) startConstraintTypes
+                                .getSelectedItemApi().getValue();
+                        constraintTypeChoosen(constraint);
+                    }
+                });
     }
 }
