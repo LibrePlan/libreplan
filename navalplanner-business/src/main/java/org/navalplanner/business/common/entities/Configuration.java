57cd010951edfdb2ea0c46d0f4e71802231b5a59
Manuel Rego Casasnovas <mrego@igalia.com>
2009-11-17 12:36:14 +0100

    ItEr35S08CUAsignacionCalendarioLaboralRecursoItEr25S09: Created Configuration class and set Hibernate mapping.

---
+++ navalplanner-business/src/main/java/org/navalplanner/business/common/entities/Configuration.java
@@ -0,0 +1,47 @@
+/*
+ * This file is part of ###PROJECT_NAME###
+ *
+ * Copyright (C) 2009 Fundación para o Fomento da Calidade Industrial e
+ *                    Desenvolvemento Tecnolóxico de Galicia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.navalplanner.business.common.entities;
+
+import org.navalplanner.business.calendars.entities.BaseCalendar;
+import org.navalplanner.business.common.BaseEntity;
+
+/**
+ * Application configuration variables.
+ *
+ * @author Manuel Rego Casasnovas <mrego@igalia.com>
+ */
+public class Configuration extends BaseEntity {
+
+    public static Configuration create() {
+        return create(new Configuration());
+    }
+
+    private BaseCalendar defaultCalendar;
+
+    public void setDefaultCalendar(BaseCalendar defaultCalendar) {
+        this.defaultCalendar = defaultCalendar;
+    }
+
+    public BaseCalendar getDefaultCalendar() {
+        return defaultCalendar;
+    }
+
+}
