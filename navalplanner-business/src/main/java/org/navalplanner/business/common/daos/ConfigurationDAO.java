8cde179bb3de0bbe09ebe24351c42464e7236dc1
Manuel Rego Casasnovas <mrego@igalia.com>
2009-11-17 12:36:15 +0100

    ItEr35S08CUAsignacionCalendarioLaboralRecursoItEr25S09: Added DAO for Configuration entity.

---
+++ navalplanner-business/src/main/java/org/navalplanner/business/common/daos/ConfigurationDAO.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of ###PROJECT_NAME###
+ *
+ * Copyright (C) 2009 Fundación para o Fomento da Calidade Industrial e
+ *                    Desenvolvemento Tecnolóxico de Galicia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.navalplanner.business.common.daos;
+
+import java.util.List;
+
+import org.navalplanner.business.common.entities.Configuration;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+
+/**
+ * DAO for {@link Configuration}.
+ *
+ * @author Manuel Rego Casasnovas <mrego@igalia.com>
+ */
+@Repository
+@Scope(BeanDefinition.SCOPE_SINGLETON)
+public class ConfigurationDAO extends GenericDAOHibernate<Configuration, Long>
+        implements IConfigurationDAO {
+
+    @Override
+    public Configuration getConfiguration() {
+        List<Configuration> list = list(Configuration.class);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+}
