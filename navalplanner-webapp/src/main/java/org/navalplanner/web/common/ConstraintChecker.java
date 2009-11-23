ff45f1cc418ca1a4de1f07edbca9f7a0707ac9eb
Diego Pino Garcia <dpino@igalia.com>
2009-11-23 10:47:05 +0100

    ItEr35S14CUAdministracionMateriaisItEr34S14: Added ConstraintChecker class for validating all constraints (component + children) of a component

---
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/common/ConstraintChecker.java
@@ -0,0 +1,69 @@
+/*
+ * This file is part of ###PROJECT_NAME###
+ *
+ * Copyright (C) 2009 Fundación para o Fomento da Calidade Industrial e
+ *                    Desenvolvemento Tecnolóxico de Galicia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.navalplanner.web.common;
+
+import java.util.List;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zul.Datebox;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.impl.api.InputElement;
+
+/**
+ * Class for checking if a component is completely valid (checks all constraints within a component)
+ *
+ * @author Diego Pino García <dpino@igalia.com>
+ */
+public class ConstraintChecker {
+
+    @SuppressWarnings("unchecked")
+    public static boolean isValid(Component component) {
+        return checkIsValid(component) && checkIsValid(component.getChildren());
+    }
+
+    private static boolean checkIsValid(List<Component> components) {
+        boolean result = true;
+        for (Component component: components) {
+            result &= isValid(component);
+        }
+        return result;
+    }
+
+    private static boolean checkIsValid(Component child) {
+        if (child instanceof Textbox) {
+            return textboxIsValid((Textbox) child);
+        }
+        if (child instanceof Datebox) {
+            return dateboxIsValid((Datebox) child);
+        }
+        return true;
+    }
+
+    private static boolean textboxIsValid(Textbox component) {
+        component.getValue();            // Forces constraint to be checked
+        return (component.isValid());
+    }
+
+    private static boolean dateboxIsValid(Datebox component) {
+        component.getValue();
+        return (component.isValid());
+    }
+}
