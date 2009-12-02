ff2e2c5b0cc81d544e02f8fa32c2f81b29513e7b
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2009-12-02 20:58:44 +0100

    ItEr37S12CUAdministracionCategoriaCosteItEr36S14: basic implementation of the entity ResourcesCostCategoryAssignment
    Implemented the entity, its DAO and a basic test.

---
+++ navalplanner-business/src/test/java/org/navalplanner/business/test/costcategories/daos/ResourcesCostCategoryAssignmentDAOTest.java
@@ -0,0 +1,92 @@
+/*
+ * This file is part of ###PROJECT_NAME###
+ *
+ * Copyright (C) 2009 Fundación para o Fomento da Calidade Industrial e
+ *                    Desenvolvemento Tecnolóxico de Galicia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.navalplanner.business.test.costcategories.daos;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.navalplanner.business.BusinessGlobalNames.BUSINESS_SPRING_CONFIG_FILE;
+import static org.navalplanner.business.test.BusinessGlobalNames.BUSINESS_SPRING_CONFIG_TEST_FILE;
+
+import java.util.List;
+
+import org.joda.time.LocalDate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.navalplanner.business.common.exceptions.InstanceNotFoundException;
+import org.navalplanner.business.costcategories.daos.IResourcesCostCategoryAssignmentDAO;
+import org.navalplanner.business.costcategories.entities.ResourcesCostCategoryAssignment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { BUSINESS_SPRING_CONFIG_FILE,
+        BUSINESS_SPRING_CONFIG_TEST_FILE })
+/**
+ * Test for {@ResourcesCostCategoryDAO}
+ *
+ * @author Jacobo Aragunde Perez <jaragunde@igalia.com>
+ *
+ */
+@Transactional
+public class ResourcesCostCategoryAssignmentDAOTest {
+
+    @Autowired
+    IResourcesCostCategoryAssignmentDAO resourcesCostCategoryAssignmentDAO;
+
+    @Test
+    public void testInSpringContainer() {
+        assertNotNull(resourcesCostCategoryAssignmentDAO);
+    }
+
+    private ResourcesCostCategoryAssignment createValidResourcesCostCategoryAssignment() {
+        ResourcesCostCategoryAssignment assignment = ResourcesCostCategoryAssignment.create();
+        assignment.setInitDate(new LocalDate());
+        return assignment;
+    }
+
+    @Test
+    public void testSaveResourcesCostCategoryAssignment() {
+        ResourcesCostCategoryAssignment assignment = createValidResourcesCostCategoryAssignment();
+        resourcesCostCategoryAssignmentDAO.save(assignment);
+        assertTrue(assignment.getId() != null);
+    }
+
+    @Test
+    public void testRemoveResourcesCostCategoryAssignment() throws InstanceNotFoundException {
+        ResourcesCostCategoryAssignment assignment = createValidResourcesCostCategoryAssignment();
+        resourcesCostCategoryAssignmentDAO.save(assignment);
+        resourcesCostCategoryAssignmentDAO.remove(assignment.getId());
+        assertFalse(resourcesCostCategoryAssignmentDAO.exists(assignment.getId()));
+    }
+
+    @Test
+    public void testListResourcesCostCategoryAssignments() {
+        int previous = resourcesCostCategoryAssignmentDAO.list(ResourcesCostCategoryAssignment.class).size();
+        ResourcesCostCategoryAssignment assignment = createValidResourcesCostCategoryAssignment();
+        resourcesCostCategoryAssignmentDAO.save(assignment);
+        List<ResourcesCostCategoryAssignment> list = resourcesCostCategoryAssignmentDAO.list(ResourcesCostCategoryAssignment.class);
+        assertEquals(previous + 1, list.size());
+    }
+}
