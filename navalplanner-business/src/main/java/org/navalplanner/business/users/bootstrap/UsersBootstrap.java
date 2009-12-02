e36913458620348ea2206fc3689135ce5f6e6c3e
Fernando Bellas Permuy <fbellas@udc.es>
2009-12-02 17:32:38 +0100

    ItEr37S07ArquitecturaServidorItEr36S10: Removed explicit service bean name.
    Removed explicit service bean name.

--- navalplanner-business/src/main/java/org/navalplanner/business/users/bootstrap/UsersBootstrap.java
+++ navalplanner-business/src/main/java/org/navalplanner/business/users/bootstrap/UsersBootstrap.java
@@ -33,7 +33,7 @@ import org.springframework.transaction.annotation.Transactional;
 /**
  * @author Fernando Bellas Permuy <fbellas@udc.es>
  */
-@Service("usersBootstrap")
+@Service
 @Transactional
 public class UsersBootstrap implements IUsersBootstrap {
 
