311b943c10027a47edd500ae03e10821d129f63b
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2010-01-04 15:58:20 +0100

    ItEr41S25CUAltaSubcontrataItEr30S08: Entity and DAO for ExternalCompany.
    A basic unit test has been written too.
    The attributes name and cif are compulsory and unique. The unique constraint
    has been set in the Hibernate mapping but not yet in the entity. The relation
    with User is not implemented either.

---
+++ navalplanner-business/src/test/java/org/navalplanner/business/test/externalcompanies/daos/ExternalCompanyDAOTest.java
@@ -0,0 +1,89 @@
+/*
+ * This file is part of ###PROJECT_NAME###
+ *
+ * Copyright (C) 2009 Fundación para o Fomento da Calidade Industrial e
+ *                    Desenvolvemento Tecnolóxico de Galicia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.navalplanner.business.test.externalcompanies.daos;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.navalplanner.business.BusinessGlobalNames.BUSINESS_SPRING_CONFIG_FILE;
+import static org.navalplanner.business.test.BusinessGlobalNames.BUSINESS_SPRING_CONFIG_TEST_FILE;
+
+import java.util.UUID;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.navalplanner.business.common.exceptions.InstanceNotFoundException;
+import org.navalplanner.business.externalcompanies.daos.IExternalCompanyDAO;
+import org.navalplanner.business.externalcompanies.entities.ExternalCompany;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { BUSINESS_SPRING_CONFIG_FILE,
+        BUSINESS_SPRING_CONFIG_TEST_FILE })
+/**
+ * Test for {@link ExternalCompanyDAO}
+ *
+ * @author Jacobo Aragunde Perez <jaragunde@igalia.com>
+ *
+ */
+@Transactional
+public class ExternalCompanyDAOTest {
+
+    @Autowired
+    IExternalCompanyDAO externalCompanyDAO;
+
+    @Test
+    public void testInSpringContainer() {
+        assertNotNull(externalCompanyDAO);
+    }
+
+    @Test
+    public void testSaveExternalCompany() {
+        ExternalCompany externalCompany = createValidExternalCompany();
+        externalCompanyDAO.save(externalCompany);
+        assertTrue(externalCompany.getId() != null);
+    }
+
+    @Test
+    public void testRemoveExternalCompany() throws InstanceNotFoundException {
+        ExternalCompany externalCompany = createValidExternalCompany();
+        externalCompanyDAO.save(externalCompany);
+        externalCompanyDAO.remove(externalCompany.getId());
+        assertFalse(externalCompanyDAO.exists(externalCompany.getId()));
+    }
+
+    @Test
+    public void testListExternalCompanies() {
+        int previous = externalCompanyDAO.list(ExternalCompany.class).size();
+        ExternalCompany externalCompany = createValidExternalCompany();
+        externalCompanyDAO.save(externalCompany);
+        assertEquals(previous + 1, externalCompanyDAO.list(ExternalCompany.class).size());
+    }
+
+    private ExternalCompany createValidExternalCompany() {
+        return ExternalCompany.create(UUID.randomUUID().toString(),
+                UUID.randomUUID().toString());
+    }
+}
