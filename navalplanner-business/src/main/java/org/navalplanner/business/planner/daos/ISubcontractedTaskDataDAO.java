a7b72fc07b8332c549fe6f5b4d0c3a7d98e5a965
Manuel Rego Casasnovas <mrego@igalia.com>
2010-01-10 12:36:03 +0100

    ItEr42S25CUMarcarUnidadeTraballoExportable: Created DAO for entity SubcontractedTaskData.

---
+++ navalplanner-business/src/main/java/org/navalplanner/business/planner/daos/ISubcontractedTaskDataDAO.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of ###PROJECT_NAME###
+ *
+ * Copyright (C) 2009 Fundación para o Fomento da Calidade Industrial e
+ *                    Desenvolvemento Tecnolóxico de Galicia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.navalplanner.business.planner.daos;
+
+import org.navalplanner.business.common.daos.IGenericDAO;
+import org.navalplanner.business.planner.entities.SubcontractedTaskData;
+
+/**
+ * DAO interface for the {@link SubcontractedTaskDataDAO} entity.
+ *
+ * @author Manuel Rego Casasnovas <mrego@igalia.com>
+ */
+public interface ISubcontractedTaskDataDAO extends
+        IGenericDAO<SubcontractedTaskData, Long> {
+
+}
