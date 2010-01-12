11c4b659b8c7d58934734da5d54aff395263af0a
Óscar González Fernández <ogonzalez@igalia.com>
2010-01-12 17:53:40 +0100

    ItEr43S12CUGravacionModelosUnidadesTraballoItEr42S17: Extracting superclass from AssignedLabelsToOrderElementModel.

---
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/orders/labels/AssignedLabelsModel.java
@@ -0,0 +1,187 @@
+/*
+ * This file is part of ###PROJECT_NAME###
+ *
+ * Copyright (C) 2009 Fundación para o Fomento da Calidade Industrial e
+ *                    Desenvolvemento Tecnolóxico de Galicia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.navalplanner.web.orders.labels;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.navalplanner.business.labels.daos.ILabelDAO;
+import org.navalplanner.business.labels.entities.Label;
+import org.navalplanner.business.labels.entities.LabelType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Óscar González Fernández <ogonzalez@igalia.com>
+ *
+ */
+public abstract class AssignedLabelsModel<T> implements IAssignedLabelsModel<T> {
+
+    @Autowired
+    private ILabelDAO labelDAO;
+
+    private T element;
+
+    @Transactional(readOnly = true)
+    public void init(T element) {
+        this.element = element;
+        initializeElementAndChildren(this.element);
+    }
+
+    private void initializeElementAndChildren(T element) {
+        reattachLabels();
+        initialize(element);
+        T current = getParent(element);
+        while (current != null) {
+            initialize(current);
+            current = getParent(current);
+        }
+        initializeChildren(element);
+    }
+
+    protected abstract T getParent(T element);
+
+    private void initializeChildren(T element) {
+        initialize(element);
+        for (T child : getChildren(element)) {
+            initializeChildren(child);
+        }
+    }
+
+    protected abstract List<T> getChildren(T element);
+
+    private void initialize(T orderElement) {
+        reattach(orderElement);
+        initializeLabels(getLabels(orderElement));
+    }
+
+    protected abstract List<Label> getLabels(T orderElement);
+
+    protected abstract void reattach(T element);
+
+    private void reattachLabels() {
+        for (Label label : getLabelsOnConversation()) {
+            labelDAO.reattach(label);
+        }
+    }
+
+    protected abstract List<Label> getLabelsOnConversation();
+
+    private void initializeLabels(Collection<Label> labels) {
+        for (Label label : labels) {
+            initializeLabel(label);
+        }
+    }
+
+    public void initializeLabel(Label label) {
+        label.getName();
+        label.getType().getName();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Label> getLabels() {
+        List<Label> result = new ArrayList<Label>();
+        if (element != null && getLabels(element) != null) {
+            reattachLabels();
+            result.addAll(getLabels(element));
+        }
+        return result;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Label> getInheritedLabels() {
+        List<Label> result = new ArrayList<Label>();
+        if (element != null) {
+            reattachLabels();
+            T parent = getParent(element);
+            while (parent != null) {
+                result.addAll(getLabels(parent));
+                parent = getParent(parent);
+            }
+        }
+        return result;
+    }
+
+    public Label createLabel(String labelName, LabelType labelType) {
+        Label label = Label.create(labelName);
+        label.setType(labelType);
+        addLabelToConversation(label);
+        return label;
+    }
+
+    protected abstract void addLabelToConversation(Label label);
+
+    @Transactional(readOnly = true)
+    public void assignLabel(Label label) {
+        reattachLabels();
+        addLabelToElement(element, label);
+    }
+
+    protected abstract void addLabelToElement(T element, Label label);
+
+    @Transactional(readOnly = true)
+    public void deleteLabel(Label label) {
+        reattachLabels();
+        removeLabel(element, label);
+    }
+
+    protected abstract void removeLabel(T element, Label label);
+
+    @Transactional(readOnly = true)
+    public Label findLabelByNameAndType(String labelName, LabelType labelType) {
+        Label label = findLabelByNameAndTypeName(labelName, labelType.getName());
+        if (label != null) {
+            initializeLabel(label);
+        }
+        return label;
+    }
+
+    /**
+     * Search {@link Label} by name and type in cache of labels
+     * @param labelName
+     * @param labelTypeName
+     * @return
+     */
+    private Label findLabelByNameAndTypeName(String labelName,
+            String labelTypeName) {
+        for (Label label : getLabelsOnConversation()) {
+            if (label.getName().equals(labelName)
+                    && label.getType().getName().equals(labelTypeName)) {
+                return label;
+            }
+        }
+        return null;
+    }
+
+    public boolean isAssigned(Label label) {
+        for (Label each : getLabels(element)) {
+            if (each.getId().equals(label.getId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Label> getAllLabels() {
+        return getLabelsOnConversation();
+    }
+
+}
--- navalplanner-webapp/src/main/java/org/navalplanner/web/orders/labels/AssignedLabelsToOrderElementModel.java
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/orders/labels/AssignedLabelsToOrderElementModel.java
@@ -21,114 +21,46 @@
 package org.navalplanner.web.orders.labels;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
-import org.navalplanner.business.labels.daos.ILabelDAO;
 import org.navalplanner.business.labels.entities.Label;
 import org.navalplanner.business.labels.entities.LabelType;
 import org.navalplanner.business.orders.daos.IOrderElementDAO;
 import org.navalplanner.business.orders.entities.OrderElement;
-import org.navalplanner.business.orders.entities.OrderLineGroup;
 import org.navalplanner.web.orders.IOrderModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Diego Pino Garcia <dpino@igalia.com>
  */
 @Service
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
-public class AssignedLabelsToOrderElementModel implements
+public class AssignedLabelsToOrderElementModel extends
+        AssignedLabelsModel<OrderElement> implements
         IAssignedLabelsToOrderElementModel {
 
     @Autowired
     private IOrderElementDAO orderDAO;
 
-    @Autowired
-    private ILabelDAO labelDAO;
-
-    private OrderElement orderElement;
-
     private IOrderModel orderModel;
 
     @Override
-    @Transactional(readOnly = true)
-    public void init(OrderElement orderElement) {
-        this.orderElement = orderElement;
-        initializeOrderElement(this.orderElement);
-    }
-
-    private void initializeOrderElement(OrderElement orderElement) {
-        reattachLabels();
-        initialize(orderElement);
-
-        OrderElement parent = orderElement.getParent();
-        while (parent != null) {
-            initialize(parent);
-            parent = parent.getParent();
-        }
-
-        initializeChildren(orderElement);
-    }
-
-    private void initializeChildren(OrderElement orderElement) {
-        initialize(orderElement);
-        for (OrderElement child : orderElement.getChildren()) {
-            initializeChildren(child);
-        }
-    }
-
-    private void initialize(OrderElement orderElement) {
-        orderDAO.reattach(orderElement);
-        orderElement.getName();
-        initializeLabels(orderElement.getLabels());
-    }
-
-    private void reattachLabels() {
-        for (Label label : orderModel.getLabels()) {
-            labelDAO.reattach(label);
-        }
-    }
-
-    private void initializeLabels(Collection<Label> labels) {
-        for (Label label : labels) {
-            initializeLabel(label);
-        }
-    }
-
-    public void initializeLabel(Label label) {
-        label.getName();
-        label.getType().getName();
-    }
-
-    @Transactional(readOnly = true)
-    public List<Label> getLabels() {
-        List<Label> result = new ArrayList<Label>();
-        if (orderElement != null && orderElement.getLabels() != null) {
-            reattachLabels();
-            result.addAll(orderElement.getLabels());
-        }
-        return result;
+    protected OrderElement getParent(OrderElement element) {
+        return element.getParent();
     }
 
     @Override
-    @Transactional(readOnly = true)
-    public List<Label> getInheritedLabels() {
-        List<Label> result = new ArrayList<Label>();
-        if (orderElement != null) {
-            reattachLabels();
-            OrderLineGroup parent = orderElement.getParent();
-            while (parent != null) {
-                result.addAll(parent.getLabels());
-                parent = parent.getParent();
-            }
-        }
-        return result;
+    protected List<OrderElement> getChildren(OrderElement element) {
+        return element.getChildren();
+    }
+
+    @Override
+    protected List<Label> getLabels(OrderElement orderElement) {
+        return new ArrayList<Label>(orderElement.getLabels());
     }
 
     @Override
@@ -140,67 +72,37 @@ public class AssignedLabelsToOrderElementModel implements
     }
 
     @Override
-    @Transactional(readOnly = true)
-    public void assignLabel(Label label) {
-        reattachLabels();
-        orderElement.addLabel(label);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public void deleteLabel(Label label) {
-        reattachLabels();
-        orderElement.removeLabel(label);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public Label findLabelByNameAndType(String labelName, LabelType labelType) {
-        Label label = findLabelByNameAndTypeName(labelName, labelType.getName());
-        if (label != null) {
-            initializeLabel(label);
-        }
-        return label;
-    }
-
-    /**
-     * Search {@link Label} by name and type in cache of labels
-     *
-     * @param labelName
-     * @param labelTypeName
-     * @return
-     */
-    private Label findLabelByNameAndTypeName(String labelName,
-            String labelTypeName) {
-        for (Label label : orderModel.getLabels()) {
-            if (label.getName().equals(labelName)
-                    && label.getType().getName().equals(labelTypeName)) {
-                return label;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public boolean isAssigned(Label label) {
-        final Set<Label> labels = orderElement.getLabels();
-
-        for (Label element : labels) {
-            if (element.getId().equals(label.getId())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
     public void setOrderModel(IOrderModel orderModel) {
         this.orderModel = orderModel;
     }
 
     @Override
-    public List<Label> getAllLabels() {
-        return (orderModel != null) ? orderModel.getLabels() : new ArrayList<Label>();
+    protected void addLabelToConversation(Label label) {
+        orderModel.addLabel(label);
+    }
+
+    @Override
+    protected void addLabelToElement(OrderElement element, Label label) {
+        element.addLabel(label);
+    }
+
+    @Override
+    protected List<Label> getLabelsOnConversation() {
+        if (orderModel == null) {
+            return Collections.emptyList();
+        }
+        return orderModel.getLabels();
+    }
+
+    @Override
+    protected void reattach(OrderElement element) {
+        orderDAO.reattach(element);
+        element.getName();
+    }
+
+    @Override
+    protected void removeLabel(OrderElement element, Label label) {
+        element.removeLabel(label);
     }
 
 }
