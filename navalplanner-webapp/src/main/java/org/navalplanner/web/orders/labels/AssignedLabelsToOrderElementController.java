680cada70670d2799182453bde2820c3464cb166
Óscar González Fernández <ogonzalez@igalia.com>
2010-01-12 18:28:48 +0100

    ItEr43S12CUGravacionModelosUnidadesTraballoItEr42S17: Extracting superclass from AssignedLabelsToOrderElementController

--- navalplanner-webapp/src/main/java/org/navalplanner/web/orders/labels/AssignedLabelsToOrderElementController.java
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/orders/labels/AssignedLabelsToOrderElementController.java
@@ -20,24 +20,9 @@
 
 package org.navalplanner.web.orders.labels;
 
-import static org.navalplanner.web.I18nHelper._;
-
-import java.util.List;
-
-import org.navalplanner.business.labels.entities.Label;
-import org.navalplanner.business.labels.entities.LabelType;
 import org.navalplanner.business.orders.entities.OrderElement;
-import org.navalplanner.web.common.Util;
-import org.navalplanner.web.common.components.Autocomplete;
-import org.navalplanner.web.common.components.bandboxsearch.BandboxSearch;
 import org.navalplanner.web.orders.IOrderElementModel;
 import org.navalplanner.web.orders.IOrderModel;
-import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.WrongValueException;
-import org.zkoss.zk.ui.util.GenericForwardComposer;
-import org.zkoss.zul.Comboitem;
-import org.zkoss.zul.Grid;
-import org.zkoss.zul.Textbox;
 
 /**
  * Controller for showing OrderElement assigned labels
@@ -46,44 +31,23 @@ import org.zkoss.zul.Textbox;
  *
  */
 public class AssignedLabelsToOrderElementController extends
-        GenericForwardComposer {
+        AssignedLabelsController<OrderElement, IOrderElementModel> {
 
     private IAssignedLabelsToOrderElementModel assignedLabelsToOrderElementModel;
 
-    private Autocomplete cbLabelType;
-
-    private Grid directLabels;
-
-    private Textbox txtLabelName;
-
-    private BandboxSearch bdLabels;
-
-    @Override
-    public void doAfterCompose(Component comp) throws Exception {
-        super.doAfterCompose(comp);
-    }
-
-    public void openWindow(IOrderElementModel orderElementModel) {
-        setOrderElementModel(orderElementModel);
-        openWindow(getOrderElement());
-    }
-
-    private void openWindow(OrderElement orderElement) {
-        assignedLabelsToOrderElementModel.init(orderElement);
-
-        // Clear components
-        bdLabels.clear();
-        txtLabelName.setValue("");
-
-        Util.reloadBindings(self);
-        Util.reloadBindings(directLabels);
-    }
 
     IOrderElementModel orderElementModel;
 
-    private void setOrderElementModel(IOrderElementModel orderElementModel) {
+    @Override
+    protected IAssignedLabelsToOrderElementModel getModel() {
+        return assignedLabelsToOrderElementModel;
+    }
+
+    @Override
+    protected void setOuterModel(IOrderElementModel orderElementModel) {
         this.orderElementModel = orderElementModel;
         setOrderModel(orderElementModel.getOrderModel());
+
     }
 
     private void setOrderModel(IOrderModel orderModel) {
@@ -96,117 +60,8 @@ public class AssignedLabelsToOrderElementController extends
         return orderElementModel.getOrderElement();
     }
 
-    /**
-     * Executed on pressing Assign button
-     *
-     * Adds selected label to direct labels list
-     *
-     */
-    public void onAssignLabel() {
-        Label label = (Label) bdLabels.getSelectedElement();
-        if (label == null) {
-            throw new WrongValueException(bdLabels, _("please, select a label"));
-        }
-        if (isAssigned(label)) {
-            throw new WrongValueException(bdLabels, _("already assigned"));
-        }
-        try {
-            assignLabel(label);
-        } catch (IllegalArgumentException e) {
-            throw new WrongValueException(bdLabels, e.getMessage());
-        }
-        bdLabels.clear();
-    }
-
-    /**
-     * Executed on pressing createAndAssign button
-     *
-     * Creates a new label for a type, in case it does not exist, and added it
-     * to the list of direct labels
-     *
-     */
-    public void onCreateAndAssign() {
-        // Check LabelType is not null
-        final Comboitem comboitem = cbLabelType.getSelectedItem();
-        if (comboitem == null || comboitem.getValue() == null) {
-            throw new WrongValueException(cbLabelType,
-                    _("please, select an item"));
-        }
-
-        // Check Label is not null or empty
-        final String labelName = txtLabelName.getValue();
-        if (labelName == null || labelName.isEmpty()) {
-            throw new WrongValueException(txtLabelName,
-                    _("cannot be null or empty"));
-        }
-
-        // Label does not exist, create
-        final LabelType labelType = (LabelType) comboitem.getValue();
-        Label label = assignedLabelsToOrderElementModel
-                .findLabelByNameAndType(labelName, labelType);
-        if (label == null) {
-            label = addLabel(labelName, labelType);
-        } else {
-            // Label is already assigned?
-            if (isAssigned(label)) {
-                throw new WrongValueException(txtLabelName,
-                        _("already assigned"));
-            }
-        }
-        try {
-            assignLabel(label);
-        } catch (IllegalArgumentException e) {
-            throw new WrongValueException(txtLabelName, e.getMessage());
-        }
-        clear(txtLabelName);
-    }
-
-    private Label addLabel(String labelName, LabelType labelType) {
-        Label label = createLabel(labelName, labelType);
-        bdLabels.addElement(label);
-        return label;
-    }
-
-    private Label createLabel(String labelName, LabelType labelType) {
-        return assignedLabelsToOrderElementModel.createLabel(labelName,
-                labelType);
-    }
-
-    private void clear(Textbox textbox) {
-        textbox.setValue("");
-    }
-
-    public void clear() {
-
-    }
-
-    private void assignLabel(Label label) {
-        assignedLabelsToOrderElementModel.assignLabel(label);
-        Util.reloadBindings(directLabels);
-    }
-
-    private boolean isAssigned(Label label) {
-        return assignedLabelsToOrderElementModel.isAssigned(label);
-    }
-
-    public void deleteLabel(Label label) {
-        assignedLabelsToOrderElementModel.deleteLabel(label);
-        Util.reloadBindings(directLabels);
-    }
-
-    public List<Label> getLabels() {
-        return assignedLabelsToOrderElementModel.getLabels();
-    }
-
-    public List<Label> getInheritedLabels() {
-        return assignedLabelsToOrderElementModel.getInheritedLabels();
-    }
-
-    public List<Label> getAllLabels() {
-        return assignedLabelsToOrderElementModel.getAllLabels();
-    }
-
-    public void close() {
-
+    @Override
+    protected OrderElement getElement() {
+        return getOrderElement();
     }
 }
