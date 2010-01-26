85750fab048117a7492bc7bd686c7eaf7feb9489
Manuel Rego Casasnovas <mrego@igalia.com>
2010-01-26 21:51:55 +0100

    ItEr45S18CUImportacionPartesTrabajo: Created basic files for work reports service.

---
+++ navalplanner-webapp/src/main/java/org/navalplanner/ws/workreports/impl/WorkReportServiceREST.java
@@ -0,0 +1,39 @@
+/*
+ * This file is part of ###PROJECT_NAME###
+ *
+ * Copyright (C) 2009 Fundación para o Fomento da Calidade Industrial e
+ *                    Desenvolvemento Tecnolóxico de Galicia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.navalplanner.ws.workreports.impl;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.navalplanner.ws.workreports.api.IWorkReportService;
+import org.springframework.stereotype.Service;
+
+/**
+ * REST-based implementation of {@link IWorkReportService}.
+ *
+ * @author Manuel Rego Casasnovas <mrego@igalia.com>
+ */
+@Path("/workreports/")
+@Produces("application/xml")
+@Service("workReportServiceREST")
+public class WorkReportServiceREST implements IWorkReportService {
+
+}
