b900f91522b9709edcd2b9063cd47582a822af93
Óscar González Fernández <ogonzalez@igalia.com>
2010-04-19 21:30:21 +0200

    ItEr55S08RFInfraestucturaEscenariosItEr54S10: Add DeepCopy class that clones objects based on annotation metadata

---
+++ navalplanner-business/src/main/java/org/navalplanner/business/util/deepcopy/DeepCopy.java
@@ -0,0 +1,376 @@
+/*
+ * This file is part of NavalPlan
+ *
+ * Copyright (C) 2009 Fundación para o Fomento da Calidade Industrial e
+ *                    Desenvolvemento Tecnolóxico de Galicia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.navalplanner.business.util.deepcopy;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+
+/**
+ * @author Óscar González Fernández <ogonzalez@igalia.com>
+ *
+ */
+public class DeepCopy {
+    private static Set<Class<?>> inmmutableTypes = new HashSet<Class<?>>(Arrays
+            .<Class<?>> asList(Boolean.class, String.class, BigDecimal.class,
+                    Double.class, Float.class, Integer.class, Short.class,
+                    Byte.class, Character.class, LocalDate.class,
+                    DateTime.class));
+
+    public static boolean isImmutableType(Class<?> klass) {
+        return klass.isPrimitive() || isEnum(klass)
+                || inmmutableTypes.contains(klass);
+    }
+
+    private static boolean isEnum(Class<?> klass) {
+        Class<?> currentClass = klass;
+        do {
+            if (currentClass.isEnum()) {
+                return true;
+            }
+            currentClass = currentClass.getSuperclass();
+        } while (currentClass != null);
+        return false;
+    }
+
+    public interface ICustomCopy {
+        public boolean canHandle(Object object);
+
+        public Object instantiateCopy(Strategy strategy, Object originValue);
+
+        public void copyDataToResult(DeepCopy deepCopy, Object origin,
+                Strategy strategy, Object result);
+    }
+
+    private static class DateCopy implements ICustomCopy {
+
+        @Override
+        public boolean canHandle(Object object) {
+            return object instanceof Date;
+        }
+
+        @Override
+        public void copyDataToResult(DeepCopy deepCopy, Object origin,
+                Strategy strategy, Object result) {
+            // already completed
+        }
+
+        @Override
+        public Object instantiateCopy(Strategy strategy, Object originValue) {
+            Date date = (Date) originValue;
+            return new Date(date.getTime());
+        }
+
+    }
+
+    private static abstract class CollectionCopy implements ICustomCopy {
+
+        @Override
+        public Object instantiateCopy(Strategy strategy, Object originValue) {
+            return getResultData(originValue);
+        }
+
+        protected abstract Collection<Object> getResultData(Object originValue);
+
+        @Override
+        public void copyDataToResult(DeepCopy deepCopy, Object origin,
+                Strategy strategy, Object result) {
+            copy(deepCopy, origin, strategy, (Collection<Object>) result);
+        }
+
+        private void copy(DeepCopy deepCopy, Object origin, Strategy strategy,
+                Collection<Object> destination) {
+            Strategy childrenStrategy = getChildrenStrategy(strategy);
+            for (Object each : originDataAsIterable(origin)) {
+                destination.add(deepCopy.copy(each, childrenStrategy));
+            }
+        }
+
+        private Strategy getChildrenStrategy(Strategy strategy) {
+            if (strategy == Strategy.SHARE_COLLECTION_ELEMENTS) {
+                return Strategy.SHARE;
+            }
+            return strategy;
+        }
+
+        private Iterable<Object> originDataAsIterable(Object originValue) {
+            return (Iterable<Object>) originValue;
+        }
+    }
+
+    private static class SetCopy extends CollectionCopy {
+
+        @Override
+        public boolean canHandle(Object object) {
+            return object instanceof Set;
+        }
+
+        @Override
+        protected Collection<Object> getResultData(Object object) {
+            return instantiate(object.getClass());
+        }
+
+        private Set<Object> instantiate(Class<? extends Object> klass) {
+            return new ImplementationInstantiation() {
+                @Override
+                protected Set<?> createDefault() {
+                    return new HashSet<Object>();
+                }
+            }.instantiate(klass);
+        }
+    }
+
+    private static class MapCopy implements ICustomCopy {
+        @Override
+        public boolean canHandle(Object object) {
+            return object instanceof Map;
+        }
+
+        @Override
+        public Object instantiateCopy(Strategy strategy, Object originValue) {
+            return instantiate(originValue.getClass());
+        }
+
+        private Map<Object, Object> instantiate(Class<? extends Object> klass) {
+            return new ImplementationInstantiation() {
+                @Override
+                protected Object createDefault() {
+                    return new HashMap<Object, Object>();
+                }
+            }.instantiate(klass);
+        }
+
+        @Override
+        public void copyDataToResult(DeepCopy deepCopy, Object origin,
+                Strategy strategy, Object result) {
+            doCopy(deepCopy, (Map<?, ?>) ((Map<?, ?>) origin), strategy, ((Map<Object, Object>) result));
+        }
+
+        private void doCopy(DeepCopy deepCopy, Map<?, ?> origin,
+                Strategy strategy, Map<Object, Object> resultMap) {
+            Strategy keyStrategy = getKeysStrategy(strategy);
+            Strategy valueStrategy = getValuesStrategy(strategy);
+            for (Entry<?, ?> entry : origin.entrySet()) {
+                Object key = deepCopy.copy(entry.getKey(), keyStrategy);
+                Object value = deepCopy.copy(entry.getValue(), valueStrategy);
+                resultMap.put(key, value);
+            }
+        }
+
+        private Strategy getKeysStrategy(Strategy strategy) {
+            if (Strategy.ONLY_SHARE_KEYS == strategy
+                    || Strategy.SHARE_COLLECTION_ELEMENTS == strategy) {
+                return Strategy.SHARE;
+            }
+            return strategy;
+        }
+
+        private Strategy getValuesStrategy(Strategy strategy) {
+            if (Strategy.ONLY_SHARE_VALUES == strategy
+                    || Strategy.SHARE_COLLECTION_ELEMENTS == strategy) {
+                return Strategy.SHARE;
+            }
+            return strategy;
+        }
+    }
+
+    private static class ListCopy extends CollectionCopy {
+
+        @Override
+        public boolean canHandle(Object object) {
+            return object instanceof List;
+        }
+
+        @Override
+        protected Collection<Object> getResultData(Object originValue) {
+            return instantiate(originValue.getClass());
+        }
+
+        private List<Object> instantiate(Class<? extends Object> klass) {
+            return new ImplementationInstantiation() {
+                @Override
+                protected Object createDefault() {
+                    return new ArrayList<Object>();
+                }
+            }.instantiate(klass);
+        }
+
+    }
+
+    private static abstract class ImplementationInstantiation {
+
+        ImplementationInstantiation() {
+        }
+
+        <T> T instantiate(Class<?> type) {
+            try {
+                Constructor<? extends Object> constructor = type
+                        .getConstructor();
+                return (T) type.cast(constructor.newInstance());
+            } catch (Exception e) {
+                return (T) createDefault();
+            }
+        }
+
+        protected abstract Object createDefault();
+    }
+
+    private static List<ICustomCopy> DEFAULT_CUSTOM_COPIERS = Arrays
+            .<ICustomCopy> asList(new DateCopy(), new SetCopy(), new MapCopy(),
+                    new ListCopy());
+
+    private Map<Object, Object> alreadyCopiedObjects = new HashMap<Object, Object>();
+
+
+    public <T> T copy(T entity) {
+        return copy(entity, null);
+    }
+
+    private <T> T copy(T value, Strategy strategy) {
+        if (value == null) {
+            return null;
+        }
+        if (alreadyCopiedObjects.containsKey(value)) {
+            return (T) alreadyCopiedObjects.get(value);
+        }
+        if (Strategy.SHARE == strategy || isImmutable(value)) {
+            return value;
+        }
+        ICustomCopy copier = findCopier(value);
+        if (copier != null) {
+            Object resultData = copier.instantiateCopy(strategy, value);
+            alreadyCopiedObjects.put(value, resultData);
+            copier.copyDataToResult(this, value, strategy, resultData);
+            return (T) resultData;
+        }
+        T result = instantiateUsingDefaultConstructor(getTypedClassFrom(value));
+        alreadyCopiedObjects.put(value, result);
+        copyProperties(value, result);
+        return result;
+    }
+
+    private boolean isImmutable(Object value) {
+        return isImmutableType(value.getClass());
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> Class<T> getTypedClassFrom(T entity) {
+        return (Class<T>) entity.getClass();
+    }
+
+    private <T> T instantiateUsingDefaultConstructor(Class<T> klass) {
+        Constructor<T> constructor;
+        try {
+            constructor = klass.getConstructor();
+        } catch (SecurityException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new IllegalArgumentException(
+                    "could not invoke default no-args constructor", e);
+        }
+        try {
+            return constructor.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void copyProperties(Object source, Object target) {
+        List<Field> fields = getAllFieldsFor(source);
+        for (Field each : fields) {
+            each.setAccessible(true);
+            if (!isIgnored(each)) {
+                Object sourceValue = readFieldValue(source, each);
+                if (sourceValue != null) {
+                    Strategy strategy = getStrategy(each, sourceValue);
+                    writeFieldValue(target, each, copy(sourceValue, strategy));
+                }
+            }
+        }
+    }
+
+    private List<Field> getAllFieldsFor(Object source) {
+        List<Field> result = new ArrayList<Field>();
+        Class<? extends Object> currentClass = source.getClass();
+        while (currentClass != null) {
+            result.addAll(Arrays.asList(currentClass.getDeclaredFields()));
+            currentClass = currentClass.getSuperclass();
+        }
+        return result;
+    }
+
+    private boolean isIgnored(Field field) {
+        return isStatic(field) || isMarkedWithIgnore(field);
+    }
+
+    private boolean isStatic(Field field) {
+        return Modifier.isStatic(field.getModifiers());
+    }
+
+    private boolean isMarkedWithIgnore(Field each) {
+        OnCopy onCopy = each.getAnnotation(OnCopy.class);
+        return onCopy != null && onCopy.value() == Strategy.IGNORE;
+    }
+
+    private void writeFieldValue(Object target, Field field, Object value) {
+        try {
+            field.set(target, value);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Object readFieldValue(Object source, Field field) {
+        try {
+            return field.get(source);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Strategy getStrategy(Field field, Object sourceValue) {
+        OnCopy onCopy = field.getAnnotation(OnCopy.class);
+        return onCopy != null ? onCopy.value() : null;
+    }
+
+    private ICustomCopy findCopier(Object sourceValue) {
+        for (ICustomCopy each : DEFAULT_CUSTOM_COPIERS) {
+            if (each.canHandle(sourceValue)) {
+                return each;
+            }
+        }
+        return null;
+    }
+}
---
+++ navalplanner-business/src/test/java/org/navalplanner/business/util/deepcopy/EntityExamples.java
@@ -0,0 +1,267 @@
+/*
+ * This file is part of NavalPlan
+ *
+ * Copyright (C) 2009 Fundación para o Fomento da Calidade Industrial e
+ *                    Desenvolvemento Tecnolóxico de Galicia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.navalplanner.business.util.deepcopy;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+/**
+ * @author Óscar González Fernández <ogonzalez@igalia.com>
+ *
+ */
+public class EntityExamples {
+
+    private EntityExamples() {
+    }
+
+    public enum TestEnum {
+        A {
+        },
+        B;
+    }
+
+    public static class Parent {
+        private EntityA entityAProperty;
+
+        private String prueba = "bar";
+
+        private Set<Object> setProperty = new HashSet<Object>();
+
+        public EntityA getEntityAProperty() {
+            return entityAProperty;
+        }
+
+        public void setEntityAProperty(EntityA entityAProperty) {
+            this.entityAProperty = entityAProperty;
+        }
+
+        public Set<Object> getSetProperty() {
+            return setProperty;
+        }
+
+        public void setSetProperty(Set<Object> setProperty) {
+            this.setProperty = setProperty;
+        }
+    }
+
+    public static class EntityA {
+
+        private static final String staticProperty = "foo";
+
+        private final String finalProperty = "bar";
+        private String stringProperty;
+
+        private int intProperty = 2;
+
+        private Object nullProperty = null;
+
+        private Date date = null;
+
+        private Set<Object> setProperty;
+
+        private Map<Object, Object> mapProperty = new HashMap<Object, Object>();
+
+        private List<Object> listProperty;
+
+        private TestEnum enumProperty = null;
+
+        @OnCopy(Strategy.IGNORE)
+        private String ignoredProperty;
+
+        @OnCopy(Strategy.SHARE)
+        private Date sharedProperty;
+
+        @OnCopy(Strategy.SHARE)
+        private List<String> sharedListProperty;
+
+        @OnCopy(Strategy.SHARE_COLLECTION_ELEMENTS)
+        private Set<Object> sharedElementsProperty;
+
+        @OnCopy(Strategy.ONLY_SHARE_KEYS)
+        private Map<Object, Object> sharedKeysMapProperty;
+
+        @OnCopy(Strategy.ONLY_SHARE_VALUES)
+        private Map<Object, Object> sharedValuesMapProperty;
+
+        @OnCopy(Strategy.SHARE_COLLECTION_ELEMENTS)
+        private Map<Object, Object> sharedCollectionElementsMapProperty;
+
+        private Parent parentProperty;
+
+        public String getStringProperty() {
+            return stringProperty;
+        }
+
+        public void setStringProperty(String stringProperty) {
+            this.stringProperty = stringProperty;
+        }
+
+        public int getIntProperty() {
+            return intProperty;
+        }
+
+        public void setIntProperty(int intProperty) {
+            this.intProperty = intProperty;
+        }
+
+        public Object getNullProperty() {
+            return nullProperty;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public Set<Object> getSetProperty() {
+            return setProperty;
+        }
+
+        public void setSetProperty(Set<Object> setProperty) {
+            this.setProperty = setProperty;
+        }
+
+        public Map<Object, Object> getMapProperty() {
+            return mapProperty;
+        }
+
+        public void setMapProperty(Map<Object, Object> mapProperty) {
+            this.mapProperty = mapProperty;
+        }
+
+        public List<Object> getListProperty() {
+            return listProperty;
+        }
+
+        public void setListProperty(List<Object> listProperty) {
+            this.listProperty = listProperty;
+        }
+
+        public void setIgnoredProperty(String ignoredProperty) {
+            this.ignoredProperty = ignoredProperty;
+        }
+
+        public String getIgnoredProperty() {
+            return ignoredProperty;
+        }
+
+        public Date getSharedProperty() {
+            return sharedProperty;
+        }
+
+        public void setSharedProperty(Date sharedProperty) {
+            this.sharedProperty = sharedProperty;
+        }
+
+        public List<String> getSharedListProperty() {
+            return sharedListProperty;
+        }
+
+        public void setSharedListProperty(List<String> sharedListProperty) {
+            this.sharedListProperty = sharedListProperty;
+        }
+
+        public Set<Object> getSharedElementsProperty() {
+            return sharedElementsProperty;
+        }
+
+        public void setSharedElementsProperty(Set<Object> sharedElementsProperty) {
+            this.sharedElementsProperty = sharedElementsProperty;
+        }
+
+        public Map<Object, Object> getSharedKeysMapProperty() {
+            return sharedKeysMapProperty;
+        }
+
+        public void setSharedKeysMapProperty(
+                Map<Object, Object> sharedKeysMapProperty) {
+            this.sharedKeysMapProperty = sharedKeysMapProperty;
+        }
+
+        public void setSharedValuesMapProperty(Map<Object, Object> originalMap) {
+            this.sharedValuesMapProperty = originalMap;
+        }
+
+        public Map<Object, Object> getSharedValuesMapProperty() {
+            return sharedValuesMapProperty;
+        }
+
+        public void setSharedCollectionElementsMapProperty(Map<Object, Object> map) {
+            this.sharedCollectionElementsMapProperty = map;
+        }
+
+        public Map<Object, Object> getSharedCollectionElementsMapProperty() {
+            return sharedCollectionElementsMapProperty;
+        }
+
+        public Parent getParentProperty() {
+            return parentProperty;
+        }
+
+        public void setParentProperty(Parent parentProperty) {
+            this.parentProperty = parentProperty;
+        }
+
+        public TestEnum getEnumProperty() {
+            return enumProperty;
+        }
+
+        public void setEnumProperty(TestEnum enumProperty) {
+            this.enumProperty = enumProperty;
+        }
+
+        public String getFinalProperty() {
+            return finalProperty;
+        }
+
+    }
+
+    public static class EntityWithoutNoArgsConstructor {
+        public EntityWithoutNoArgsConstructor(String arg) {
+        }
+    }
+
+    public static class SuperclassExample {
+        private String superClassStringProperty;
+
+        public String getSuperClassStringProperty() {
+            return superClassStringProperty;
+        }
+
+        public void setSuperClassStringProperty(String superClassStringProperty) {
+            this.superClassStringProperty = superClassStringProperty;
+        }
+    }
+
+    public static class SubClassExample extends SuperclassExample {
+
+        private int intProperty;
+
+    }
+
+}
