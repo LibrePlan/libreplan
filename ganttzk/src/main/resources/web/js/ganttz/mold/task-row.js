c33903415785eeeb582b6525d9e813943541027a
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-08-09 16:14:01 +0200

    Mold for TaskRow component
    * Created Javascript mold for TaskRow component
    
    FEA: ItEr02S03MigracionZK5

--- ganttzk/src/main/resources/web/js/ganttz/mold/task-row.js
+++ ganttzk/src/main/resources/web/js/ganttz/mold/task-row.js
@@ -1,1 +1,7 @@
-function(out){}
+function(out){
+	out.push('<div id="'+ this.uuid + '" class="row" z.valor="boxid=' + this.uuid +'">');
+	for(var w = this.firstChild; w; w = w.nextSibling)
+		w.redraw(out);
+	out.push('<div id="deadline'+ this.uuid + '" class="deadline"></div>');
+	out.push('<div id="consolidatedline' + this.uuid + '" class="consolidatedline"></div>');
+}
