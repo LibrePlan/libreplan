d6a57710795a672f91b518cd78c92a93ed75cc23
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-08-09 15:55:17 +0200

    Mold for TaskList comp.
    * Implemented javascript mold for TaskList component
    
    FEA: ItEr02S03MigracionZK5

--- ganttzk/src/main/resources/web/js/ganttz/mold/task-list.js
+++ ganttzk/src/main/resources/web/js/ganttz/mold/task-list.js
@@ -1,1 +1,14 @@
-function(out){}
+function(out){
+    out.push('<script type="text/javascript">',
+            'document.body.class = "yui-skin-sam";',
+            '</script>');
+
+    out.push('<div id="scroll_container">');
+        out.push('<div z.type="gantt.tasklist.TaskList" z.autoz="true" ' + this.domAttrs_() + '>');
+            out.push('<div id="listtasks"');
+                for(var w = this.firstChild; w; w = this.nextSibling)
+                    w.redraw(out);
+            out.push('</div');
+        out.push('</div>');
+    out.push('</div>');
+}
