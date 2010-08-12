18c306a2b332e6364aabadeb07d20a70138479cf
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-08-12 10:43:15 +0200

    Created GanttPanel widget-class
    * Now it just handles the mousemove event over the <div id="ganttpanel"> ... container
    
    FEA: ItEr02S03MigracionZK5

--- ganttzk/src/main/resources/web/js/ganttz/GanttPanel.js
+++ ganttzk/src/main/resources/web/js/ganttz/GanttPanel.js
@@ -1,3 +1,36 @@
 zk.$package("ganttz");
 
-ganttz.GanttPanel = zk.$extends(zk.Widget, {});
+ganttz.GanttPanel = zk.$extends(zk.Widget,{
+	$define: {
+		xMouse : null,
+		yMouse : null
+	},
+	$init : function(){
+		this.$super('$init');
+		if (!this.$class.getInstance()){
+			this.$class.setInstance(this);
+		}
+		else
+			throw "There's already one GanttPanel"
+	},
+	bind_ : function(evt){
+		this.$supers('bind_', arguments);
+		this.domListen_(this.$n(), "onMousemove", '_calcXY');
+	},
+	unbind_ : function(evt){
+		this.domUnListen_(this.$n(), "onMousemove", '_calcXY');
+		this.$supers('unbind_', arguments);
+	},
+	_calcXY : function(event){
+		var arrPos = YAHOO.util.Event.getXY(event);
+		this.setXMouse(arrPos[0]);
+		this.setYMouse(arrPos[1]);
+	}
+},{
+	getInstance : function(){
+		return this._instance;
+	},
+	setInstance : function(instance){
+		this._instance = instance;
+	}
+});
