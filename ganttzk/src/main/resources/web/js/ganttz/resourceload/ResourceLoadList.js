4da7bcbc47682474b6b9ace1e423a2d48a610f9f
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-08-20 12:51:31 +0200

    Added recalculateTimeTrackerHeight
    * Added this method to ResourceLoadList widget-class
    
    FEA: ItEr02S03MigracionZK5

--- ganttzk/src/main/resources/web/js/ganttz/resourceload/ResourceLoadList.js
+++ ganttzk/src/main/resources/web/js/ganttz/resourceload/ResourceLoadList.js
@@ -4,8 +4,21 @@ ganttz.resourceload.ResourceLoadList = zk.$extends(zk.Widget,{
     $init : function(){
         this.$super('$init');
         this.$class.setInstance(this);
+    },
+    recalculateTimeTrackerHeight : function(){
+        var DOMresourceLoadList = jq('.resourceloadlist');
+        var DOMfirstWatermarkColumn = jq('.rightpanellayout tr#watermark td :first');
+
+        if ( DOMresourceLoadList != undefined && DOMfirstWatermarkColumn != undefined){
+            DOMresourceLoadList.height(
+                    Math.max(
+                            DOMresourceLoadList.innerHeight() + this.$class.WATERMARK_MARGIN_BOTTOM,
+                            this.$class.WATERMARK_MIN_HEIGHT));
+        }
     }
 },{ //Class stuff
+    WATERMARK_MIN_HEIGHT : 450,
+    WATERMARK_MARGIN_BOTTOM : 40,
     setInstance : function(instance){
         this._instance = instance;
     },
