dfb2e507408ad638cda6be6c743f7eb4c2a62ece
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-08-20 12:09:01 +0200

    Added class-methods setInstance and getIntance
    * As we need a reference to the widget-object created for ResourceLoadList we keep this reference on the class itself
    
    FEA: ItEr02S03MigracionZK5

--- ganttzk/src/main/resources/web/js/ganttz/resourceload/ResourceLoadList.js
+++ ganttzk/src/main/resources/web/js/ganttz/resourceload/ResourceLoadList.js
@@ -1,3 +1,15 @@
 zk.$package("ganttz.resourceload");
 
-ganttz.resourceload.ResourceLoadList = zk.$extends(zk.Widget,{});
+ganttz.resourceload.ResourceLoadList = zk.$extends(zk.Widget,{
+    $init : function(){
+        this.$super('init');
+        this.$class.setInstance(this);
+    }
+},{ //Class stuff
+    setInstance : function(instance){
+        this._instance = instance;
+    },
+    getInstance : function(){
+        return this._instance;
+    }
+});
