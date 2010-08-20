8d296813df863ae40ec8d6a880a0b3ce449cd8e9
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-08-20 14:16:12 +0200

    Event handler for resize event
    * Added an event handler for the onResize event at ResourceLoadList widget-class
    
    FEA: ItEr02S03MigracionZK5

--- ganttzk/src/main/resources/web/js/ganttz/resourceload/ResourceLoadList.js
+++ ganttzk/src/main/resources/web/js/ganttz/resourceload/ResourceLoadList.js
@@ -5,6 +5,14 @@ ganttz.resourceload.ResourceLoadList = zk.$extends(zk.Widget,{
         this.$super('$init');
         this.$class.setInstance(this);
     },
+    bind_ : function(evt){
+        this.$supers('bind_', arguments);
+        this.domListen_(jq(window), 'onResize', 'adjustTimeTrackerSize');
+    },
+    unbind_ : function(evt){
+        this.domUnlisten_(jq(window), 'onResize', 'adjustTimeTrackerSize');
+        this.$supers('unbind_', arguments);
+    },
     recalculateTimeTrackerHeight : function(){
         var DOMresourceLoadList = jq('.resourceloadlist');
         var DOMfirstWatermarkColumn = jq('.rightpanellayout tr#watermark td :first');
@@ -15,6 +23,21 @@ ganttz.resourceload.ResourceLoadList = zk.$extends(zk.Widget,{
                             DOMresourceLoadList.innerHeight() + this.$class.WATERMARK_MARGIN_BOTTOM,
                             this.$class.WATERMARK_MIN_HEIGHT));
         }
+    },
+    adjustTimeTrackerSize : function(){
+        this.recalculateTimeTrackerHeight();
+
+        /*We can't use this.getHeight() as the _height property
+         * won't be set for this object and even
+         *
+         * TODO: maybe create a _height property and update it
+         * when it changes (recalculateTimeTrackerHeight) son we avoid
+         * using DOM selectors
+         * */
+        jq('#watermark').height(jq(this.$n()).innerHeight());
+        jq('#timetracker').width(jq(this.$n()).innerWidth());
+
+        jq('.resourceloadlist').width(jq('.second_level_ :first'));
     }
 },{ //Class stuff
     WATERMARK_MIN_HEIGHT : 450,
