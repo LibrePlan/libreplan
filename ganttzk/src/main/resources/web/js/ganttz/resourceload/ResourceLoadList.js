0ef5a6d4e98e9b6630afeba697927bdc3cccf57c
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-08-23 10:59:41 +0200

    Added event handler for scrolling
    FEA: ItEr02S03MigracionZK5

--- ganttzk/src/main/resources/web/js/ganttz/resourceload/ResourceLoadList.js
+++ ganttzk/src/main/resources/web/js/ganttz/resourceload/ResourceLoadList.js
@@ -8,19 +8,21 @@ ganttz.resourceload.ResourceLoadList = zk.$extends(zk.Widget,{
     bind_ : function(evt){
         this.$supers('bind_', arguments);
         this.domListen_(jq(window), 'onResize', 'adjustTimeTrackerSize');
+        this.domListen_(jq('.rightpanellayout div:first'), 'onScroll', '_listenToScroll');
     },
     unbind_ : function(evt){
         this.domUnlisten_(jq(window), 'onResize', 'adjustTimeTrackerSize');
+        this.domUnlisten_(jq('.rightpanellayout div:first'), 'onScroll', '_listenToScroll');
         this.$supers('unbind_', arguments);
     },
     recalculateTimeTrackerHeight : function(){
-        var DOMresourceLoadList = jq('.resourceloadlist');
+        var DOMResourceLoadList = jq('.resourceloadlist');
         var DOMfirstWatermarkColumn = jq('.rightpanellayout tr#watermark td :first');
 
-        if ( DOMresourceLoadList != undefined && DOMfirstWatermarkColumn != undefined){
-            DOMresourceLoadList.height(
+        if ( DOMResourceLoadList != undefined && DOMfirstWatermarkColumn != undefined){
+            DOMResourceLoadList.height(
                     Math.max(
-                            DOMresourceLoadList.innerHeight() + this.$class.WATERMARK_MARGIN_BOTTOM,
+                            DOMResourceLoadList.innerHeight() + this.$class.WATERMARK_MARGIN_BOTTOM,
                             this.$class.WATERMARK_MIN_HEIGHT));
         }
     },
@@ -38,6 +40,15 @@ ganttz.resourceload.ResourceLoadList = zk.$extends(zk.Widget,{
         jq('#timetracker').width(jq(this.$n()).innerWidth());
 
         jq('.resourceloadlist').width(jq('.second_level_ :first'));
+    },
+    _listenToScroll : function(){
+        var scrolledPannelScrollLeft = jq('.rightpanellayout div:first').scrollLeft();
+        var scrolledPannelScrollTop = jq('.rightpanellayout div:first').scrollTop();
+
+        jq('canvas.timeplot-canvas').css("left", "-" + scrolledPannelScrollLeft + "px");
+        jq('.timetrackergap').css("left", "-" + scrolledPannelScrollLeft + "px");
+        jq('.leftpanelgap .z-tree-body').css("top", "-" + scrolledPannelScrollTop + "px");
+        jq('.resourcesloadgraph div').scrollLeft(scrolledPannelScrollLeft + "px");
     }
 },{ //Class stuff
     WATERMARK_MIN_HEIGHT : 450,
