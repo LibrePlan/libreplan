df241aaa554f66875f958ea7e75477c89385ed6a
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-08-23 17:56:59 +0200

    Mold content for LimitingDependencyList
    FEA: ItEr02S03MigracionZK5

--- navalplanner-webapp/src/main/resources/web/js/limitingresources/mold/limiting-dependency-list.js
+++ navalplanner-webapp/src/main/resources/web/js/limitingresources/mold/limiting-dependency-list.js
@@ -1,1 +1,11 @@
-function(out){}
+function(out){
+	out.push('<div ', this.domAttrs_(),
+			'z.type="limitingresources.limitingdependencylist.LimitingDependencyList',
+			'z.autoz="true"',
+			'>');
+		out.push('<div id="listlimitingdependencies">');
+		for (var w = this.firstChild; w; w = w.nextSibling)
+	        w.redraw(out);
+		out.push('</div>');
+	out.push('</div>');
+}
