eb71ff6189d192e7c2b85dfd94513c39f036ffa7
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-08-24 15:31:02 +0200

    Mold content for TaskContainerComponent
    FEA: ItEr02S03MigracionZK5

--- ganttzk/src/main/resources/web/js/ganttz/mold/task-container.js
+++ ganttzk/src/main/resources/web/js/ganttz/mold/task-container.js
@@ -1,1 +1,34 @@
-function(out){}
+function(out){
+	out.push('<div ', this.domAttrs_(),
+			'z.type="ganttz.taskcontainer.TaskContainer"',
+			'idTask="', this.id,'"',
+			'z.autoz="true"',
+			'class="taskgroup"',
+			'>');
+
+		out.push('<div class="task-labels">',
+					this.getLabelsText(),
+				'</div>');
+
+		out.push('<div class="task-resources">',
+					'<div class="task-resources-inner">',
+						this.getResourcesText(),
+				'</div>');
+
+		out.push('<div class="taskcontainer_completion">',
+					'<div class="completion"></div>',
+					'<div class="completion2"></div>',
+				'</div>');
+
+		out.push('<div class="border-container">',
+					'<div class="taskgroup_start"></div>',
+					'<div class="taskgroup_end"></div>',
+				'</div>');
+
+		out.push('<div id="tasktooltip', this.uuid, '"',
+				'class="task_tooltip">',
+					this.getTooltipText(),
+				'</div>');
+
+	out.push('</div>');
+}
