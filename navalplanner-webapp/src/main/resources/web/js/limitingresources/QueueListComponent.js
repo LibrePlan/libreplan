c05d505ab4ece1051f5794f327fa09d459651e62
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-08-24 11:51:46 +0200

    Added adjustResourceLoadRows for QueueListComponent
    FEA: ItEr02S03MigracionZK5

--- navalplanner-webapp/src/main/resources/web/js/limitingresources/QueueListComponent.js
+++ navalplanner-webapp/src/main/resources/web/js/limitingresources/QueueListComponent.js
@@ -1,3 +1,10 @@
 zk.$package("limitingresources");
 
-limitingresources.QueueListComponent = zk.$extends(zk.Widget,{})
+limitingresources.QueueListComponent = zk.$extends(zk.Widget,{
+	adjustResourceLoadRows : function(){
+		var width = jq('.rightpanellayout #timetracker .z-grid-header :first').innerWidth();
+		jq('.row_resourceload').each(function(index, element){
+			jq(element).width(width);
+		});
+	}
+})
