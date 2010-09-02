4baf3efc0a2ccd6146dc44bdfc77115fdbc55275
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-09-02 11:33:59 +0200

    Added missing $supers call at overwrote method
    * As this $supers call was missing the children wasn't added to the parent's children array, it was just inserted at the DOM
    
    FEA: ItEr02S03MigracionZK5

--- ganttzk/src/main/resources/web/js/ganttz/DependencyList.js
+++ ganttzk/src/main/resources/web/js/ganttz/DependencyList.js
@@ -2,6 +2,8 @@ zk.$package("ganttz");
 
 ganttz.DependencyList = zk.$extends(zk.Widget, {
 	appendChild : function(child){
+		//true for ignoring DOM insertion
+		this.$supers('appendChild',[child,true]);
 		jq('#listdependencies').append(child);
 	}
 });
