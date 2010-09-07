a61037aa1dc135a644453ea1d5b5d69c210ca3c7
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-09-07 09:25:51 +0200

    Added singleton-like structure for DependencyList
    * Added seg/getInstance methods
    
    FEA: ItEr02S03MigracionZK5

--- ganttzk/src/main/resources/web/js/ganttz/DependencyList.js
+++ ganttzk/src/main/resources/web/js/ganttz/DependencyList.js
@@ -1,9 +1,20 @@
 zk.$package("ganttz");
 
 ganttz.DependencyList = zk.$extends(zk.Widget, {
+	$init : function(){
+		this.$supers('$init', arguments);
+		this.$class.setInstance(this);
+	},
 	appendChild : function(child){
 		//true for ignoring DOM insertion
 		this.$supers('appendChild',[child,true]);
 		jq('#listdependencies').append(child);
+	}
+},{
+	setInstance : function(instance){
+		this._instance = instance;
+	},
+	getInstance : function(){
+		return this._instance;
 	}
 });
