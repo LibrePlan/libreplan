4a7987b8f3e2ddc9c64c992ec950f3fef7bb46a2
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-09-08 10:14:04 +0200

    Added drag&drop functionallity
    * Yahoo is included on the file for providing drag&drop. This must be deleted if the drag&drop is implemented with jQuery
    
    FEA: ItEr02S03MigracionZK5

--- ganttzk/src/main/resources/web/js/ganttz/TaskComponent.js
+++ ganttzk/src/main/resources/web/js/ganttz/TaskComponent.js
@@ -1,5 +1,61 @@
 zk.$package("ganttz");
 
+/*
+ * This YAHOO code is here because it's used for the Drag&Drop. Once the Drag&Drop is implemented with jQuery
+ * this code must be removed
+ * */
+YAHOO.example.DDRegion = function(id, sGroup, config) {
+    this.cont = config.cont;
+    YAHOO.example.DDRegion.superclass.constructor.apply(this, arguments);
+};
+
+var myDom = YAHOO.util.Dom, myEvent = YAHOO.util.Event
+
+YAHOO.extend(YAHOO.example.DDRegion, YAHOO.util.DD, {
+    cont : null,
+    init : function() {
+        // Call the parent's init method
+    YAHOO.example.DDRegion.superclass.init.apply(this, arguments);
+    this.initConstraints();
+
+    myEvent.on(window, 'resize', function() {
+        this.initConstraints();
+    }, this, true);
+},
+initConstraints : function() {
+
+    // Get the top, right, bottom and left positions
+    var region = myDom.getRegion(this.cont);
+
+    // Get the element we are working on
+    var el = this.getEl();
+
+    // Get the xy position of it
+    var xy = myDom.getXY(el);
+
+    // Get the width and height
+    var width = parseInt(myDom.getStyle(el, 'width'), 10);
+    var height = parseInt(myDom.getStyle(el, 'height'), 10);
+
+    // Set left to x minus left
+    var left = xy[0] - region.left;
+
+    // Set right to right minus x minus width
+    var right = region.right - xy[0] - width;
+
+    // Set top to y minus top
+    var top = xy[1] - region.top;
+
+    // Set bottom to bottom minus y minus height
+    var bottom = region.bottom - xy[1] - height;
+
+    // Set the constraints based on the above calculations
+    this.setXConstraint(left, right);
+    this.setYConstraint(top, bottom);
+}
+});
+
+
 ganttz.TaskComponent = zk.$extends(zk.Widget, {
     $define :{
         resourcesText    : null,
@@ -10,6 +66,7 @@ ganttz.TaskComponent = zk.$extends(zk.Widget, {
         this.$supers('bind_', arguments);
         this.domListen_(this.$n(), "onMouseover", '_showTooltip');
         this.domListen_(this.$n(), "onMouseout", '_hideTooltip');
+        if( jq(this.$n()).attr('movingtasksenabled') ) this._addDragDrop();
     },
     unbind_ : function(event){
         this.domUnlisten_(this.$n(), "onMouseout", '_hideTooltip');
@@ -27,6 +84,32 @@ ganttz.TaskComponent = zk.$extends(zk.Widget, {
         if(this._dependencies == undefined) this._dependencies = [];
         this._dependencies.push(dependency);
     },
+    _addDragDrop : function(){
+        var dragdropregion = this._getDragDropRegion();
+
+        dragdropregion.on('dragEvent', this.proxy(function(ev) {
+            // Slight overload. It could be more efficent to overwrite the YUI
+            // method
+            // that is setting the top property
+                jq(this.$n()).css('top','');
+                if (this._dependencies != undefined) {
+                    jq.each(this._dependencies, function(index, dependency){
+                        dependency.draw();
+                    });
+                }
+            }), null, false);
+         // Register the event endDragEvent
+        dragdropregion.on('endDragEvent', this.proxy(function(ev) {
+            var position = jq(this.$n()).position();
+
+            zAu.send(new zk.Event(this, 'onUpdatePosition', [new String(position.left), new String(position.top)]));
+//            zkau.send( {
+//                uuid : cmp.id,
+//                cmd : "updatePosition",
+//                data : [ cmp.style.left, cmp.style.top ]
+//            });
+        }), null, false);
+    },
     _createArrow : function(){
         var WGTdependencylist = ganttz.DependencyList.getInstance();
         var unlinkedDependency = new ganttz.UnlinkedDependencyComponent();
@@ -36,6 +119,15 @@ ganttz.TaskComponent = zk.$extends(zk.Widget, {
         WGTdependencylist.rerender();
 
         unlinkedDependency.draw();
+    },
+    _getDragDropRegion : function(){
+        if (typeof (this._dragDropRegion) == 'undefined') {
+            // Create the laned drag&drop component
+            this._dragDropRegion = new YAHOO.example.DDRegion(this.uuid, '', {
+                cont : this.parent.getId()
+            });
+        }
+        return this._dragDropRegion;
     },
     _showTooltip : function(){
         this.mouseOverTask = true;
