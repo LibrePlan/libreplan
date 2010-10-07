6824272a795aec2de085598b865a4e037b558b2d
Lorenzo Tilve <ltilve@igalia.com>
2010-10-07 12:03:30 +0200

    Made Criterion shortByName method case-insensitive
    FEA: ItEr61S05BugFixing

--- navalplanner-business/src/main/java/org/navalplanner/business/resources/entities/Criterion.java
+++ navalplanner-business/src/main/java/org/navalplanner/business/resources/entities/Criterion.java
@@ -80,7 +80,8 @@ public class Criterion extends IntegrationEntity implements ICriterion {
                 if (o2.getName() == null) {
                     return -1;
                 }
-                return o1.getName().compareTo(o2.getName());
+                return o1.getName().toLowerCase().compareTo(
+                        o2.getName().toLowerCase());
             }
         });
         return result;
