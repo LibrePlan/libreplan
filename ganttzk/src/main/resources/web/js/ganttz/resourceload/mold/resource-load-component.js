fb00c282787cbd5c1eda424c761aa65c7dc8c12d
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-11-29 12:49:47 +0100

    Added missing closing symbol
    * As a result of it, the names of resources appeared on the chart
    
    FEA: ItEr02S03MigracionZK5

--- ganttzk/src/main/resources/web/js/ganttz/resourceload/mold/resource-load-component.js
+++ ganttzk/src/main/resources/web/js/ganttz/resourceload/mold/resource-load-component.js
@@ -1,7 +1,8 @@
 function(out){
     out.push('<div ', this.domAttrs_(),
             ' class="row_resourceload resourceload-'+ this.getResourceLoadType(),'"',
-            ' z.autoz="true"');
+            ' z.autoz="true"',
+            '>');
         out.push('<span class="resourceload_name">', this.getResourceLoadName(),'</span>');
         for(var w = this.firstChild; w; w = w.nextSibling)
             w.redraw(out);
