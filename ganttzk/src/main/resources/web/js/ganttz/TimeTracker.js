85957d30cbcf83bc05737f1635a0ab779e9d4fe8
Farruco Sanjurjo <fsanjurjo@igalia.com>
2010-12-03 14:00:41 +0100

    Added code to make TimeTracker's widget-class singleton
    FEA: ItEr02S03MigracionZK5

--- ganttzk/src/main/resources/web/js/ganttz/TimeTracker.js
+++ ganttzk/src/main/resources/web/js/ganttz/TimeTracker.js
@@ -1,3 +1,15 @@
 zk.$package("ganttz");
 
-ganttz.TimeTracker = zk.$extends(zk.Macro,{})
+ganttz.TimeTracker = zk.$extends(zk.Macro,{
+	$init : function(){
+    this.$supers('$init', arguments);
+    	this.$class.setInstance(this);
+	}
+},{
+    getInstance : function(){
+    	return this._instance;
+	},
+	setInstance : function(instance){
+		this._instance = instance;
+	}
+})
