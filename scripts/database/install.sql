064a9117a54e24214c9bbc6dbf358a4cdc6acdcf
Manuel Rego Casasnovas <mrego@igalia.com>
2011-01-21 12:03:41 +0100

    Fixed problem in Debian package install with some missing SQL sentences.
    Bug reported on NavalPlan forum:
    http://sourceforge.net/projects/navalplan/forums/forum/1085571/topic/4064159/
    
    FEA: ItEr68S04BugFixing

--- scripts/database/install.sql
+++ scripts/database/install.sql
@@ -1655,3 +1655,25 @@ ALTER TABLE configuration ADD scenarios_visible BOOLEAN;
 UPDATE configuration SET scenarios_visible = FALSE;
 
 INSERT INTO databasechangelog (AUTHOR, COMMENTS, DATEEXECUTED, DESCRIPTION, EXECTYPE, FILENAME, ID, LIQUIBASE, MD5SUM, ORDEREXECUTED) VALUES ('ltilve', 'Add new column with scenarios visibility flag', NOW(), 'Add Column, Update Data', 'EXECUTED', 'src/main/resources/db.changelog-initial.xml', 'add-scenarios-enabled-configuration-setting', '2.0-rc7', '3:53d99bb420a0c55c8eaa9389e3fc0ed5', 325);
+
+
+-- *********************************************************************
+-- Update Database Script - NavalPlan 1.0.0
+-- *********************************************************************
+-- Change Log: src/main/resources/db.changelog-initial.xml
+-- Ran at: 1/7/11 3:49 PM
+-- Against: naval@jdbc:postgresql://localhost/navaldev
+-- Liquibase version: 2.0-rc7
+-- *********************************************************************
+
+-- Lock Database
+-- Changeset src/main/resources/db.changelog-initial.xml::change-types-start-finish-date-criterion-satisfaction::ogonzalez::(Checksum: 3:93527c263e394c3960738fc2e9734c4c)
+-- Change types of start and finish date to date for criterion satisfaction table
+ALTER TABLE criterion_satisfaction ALTER COLUMN start_date TYPE DATE;
+
+ALTER TABLE criterion_satisfaction ALTER COLUMN finish_date TYPE DATE;
+
+INSERT INTO databasechangelog (AUTHOR, COMMENTS, DATEEXECUTED, DESCRIPTION, EXECTYPE, FILENAME, ID, LIQUIBASE, MD5SUM, ORDEREXECUTED) VALUES ('ogonzalez', 'Change types of start and finish date to date for criterion satisfaction table', NOW(), 'Modify data type (x2)', 'EXECUTED', 'src/main/resources/db.changelog-initial.xml', 'change-types-start-finish-date-criterion-satisfaction', '2.0-rc7', '3:93527c263e394c3960738fc2e9734c4c', 326);
+
+-- Release Database Lock
+-- Release Database Lock
