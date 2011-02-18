cd4f463366065aa36bdea8fa81cacf29532d0749
Farruco Sanjurjo <fsanjurjo@igalia.com>
2011-02-18 10:38:59 +0100

    Added missing files for JasperreportComponent
    * Widget,mold and zk.wpd files
    * Component defined on lang-addon.xml
    
    FEA: ItEr02S03MigracionZK5

---
+++ navalplanner-webapp/src/main/resources/web/js/common/components/JasperreportComponent.js
@@ -0,0 +1,3 @@
+zk.$package("common.components");
+
+common.components.JasperreportComponent = zk.$extends(zk.Widget,{})
---
+++ navalplanner-webapp/src/main/resources/web/js/common/components/mold/jasperreport-component.js
@@ -0,0 +1,6 @@
+function(out){
+	out.push('<div ', this.domAttrs_(),
+			'class="jasperreport-component"',
+			'>');
+	out.push('</div>');
+}
