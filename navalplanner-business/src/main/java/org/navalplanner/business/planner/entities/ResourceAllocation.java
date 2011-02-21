a196487fd0f5d84ae5e1bf49ce1605fd05cc4bc7
Óscar González Fernández <ogonzalez@igalia.com>
2011-02-21 17:00:39 +0100

    Apply validation annotation to the field
    Because it's a value object this is not dangerous, i.e., it can't be a
    proxy. Now it's really checked a not null value is not saved.

--- navalplanner-business/src/main/java/org/navalplanner/business/planner/entities/ResourceAllocation.java
+++ navalplanner-business/src/main/java/org/navalplanner/business/planner/entities/ResourceAllocation.java
@@ -461,6 +461,7 @@ public abstract class ResourceAllocation<T extends DayAssignment> extends
     private AssignmentFunction assignmentFunction;
 
     @OnCopy(Strategy.SHARE)
+    @NotNull
     private ResourcesPerDay resourcesPerDay;
 
     private Integer intendedTotalHours;
@@ -1552,7 +1553,6 @@ public abstract class ResourceAllocation<T extends DayAssignment> extends
         return calculateResourcesPerDayFromAssignments(getConsolidatedAssignments());
     }
 
-    @NotNull
     public ResourcesPerDay getResourcesPerDay() {
         if (resourcesPerDay == null) {
             return ResourcesPerDay.amount(0);
