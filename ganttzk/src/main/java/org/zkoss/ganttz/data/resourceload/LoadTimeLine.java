96bb5c0ae97b18fe780b4f8ffc2f3faad69c9b37
Diego Pino Garcia <dpino@igalia.com>
2011-03-29 12:50:02 +0200

    [Bug #952] Show list of resources in 'Resource load view' in order
    FEA: ItEr73S04BugFixing

--- ganttzk/src/main/java/org/zkoss/ganttz/data/resourceload/LoadTimeLine.java
+++ ganttzk/src/main/java/org/zkoss/ganttz/data/resourceload/LoadTimeLine.java
@@ -29,7 +29,7 @@ import org.apache.commons.lang.Validate;
 import org.joda.time.LocalDate;
 import org.zkoss.ganttz.util.Interval;
 
-public class LoadTimeLine {
+public class LoadTimeLine implements Comparable<LoadTimeLine>{
 
     private final String conceptName;
     private final List<LoadPeriod> loadPeriods;
@@ -193,6 +193,15 @@ public class LoadTimeLine {
             }
         }
         return result;
+    }
+
+    public String toString() {
+        return conceptName;
+    }
+
+    @Override
+    public int compareTo(LoadTimeLine o) {
+        return conceptName.compareTo(o.getConceptName());
     }
 
 }
