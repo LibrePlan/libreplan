bf520e6398a507f9633db622de9f1a80ed1360ec
Diego Pino Garcia <dpino@igalia.com>
2011-04-05 12:47:43 +0200

    [Bug #964] Impossible to delete materials not assigned to an order element
    FEA: ItEr74S04BugFixing

--- navalplanner-business/src/main/java/org/navalplanner/business/materials/entities/MaterialCategory.java
+++ navalplanner-business/src/main/java/org/navalplanner/business/materials/entities/MaterialCategory.java
@@ -145,6 +145,7 @@ public class MaterialCategory extends IntegrationEntity {
     }
 
     public void removeMaterial(Material material) {
+        material.setCategory(null);
         materials.remove(material);
     }
 
