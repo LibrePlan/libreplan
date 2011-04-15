03908842754415e0e3d48dedac5d8459df8f226e
Diego Pino Garcia <dpino@igalia.com>
2011-04-15 14:18:21 +0200

    [Bug #986] Remove unused code
    FEA: ItEr74S04BugFixing

--- navalplanner-business/src/main/java/org/navalplanner/business/labels/entities/Label.java
+++ navalplanner-business/src/main/java/org/navalplanner/business/labels/entities/Label.java
@@ -25,7 +25,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.Validate;
 import org.hibernate.validator.NotEmpty;
 import org.hibernate.validator.NotNull;
 import org.navalplanner.business.common.IntegrationEntity;
@@ -84,15 +83,6 @@ public class Label extends IntegrationEntity {
 
     public Set<OrderElement> getOrderElements() {
         return Collections.unmodifiableSet(orderElements);
-    }
-
-    public void addOrderElement(OrderElement orderElement) {
-        Validate.notNull(orderElement);
-        orderElements.add(orderElement);
-    }
-
-    public void removeOrderElement(OrderElement orderElement) {
-        orderElements.add(orderElement);
     }
 
     public boolean isEqualTo(Label label) {
