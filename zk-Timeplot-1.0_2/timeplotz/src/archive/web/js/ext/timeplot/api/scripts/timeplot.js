b8899ba3c7efe4dd3e408d77662bcb4e5ffb29c8
Lorenzo Tilve Álvaro <ltilve@igalia.com>
2011-04-18 11:28:23 +0200

    Set gradient default property to false in timeplotz
    It is necessary to run zk-Timeplot-1.0_2/deploy.sh to regenerate the .jar and
    delete the caches to refresh the browser javascript files.
    
    FEA: ItEr74S04BugFixing

--- zk-Timeplot-1.0_2/timeplotz/src/archive/web/js/ext/timeplot/api/scripts/timeplot.js
+++ zk-Timeplot-1.0_2/timeplotz/src/archive/web/js/ext/timeplot/api/scripts/timeplot.js
@@ -40,7 +40,7 @@ Timeplot.createPlotInfo = function(params) {
         valueGeometry:     ("valueGeometry" in params) ? params.valueGeometry : new Timeplot.DefaultValueGeometry(),
         timeZone:          ("timeZone" in params) ? params.timeZone : 0,
         fillColor:         ("fillColor" in params) ? ((params.fillColor == "string") ? new Timeplot.Color(params.fillColor) : params.fillColor) : null,
-        fillGradient:      ("fillGradient" in params) ? params.fillGradient : true,
+        fillGradient:      ("fillGradient" in params) ? params.fillGradient : false,
         fillFrom:          ("fillFrom" in params) ? params.fillFrom : Number.NEGATIVE_INFINITY,
         lineColor:         ("lineColor" in params) ? ((params.lineColor == "string") ? new Timeplot.Color(params.lineColor) : params.lineColor) : new Timeplot.Color("#606060"),
         lineWidth:         ("lineWidth" in params) ? params.lineWidth : 1.0,
--- zk-Timeplot-1.0_2/timeplotz/src/archive/web/js/timeplotz/timeplot.js
+++ zk-Timeplot-1.0_2/timeplotz/src/archive/web/js/timeplotz/timeplot.js
@@ -9040,12 +9040,19 @@ ctx.lineJoin='miter';
 
 if(this._dataSource){
 if(this._plotInfo.fillColor){
+
+ if (this._plotInfo.fillGradient) {
+
 var gradient=ctx.createLinearGradient(0,this._canvas.height,0,0);
 gradient.addColorStop(0,this._plotInfo.fillColor.toString());
 gradient.addColorStop(0.5,this._plotInfo.fillColor.toString());
 gradient.addColorStop(1,'rgba(255,255,255,0)');
 
 ctx.fillStyle=gradient;
+ } else {
+ctx.fillStyle = this._plotInfo.fillColor.toString();
+}
+
 
 ctx.beginPath();
 ctx.moveTo(0,0);
