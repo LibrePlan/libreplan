da568213fc407bbf4856a65665117c428c1b4bee
Lorenzo Tilve Álvaro <ltilve@igalia.com>
2011-04-24 21:15:41 +0200

    [Bug #907] Changed styles in advanced limiting assignment cells
    Now they are represented with the same styles as in the gantt view to
    improve coherence.
    
    FEA: ItEr74S04BugFixing

--- navalplanner-webapp/src/main/java/org/navalplanner/web/planner/allocation/AdvancedAllocationController.java
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/planner/allocation/AdvancedAllocationController.java
@@ -1581,6 +1581,7 @@ class Row {
             intbox.setValue(getHoursForDetailItem(item));
             if (isLimiting) {
                 intbox.setDisabled(true);
+                intbox.setSclass(" limiting");
             }
         }
     }
