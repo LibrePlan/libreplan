LibrePlan/libreplan

commit c05150b2345a4c2bebd631c690daf69aeda0f06e

When use redirect avoid to use fragment
The fragment is the part after the pound, #. If a fragment is used,
first the page is loaded and then the fragment is processed
afterwards. Not using fragments it can go directly to the correct
page.

FEA: ItEr74S04BugFixing

navalplanner-webapp/src/main/java/org/navalplanner/web/common/entrypoints/URLHandler.java
@@ -221,7 +221,8 @@ public class URLHandler<T> {
     }
 
     private String buildRedirectURL(String fragment) {
-        StringBuilder linkValue = new StringBuilder(page).append(fragment);
+        StringBuilder linkValue = new StringBuilder(page).append(";").append(
+                !fragment.isEmpty() ? fragment.substring(1) : "");
         return linkValue.toString();
     }
 

commit 086c1891ddb4a8ea6cf8a5b39ffa033fdf086939

Fix regression introduced in c05150b2345a4c2bebd631c690daf69aeda0f06e
Most pages only handled entry points through a bookmark change. Now
they handle requests with matrix parameters too.

FEA: ItEr74S04BugFixing

ganttzk/src/main/java/org/zkoss/ganttz/Planner.java
@@ -373,6 +373,8 @@ public class Planner extends HtmlMacroComponent  {
 
         this.visibleChart = configuration.isExpandPlanningViewCharts();
         ((South) getFellow("graphics")).setOpen(this.visibleChart);
+
+        newContext.getDiagramGraph().enforceAllRestrictions();
     }
 
     private void resettingPreviousComponentsToNull() {

navalplanner-webapp/src/main/java/org/navalplanner/web/common/entrypoints/URLHandler.java
@@ -303,7 +303,12 @@ public class URLHandler<T> {
         return false;
     }
 
-    public <S extends T> void registerListener(final S controller, Page page) {
+    public <S extends T> void register(final S controller, Page page) {
+        registerBookmarkListener(controller, page);
+        applyIfMatches(controller);
+    }
+
+    public <S extends T> void registerBookmarkListener(final S controller, Page page) {
         page.addEventListener("onBookmarkChange", new EventListener() {
 
             @Override

navalplanner-webapp/src/main/java/org/navalplanner/web/planner/tabs/MultipleTabsPlannerController.java
@@ -379,7 +379,7 @@ public class MultipleTabsPlannerController implements Composer,
             goToCompanyScheduling();
             planningTab.toggleToFeedback();
         }
-        handler.registerListener(this, comp.getPage());
+        handler.registerBookmarkListener(this, comp.getPage());
     }
 
     private TabsRegistry getTabsRegistry() {

navalplanner-webapp/src/main/java/org/navalplanner/web/resources/worker/WorkerCRUDController.java
@@ -338,7 +338,7 @@ public class WorkerCRUDController extends GenericForwardComposer implements
 
         final URLHandler<IWorkerCRUDControllerEntryPoints> handler = URLHandlerRegistry
                 .getRedirectorFor(IWorkerCRUDControllerEntryPoints.class);
-        handler.registerListener(this, page);
+        handler.register(this, page);
         getVisibility().showOnly(listWindow);
         initFilterComponent();
         setupFilterLimitingResourceListbox();

navalplanner-webapp/src/main/java/org/navalplanner/web/templates/OrderTemplatesController.java
@@ -267,7 +267,7 @@ public class OrderTemplatesController extends GenericForwardComposer implements
 
         final URLHandler<IOrderTemplatesControllerEntryPoints> handler = handlerRegistry
                 .getRedirectorFor(IOrderTemplatesControllerEntryPoints.class);
-        handler.registerListener(this, page);
+        handler.register(this, page);
 
         setBreadcrumbs(comp);
     }

navalplanner-webapp/src/main/java/org/navalplanner/web/users/UserCRUDController.java
@@ -86,7 +86,7 @@ public class UserCRUDController extends GenericForwardComposer implements
 
         final URLHandler<IUserCRUDController> handler = URLHandlerRegistry
                 .getRedirectorFor(IUserCRUDController.class);
-        handler.registerListener(this, page);
+        handler.register(this, page);
 
         getVisibility().showOnly(listWindow);
         passwordBox = (Textbox) createWindow.getFellowIfAny("password");

navalplanner-webapp/src/main/java/org/navalplanner/web/workreports/WorkReportCRUDController.java
@@ -173,7 +173,7 @@ public class WorkReportCRUDController extends GenericForwardComposer implements
         comp.setVariable("controller", this, true);
         final URLHandler<IWorkReportCRUDControllerEntryPoints> handler = URLHandlerRegistry
                 .getRedirectorFor(IWorkReportCRUDControllerEntryPoints.class);
-        handler.registerListener(this, page);
+        handler.register(this, page);
         initCurrentList();
         if(listType != null) {
             //listType is null in reports -> work report lines

navalplanner-webapp/src/main/java/org/navalplanner/web/workreports/WorkReportTypeCRUDController.java
@@ -141,7 +141,7 @@ public class WorkReportTypeCRUDController extends GenericForwardComposer
         comp.setVariable("controller", this, true);
         final URLHandler<IWorkReportTypeCRUDControllerEntryPoints> handler = URLHandlerRegistry
                 .getRedirectorFor(IWorkReportTypeCRUDControllerEntryPoints.class);
-        handler.registerListener(this, page);
+        handler.register(this, page);
         getVisibility().showOnly(listWindow);
     }
 