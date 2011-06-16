37994c73b7cf98b116530cd28653bbc039c2199f
Manuel Rego Casasnovas <rego@igalia.com>
2011-06-16 12:37:05 +0200

    [Bug #971] Fixed reseting Tree model to null.
    FEA: ItEr74S04BugFixing

--- navalplanner-webapp/src/main/java/org/navalplanner/web/orders/OrderCRUDController.java
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/orders/OrderCRUDController.java
@@ -406,7 +406,10 @@ public class OrderCRUDController extends GenericForwardComposer {
             orderElementsTree.useController(orderElementTreeController);
             orderElementTreeController.setReadOnly(readOnly);
 
-            setTreeRenderer(orderElementsTree);
+            Tree tree = (Tree) orderElementsTree.getFellowIfAny("tree");
+            tree.setModel(null);
+            tree.setTreeitemRenderer(orderElementTreeController.getRenderer());
+
             reloadTree(orderElementsTree);
         }
     }
@@ -422,11 +425,6 @@ public class OrderCRUDController extends GenericForwardComposer {
                 tree.clearSelection();
             }
         });
-    }
-
-    private void setTreeRenderer(TreeComponent orderElementsTree) {
-        final Tree tree = (Tree) orderElementsTree.getFellowIfAny("tree");
-        tree.setTreeitemRenderer(orderElementTreeController.getRenderer());
     }
 
     /*
