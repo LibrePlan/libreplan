5af60d8d37570b15ea09ab9edfbf51c9c4b4d9d0
Óscar González Fernández <ogonzalez@igalia.com>
2011-06-23 21:40:42 +0200

    Remove zk-Timeplot-1.0_2 project
    It is no longer needed. The timeplot for zk 5 seems to work fine.
    
    FEA: ItEr75S08MigrationZK5

--- zk-Timeplot-1.0_2/deploy.sh
+++
@@ -1,2 +0,0 @@
-./build build.debug timeplotz
-mvn install:install-file -Dfile=dist/lib/timeplotz.jar -DpomFile=timeplotz/pom.xml
--- zk-Timeplot-1.0_2/timeplotz/src/org/zkforge/timeplot/Version.java
+++
@@ -1,7 +0,0 @@
-package org.zkforge.timeplot;
-
-public class Version {
-
-	public final static String UID = "1.0_2";
-
-}
