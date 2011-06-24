124a1bb6661931f2555f1e2b4b94c64ae076aa59
Óscar González Fernández <ogonzalez@igalia.com>
2011-06-24 19:45:34 +0200

    Add missing function adjustTimeTrackerSize
    FEA: ItEr75S08MigrationZK5

--- navalplanner-webapp/src/main/resources/web/js/limitingresources/QueueListComponent.js
+++ navalplanner-webapp/src/main/resources/web/js/limitingresources/QueueListComponent.js
@@ -1,10 +1,20 @@
 zk.$package("limitingresources");
 
 limitingresources.QueueListComponent = zk.$extends(zk.Widget,{
-	adjustResourceLoadRows : function(){
+	adjustResourceLoadRows : function() {
 		var width = jq('.rightpanellayout #timetracker .z-grid-header :first').innerWidth();
 		jq('.row_resourceload').each(function(index, element){
 			jq(element).width(width);
 		});
-	}
-})
+	},
+	adjustTimeTrackerSize: function() {
+        var limitingResourcesList = jq('.limitingresourceslist');
+        jq('#watermark').height(this.$n().clientHeight);
+        jq('#timetracker').width(this.$n().clientWidth).each(function() {
+            // timetracker exists
+            var limitingResourcesList = jq('.limitingresourceslist');
+            limitingResourcesList.width(jq('.second_level_').get(0).clientWidth);
+            jq('.rightpanellayout tr#watermark td').height(limitingResourcesList.get(0).clientHeight + 120);
+        });
+    }
+});
