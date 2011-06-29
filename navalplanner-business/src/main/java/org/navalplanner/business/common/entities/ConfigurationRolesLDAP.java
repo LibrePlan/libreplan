LibrePlan/libreplan

commit 40a87d837f2997a9088628a60f618de030e92c33

Created constructor for LDAPConfiguration component and added some doc.
FEA: ItEr75S06LdapAuhenticationItEr74S09

navalplanner-business/src/main/java/org/navalplanner/business/common/entities/ConfigurationRolesLDAP.java
@@ -26,7 +26,7 @@ import org.hibernate.validator.NotEmpty;
  * LDAP roles and LibrePlan roles this will be used in LDAP configuration tab of
  * the Configuration screen.
  *
- * This class is a component of LdapConfiguration class
+ * This class is a component of {@link LDAPConfiguration} class
  *
  * @author Ignacio Diaz Teijido <ignacio.diaz@comtecsf.es>
  * @author Cristina Alvarino Perez <cristina.alvarino@comtecsf.es>
@@ -37,22 +37,19 @@ public class ConfigurationRolesLDAP {
 
     private String roleLibreplan;
 
+    public ConfigurationRolesLDAP(String roleLdap, String roleLibreplan) {
+        this.roleLdap = roleLdap;
+        this.roleLibreplan = roleLibreplan;
+    }
+
     @NotEmpty(message = "role ldap not specified")
     public String getRoleLdap() {
         return roleLdap;
     }
 
-    public void setRoleLdap(String roleLdap) {
-        this.roleLdap = roleLdap;
-    }
-
     @NotEmpty(message = "role libreplan not specified")
     public String getRoleLibreplan() {
         return roleLibreplan;
-    }
-
-    public void setRoleLibreplan(String roleLibreplan) {
-        this.roleLibreplan = roleLibreplan;
     }
 
 }

navalplanner-business/src/main/java/org/navalplanner/business/common/entities/LDAPConfiguration.java
@@ -69,7 +69,18 @@ public class LDAPConfiguration extends BaseEntity {
     // LDAP roles will be used or not
     private Boolean ldapSaveRolesDB = false;
 
-    // A list which stores the matching between LDAP roles and LibrePlan roles
+    /**
+     * A list which stores the matching between LDAP roles and LibrePlan roles.
+     * {@link ConfigurationRolesLDAP} is a component.
+     *
+     * For each matching a new {@link ConfigurationRolesLDAP} would be stored in
+     * this list.
+     *
+     * E.g., if we have that ROLE_ADMINISTRATION in LibrePlan matches with
+     * admins and editors roles in LDAP there will be 2 objects of
+     * {@link ConfigurationRolesLDAP} class: ROLE_ADMINISTRATION - admins and
+     * ROLE_ADMINISTRATION - editors
+     */
     private List<ConfigurationRolesLDAP> configurationRolesLdap = new ArrayList<ConfigurationRolesLDAP>();
 
     private Map<String, List<String>> mapMatchingRoles = new HashMap<String, List<String>>();
@@ -175,10 +186,8 @@ public class LDAPConfiguration extends BaseEntity {
         removeConfigurationRolesLdapForRoleLibreplan(roleLibreplan);
 
         for (String roleLdap : rolesLdap) {
-            ConfigurationRolesLDAP configuration = new ConfigurationRolesLDAP();
-            configuration.setRoleLibreplan(roleLibreplan);
-            configuration.setRoleLdap(roleLdap);
-            configurationRolesLdap.add(configuration);
+            configurationRolesLdap.add(new ConfigurationRolesLDAP(roleLdap,
+                    roleLibreplan));
         }
     }
 

commit 8071b656d938c3e9e43a0e954ebc1aaaccb7c955

Fixed issue adding constructor without parameters for Hibernate.
* Problem was introduced in commit
  40a87d837f2997a9088628a60f618de030e92c33

FEA: ItEr74S09LdapAuhentication

navalplanner-business/src/main/java/org/navalplanner/business/common/entities/ConfigurationRolesLDAP.java
@@ -37,6 +37,12 @@ public class ConfigurationRolesLDAP {
 
     private String roleLibreplan;
 
+    /**
+     * Default constructor for Hibernate. Do not use!
+     */
+    protected ConfigurationRolesLDAP() {
+    }
+
     public ConfigurationRolesLDAP(String roleLdap, String roleLibreplan) {
         this.roleLdap = roleLdap;
         this.roleLibreplan = roleLibreplan;