60f0d976efe3a08edfa2bcbe9983a318e419b98e
Óscar González Fernández <ogonzalez@igalia.com>
2011-09-20 19:18:52 +0200

    The cause must be thrown
    FEA: ItEr75S11PreventLooseChanges

--- navalplanner-webapp/src/main/java/org/navalplanner/web/common/concurrentdetection/ConcurrentModificationHandling.java
+++ navalplanner-webapp/src/main/java/org/navalplanner/web/common/concurrentdetection/ConcurrentModificationHandling.java
@@ -66,7 +66,7 @@ public class ConcurrentModificationHandling {
                         ConcurrentModificationController.showException(
                                 optimisticLockingFailureException, goToPage);
                     }
-                    throw e;
+                    throw cause;
                 }
             }
         };
