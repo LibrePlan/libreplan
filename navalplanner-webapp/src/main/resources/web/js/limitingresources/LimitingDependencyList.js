aaad249a72d7e87ab10d9d207c3226ed1e01bf7e
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2011-09-27 21:04:00 +0200

    Make new dependencies to be rendered in the correct position of the DOM.
    FEA: ItEr75S08MigrationZK5

--- navalplanner-webapp/src/main/resources/web/js/limitingresources/LimitingDependencyList.js
+++ navalplanner-webapp/src/main/resources/web/js/limitingresources/LimitingDependencyList.js
@@ -1,3 +1,12 @@
 zk.$package("limitingresources");
 
-limitingresources.LimitingDependencyList = zk.$extends(zk.Widget,{})
+limitingresources.LimitingDependencyList = zk.$extends(zk.Widget,{
+    appendChild: function(ao,al) {
+        this.$supers('appendChild', arguments);
+        //by default, appenChild appends the child (the .dependency object) as
+        //a direct child of the LimitingDependencylist object, but we want to
+        //to be added inside div#listlimitingdependencies, so we move the
+        //dependency once it's created
+        jq('#'+ao.uuid).appendTo('#listlimitingdependencies');
+    }
+})
