3db00dffffd0de8210b3701ff854cf9003c4347b
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2012-02-02 19:25:13 +0100

    Bug #1343: Show default filtering dates on company view.
    They are calculated by default as the start and end dates of the first and last
    open projects, but the user had no feedback on which were those dates.
    
    FEA: ItEr76S04BugFixing

--- libreplan-webapp/src/main/java/org/libreplan/web/planner/company/CompanyPlanningController.java
+++ libreplan-webapp/src/main/java/org/libreplan/web/planner/company/CompanyPlanningController.java
@@ -286,7 +286,13 @@ public class CompanyPlanningController implements Composer {
         Boolean includeOrderElements = checkIncludeOrderElements.isChecked();
 
         if (listFilters.isEmpty() && startDate == null && finishDate == null) {
-            return model.getDefaultPredicate(includeOrderElements);
+            IPredicate predicate = model.getDefaultPredicate(includeOrderElements);
+            //show filter dates calculated by default on screen
+            filterStartDate.setValue(model.getFilterStartDate().
+                    toDateMidnight().toDate());
+            filterFinishDate.setValue(model.getFilterFinishDate().
+                    toDateMidnight().toDate());
+            return predicate;
         }
         return new TaskGroupPredicate(listFilters, startDate, finishDate,
                 includeOrderElements);
