183977850ef801b93412b8ddd37461d7636c8cd2
Lorenzo Tilve Álvaro <ltilve@igalia.com>
2012-02-15 09:56:51 +0100

    Bug #1369: Improved behaviour of project creation popup when using templates
       * If a template is selected, the 'generate code' checkbox is automatically set and disabled
       * When selecting a project, start project date is calculated with the current day plus template start delay
       * Deadline is calculated as start project date plus template duration till deadline
    
    FEA: ItEr76S04BugFixing

--- libreplan-webapp/src/main/java/org/libreplan/web/common/components/bandboxsearch/BandboxSearch.java
+++ libreplan-webapp/src/main/java/org/libreplan/web/common/components/bandboxsearch/BandboxSearch.java
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2009-2010 Fundación para o Fomento da Calidade Industrial e
  *                         Desenvolvemento Tecnolóxico de Galicia
- * Copyright (C) 2010-2011 Igalia, S.L.
+ * Copyright (C) 2010-2012 Igalia, S.L.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -293,6 +293,10 @@ public class BandboxSearch extends HtmlMacroComponent {
         listbox.addEventListener(event, listener);
     }
 
+
+    public void setBandboxEventListener(String event, EventListener listener) {
+        bandbox.addEventListener(event, listener);
+    }
 
     public void foucusOnInput() {
         bandbox.setFocus(true);
