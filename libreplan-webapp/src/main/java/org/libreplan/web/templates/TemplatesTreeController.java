e2df00309a35efa94a6a10d891f702e8300927e3
Manuel Rego Casasnovas <rego@igalia.com>
2012-04-04 16:51:15 +0200

    Bug #1406: Add validation in the UI and also a try catch for possible ValidationExceptions
    FEA: ItEr76S04BugFixing

--- libreplan-webapp/src/main/java/org/libreplan/web/templates/TemplatesTreeController.java
+++ libreplan-webapp/src/main/java/org/libreplan/web/templates/TemplatesTreeController.java
@@ -127,6 +127,8 @@ public class TemplatesTreeController extends
                     element.setCode(value);
                 }
             });
+            textBoxCode.setConstraint("no empty:"
+                    + _("cannot be null or empty"));
             addCell(textBoxCode);
         }
 
