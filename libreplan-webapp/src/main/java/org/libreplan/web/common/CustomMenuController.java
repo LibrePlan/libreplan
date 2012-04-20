a31fecf2b0cf041dca50cbc9c2c13a9d7367a211
Susana Montes Pedreira <smontes@wirelessgalicia.com>
2012-04-20 18:39:44 +0100

    change the name of the entry Work Reports to be Time Tracking.
    FEA: ItEr76S22ExpenseTrackingSystem

--- libreplan-webapp/src/main/java/org/libreplan/web/common/CustomMenuController.java
+++ libreplan-webapp/src/main/java/org/libreplan/web/common/CustomMenuController.java
@@ -297,7 +297,8 @@ public class CustomMenuController extends Div implements IMenuItemsRegister {
         resourcesItems.add(subItem(_("Workers"), "/resources/worker/worker.zul","05-recursos.html#xesti-n-de-traballadores"));
         resourcesItems.add(subItem(_("Machines"), "/resources/machine/machines.zul","05-recursos.html#xesti-n-de-m-quinas"));
         resourcesItems.add(subItem(_("Virtual Workers Groups"),"/resources/worker/virtualWorkers.zul","05-recursos.html#xesti-n-de-traballadores"));
-        resourcesItems.add(subItem(_("Work Reports"), "/workreports/workReport.zul", "09-partes.html#id3"));
+        resourcesItems.add(subItem(_("Time Tracking"), "/workreports/workReport.zul",
+                "09-partes.html#id3"));
         if ((SecurityUtils.isUserInRole(UserRole.ROLE_ADMINISTRATION))
                 || (SecurityUtils.isUserInRole(UserRole.ROLE_EXPENSE_TRACKING))) {
             resourcesItems
