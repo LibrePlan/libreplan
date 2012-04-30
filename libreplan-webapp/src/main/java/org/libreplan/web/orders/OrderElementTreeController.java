a27c764a32ff0e55d0806bd33ded3b2f0de25759
Manuel Rego Casasnovas <rego@igalia.com>
2012-04-30 12:24:17 +0200

    Bug #1388: Fix issue updating name textbox when coming back from pop-up
    FEA: ItEr76S04BugFixing

--- libreplan-webapp/src/main/java/org/libreplan/web/orders/OrderElementTreeController.java
+++ libreplan-webapp/src/main/java/org/libreplan/web/orders/OrderElementTreeController.java
@@ -392,6 +392,7 @@ public class OrderElementTreeController extends TreeController<OrderElement> {
             IOrderElementModel model = orderModel
                     .getOrderElementModel(currentOrderElement);
             orderElementController.openWindow(model);
+            updateNameFor(currentOrderElement);
             updateHoursFor(currentOrderElement);
             updateBudgetFor(currentOrderElement);
         }
@@ -618,6 +619,7 @@ public class OrderElementTreeController extends TreeController<OrderElement> {
 
     public void refreshRow(Treeitem item) {
         try {
+            getRenderer().updateNameFor((OrderElement) item.getValue());
             getRenderer().updateHoursFor((OrderElement) item.getValue());
             getRenderer().updateBudgetFor((OrderElement) item.getValue());
             getRenderer().render(item, item.getValue());
@@ -736,6 +738,18 @@ public class OrderElementTreeController extends TreeController<OrderElement> {
                     OrderLine line = (OrderLine) element;
                     line.setBudget(budget);
                 }
+            }
+
+        };
+    }
+
+    @Override
+    protected INameHandler<OrderElement> getNameHandler() {
+        return new INameHandler<OrderElement>() {
+
+            @Override
+            public String getNameFor(OrderElement element) {
+                return element.getName();
             }
 
         };
