e1f91e49002d4173e6584977eefe0735ab6e532a
Manuel Rego Casasnovas <rego@igalia.com>
2012-04-30 18:22:22 +0200

    Bug #1263: Fix issue reseting the value of the textbox
    The problem is fixed both in forwards and backwards planning
    
    FEA: ItEr76S04BugFixing

--- ganttzk/src/main/java/org/zkoss/ganttz/LeftTasksTreeRow.java
+++ ganttzk/src/main/java/org/zkoss/ganttz/LeftTasksTreeRow.java
@@ -388,18 +388,19 @@ public class LeftTasksTreeRow extends GenericForwardComposer {
                     }
                 });
             } catch (ParseException e) {
-                getStartDateTextBox().setValue(
-                        dateFormat.format(task.getBeginDate()
-                                .toDayRoundedDate()));
+                // Do nothing as textbox is rested in the next sentence
             }
+            getStartDateTextBox().setValue(
+                    dateFormat.format(task.getBeginDate().toDayRoundedDate()));
         } else if (updatedComponent == getEndDateTextBox()) {
             try {
                 Date newEnd = dateFormat.parse(getEndDateTextBox().getValue());
                 task.resizeTo(LocalDate.fromDateFields(newEnd));
             } catch (ParseException e) {
-                getEndDateTextBox().setValue(
-                        asString(task.getEndDate().toDayRoundedDate()));
+                // Do nothing as textbox is rested in the next sentence
             }
+            getEndDateTextBox().setValue(
+                    asString(task.getEndDate().toDayRoundedDate()));
         }
         planner.updateTooltips();
     }
