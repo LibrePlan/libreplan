213ed7cda8ddffbddbf64423e3d01454c69aa8e4
Manuel Rego Casasnovas <rego@igalia.com>
2012-05-23 15:44:33 +0200

    demo: Create default admin for Czech language with "admin" as password
    Add default users in login.zul

--- libreplan-webapp/src/main/java/org/libreplan/web/users/bootstrap/MandatoryUser.java
+++ libreplan-webapp/src/main/java/org/libreplan/web/users/bootstrap/MandatoryUser.java
@@ -189,6 +189,20 @@ public enum MandatoryUser {
             return Language.POLISH_LANGUAGE;
         }
     },
+    ADMIN_CS(Arrays.asList(UserRole.ROLE_ADMINISTRATION,
+            UserRole.ROLE_READ_ALL_ORDERS, UserRole.ROLE_EDIT_ALL_ORDERS,
+            UserRole.ROLE_CREATE_ORDER), false) {
+
+        @Override
+        public boolean hasChangedDefaultPassword() {
+            return getConfiguration().getChangedDefaultAdminPassword();
+        }
+
+        @Override
+        public Language getApplicationLanguage() {
+            return Language.CZECH_LANGUAGE;
+        }
+    },
     WSREADER(Arrays.asList(UserRole.ROLE_WS_READER), Configuration
             .isExampleUsersDisabled()) {
         @Override
