4e943710f43a700192939151db9a74b9388c1c27
Manuel Rego Casasnovas <rego@igalia.com>
2012-06-14 19:12:54 +0200

    Create default example profiles
    FEA: ItEr76S30PermissionsEnhancements

---
+++ libreplan-business/src/main/java/org/libreplan/business/users/entities/ProfilesBootstrap.java
@@ -0,0 +1,51 @@
+/*
+ * This file is part of LibrePlan
+ *
+ * Copyright (C) 2012 Igalia, S.L.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.libreplan.business.users.entities;
+
+import org.libreplan.business.users.daos.IProfileDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Creates the default {@link org.libreplan.business.users.entities.Profile
+ * Profiles} and saves them into the database if there isn't any profile defined
+ * yet.
+ *
+ * @author Manuel Rego Casasnovas <rego@igalia.com>
+ */
+@Component
+@Scope("singleton")
+public class ProfilesBootstrap implements IProfileBootstrap {
+
+    @Autowired
+    private IProfileDAO profileDAO;
+
+    @Override
+    @Transactional
+    public void loadRequiredData() {
+        if (profileDAO.list(Profile.class).isEmpty()) {
+            for (PredefinedProfiles profile : PredefinedProfiles.values()) {
+                profileDAO.save(profile.createProfile());
+            }
+        }
+    }
+
+}
