LibrePlan/libreplan

commit db4e37c09f1b72870de3c2e5e9ba3dea12062cc0

Implemented BudgetModel as a subclass of BudgetTemplateModel to separate the operations only needed in the budget perspective.
FEA: AnA06S02CreacionNovoOrzamento

libreplan-webapp/src/main/java/org/libreplan/web/planner/budget/BudgetModel.java
@@ -1,0 +1,81 @@
+/*
+ * This file is part of LibrePlan
+ *
+ * Copyright (C) 2012 Igalia, S.L.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.libreplan.web.planner.budget;
+
+import org.libreplan.business.orders.daos.IOrderDAO;
+import org.libreplan.business.orders.entities.Order;
+import org.libreplan.business.orders.entities.OrderStatusEnum;
+import org.libreplan.business.templates.entities.Budget;
+import org.libreplan.web.planner.order.PlanningStateCreator;
+import org.libreplan.web.planner.order.PlanningStateCreator.IActionsOnRetrieval;
+import org.libreplan.web.planner.order.PlanningStateCreator.PlanningState;
+import org.libreplan.web.templates.budgettemplates.BudgetTemplatesModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.zkoss.zk.ui.Desktop;
+
+/**
+ * @author Jacobo Aragunde Pérez <jaragunde@igalia.com>
+ */
+@Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class BudgetModel extends BudgetTemplatesModel implements IBudgetModel {
+
+    @Autowired
+    private IOrderDAO orderDAO;
+
+    @Autowired
+    private PlanningStateCreator planningStateCreator;
+
+    private PlanningState planningState;
+
+    @Override
+    @Transactional
+    public void saveThroughPlanningState(Desktop desktop,
+            boolean showSaveMessage) {
+        this.planningState = planningStateCreator.retrieveOrCreate(desktop,
+                ((Budget) getTemplate()).getAssociatedOrder(),
+                new IActionsOnRetrieval() {
+
+                    @Override
+                    public void onRetrieval(PlanningState planningState) {
+                        planningState.reattach();
+                    }
+                });
+        if (showSaveMessage) {
+            this.planningState.getSaveCommand().save(null);
+        } else {
+            this.planningState.getSaveCommand().save(null, null);
+        }
+    }
+
+    @Override
+    @Transactional
+    public void closeBudget() {
+        Budget budget = (Budget) getTemplate();
+        Order order = budget.getAssociatedOrder();
+        orderDAO.reattach(order);
+        order.setState(OrderStatusEnum.OFFERED);
+        budget.createOrderLineElementsForAssociatedOrder();
+    }
+}

libreplan-webapp/src/main/java/org/libreplan/web/templates/budgettemplates/BudgetTemplatesModel.java
@@ -40,7 +40,6 @@ import org.libreplan.business.orders.daos.IOrderElementDAO;
 import org.libreplan.business.orders.entities.HoursGroup;
 import org.libreplan.business.orders.entities.Order;
 import org.libreplan.business.orders.entities.OrderElement;
-import org.libreplan.business.orders.entities.OrderStatusEnum;
 import org.libreplan.business.qualityforms.daos.IQualityFormDAO;
 import org.libreplan.business.qualityforms.entities.QualityForm;
 import org.libreplan.business.requirements.entities.DirectCriterionRequirement;
@@ -51,22 +50,17 @@ import org.libreplan.business.resources.entities.CriterionType;
 import org.libreplan.business.scenarios.IScenarioManager;
 import org.libreplan.business.scenarios.entities.Scenario;
 import org.libreplan.business.templates.daos.IOrderElementTemplateDAO;
-import org.libreplan.business.templates.entities.Budget;
 import org.libreplan.business.templates.entities.BudgetTemplate;
 import org.libreplan.business.templates.entities.OrderElementTemplate;
 import org.libreplan.web.common.concurrentdetection.OnConcurrentModification;
 import org.libreplan.web.orders.QualityFormsOnConversation;
 import org.libreplan.web.orders.labels.LabelsOnConversation;
-import org.libreplan.web.planner.order.PlanningStateCreator;
-import org.libreplan.web.planner.order.PlanningStateCreator.IActionsOnRetrieval;
-import org.libreplan.web.planner.order.PlanningStateCreator.PlanningState;
 import org.libreplan.web.templates.OrderElementsOnConversation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import org.zkoss.zk.ui.Desktop;
 
 /**
  * @author Óscar González Fernández <ogonzalez@igalia.com>
@@ -114,11 +108,6 @@ public class BudgetTemplatesModel implements IBudgetTemplatesModel {
     private TemplatesTree treeModel;
 
     private LabelsOnConversation labelsOnConversation;
-
-    @Autowired
-    private PlanningStateCreator planningStateCreator;
-
-    private PlanningState planningState;
 
     private LabelsOnConversation getLabelsOnConversation() {
         if (labelsOnConversation == null) {
@@ -396,36 +385,5 @@ public class BudgetTemplatesModel implements IBudgetTemplatesModel {
             }
         }
         return true;
-    }
-
-    @Override
-    @Transactional
-    public void saveThroughPlanningState(Desktop desktop,
-            boolean showSaveMessage) {
-        this.planningState = planningStateCreator.retrieveOrCreate(desktop,
-                ((Budget) getTemplate()).getAssociatedOrder(),
-                new IActionsOnRetrieval() {
-
-                    @Override
-                    public void onRetrieval(PlanningState planningState) {
-                        planningState.reattach();
-                    }
-                });
-        if (showSaveMessage) {
-            this.planningState.getSaveCommand().save(null);
-        } else {
-            this.planningState.getSaveCommand().save(null, null);
-        }
-    }
-
-    @Override
-    @Transactional
-    public void closeBudget() {
-        Budget budget = (Budget) getTemplate();
-        Order order = budget.getAssociatedOrder();
-        orderDAO.reattach(order);
-        order.setState(
-                OrderStatusEnum.OFFERED);
-        budget.createOrderLineElementsForAssociatedOrder();
     }
 }

libreplan-webapp/src/main/java/org/libreplan/web/planner/budget/BudgetController.java
@@ -27,7 +27,6 @@ import org.libreplan.web.common.Util;
 import org.libreplan.web.planner.order.ISaveCommand;
 import org.libreplan.web.planner.tabs.IGlobalViewEntryPoints;
 import org.libreplan.web.templates.budgettemplates.EditTemplateWindowController;
-import org.libreplan.web.templates.budgettemplates.IBudgetTemplatesModel;
 import org.libreplan.web.templates.budgettemplates.IEditionSubwindowController;
 import org.libreplan.web.templates.budgettemplates.TemplatesTreeController;
 import org.libreplan.web.templates.labels.LabelsAssignmentToTemplateComponent;
@@ -58,7 +57,7 @@ public class BudgetController extends GenericForwardComposer implements
         IEditionSubwindowController {
 
     @Autowired
-    private IBudgetTemplatesModel model;
+    private IBudgetModel model;
 
     private TreeComponent treeComponent;
 

libreplan-webapp/src/main/java/org/libreplan/web/planner/budget/IBudgetModel.java
@@ -16,18 +16,18 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.libreplan.web.templates.budgettemplates;
 
-import org.libreplan.business.templates.entities.OrderElementTemplate;
-import org.libreplan.web.templates.ITemplatesModel;
+package org.libreplan.web.planner.budget;
+
+import org.libreplan.web.templates.budgettemplates.IBudgetTemplatesModel;
+import org.zkoss.zk.ui.Desktop;
 
 /**
  * @author Jacobo Aragunde Pérez <jaragunde@igalia.com>
- *
  */
-public interface IBudgetTemplatesModel extends ITemplatesModel {
+public interface IBudgetModel extends IBudgetTemplatesModel {
 
-    void notifyUpdate(OrderElementTemplate element);
+    void saveThroughPlanningState(Desktop desktop, boolean showSaveMessage);
 
-    boolean checkValidCode(OrderElementTemplate element, String code);
+    void closeBudget();
 }

commit 809e8327400afd5c51bf7ab097d0c57d16fd4748

Fix bug introduced in commit db4e37c09f1b72870de3c2e5e9ba3dea12062cc0.
Two beans with type IBudgetTemplateModel cannot coexist because Spring is not
able to select one of them. In that commit we created two beans of that type:
BudgetTemplateModel and BudgetModel.

FEA: AnA06S02CreacionNovoOrzamento

libreplan-webapp/src/main/java/org/libreplan/web/planner/budget/BudgetModel.java
@@ -1,100 +1,0 @@
-/*
- * This file is part of LibrePlan
- *
- * Copyright (C) 2012 Igalia, S.L.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-package org.libreplan.web.planner.budget;
-
-import static org.libreplan.web.I18nHelper._;
-
-import org.libreplan.business.common.exceptions.ValidationException;
-import org.libreplan.business.orders.daos.IOrderDAO;
-import org.libreplan.business.orders.entities.Order;
-import org.libreplan.business.orders.entities.OrderStatusEnum;
-import org.libreplan.business.templates.entities.Budget;
-import org.libreplan.web.planner.order.PlanningStateCreator;
-import org.libreplan.web.planner.order.PlanningStateCreator.IActionsOnRetrieval;
-import org.libreplan.web.planner.order.PlanningStateCreator.PlanningState;
-import org.libreplan.web.templates.budgettemplates.BudgetTemplatesModel;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import org.zkoss.zk.ui.Desktop;
-
-/**
- * @author Jacobo Aragunde Pérez <jaragunde@igalia.com>
- */
-@Component
-@Scope(BeanDefinition.SCOPE_PROTOTYPE)
-public class BudgetModel extends BudgetTemplatesModel implements IBudgetModel {
-
-    @Autowired
-    private IOrderDAO orderDAO;
-
-    @Autowired
-    private PlanningStateCreator planningStateCreator;
-
-    private PlanningState planningState;
-
-    @Override
-    @Transactional
-    public void saveThroughPlanningState(Desktop desktop,
-            boolean showSaveMessage) {
-        if (!getAssociatedOrder().getState().equals(OrderStatusEnum.BUDGET)) {
-            throw new ValidationException(
-                    _("The project budget cannot be modified once it has been closed"));
-        }
-        this.planningState = planningStateCreator.retrieveOrCreate(desktop,
-                getAssociatedOrder(), new IActionsOnRetrieval() {
-
-                    @Override
-                    public void onRetrieval(PlanningState planningState) {
-                        planningState.reattach();
-                    }
-                });
-        if (showSaveMessage) {
-            this.planningState.getSaveCommand().save(null);
-        } else {
-            this.planningState.getSaveCommand().save(null, null);
-        }
-    }
-
-    @Override
-    @Transactional
-    public void closeBudget() {
-        Budget budget = (Budget) getTemplate();
-        Order order = budget.getAssociatedOrder();
-        if (!order.getState().equals(OrderStatusEnum.BUDGET)) {
-            throw new ValidationException(_("The budget is already closed"));
-        }
-        orderDAO.reattach(order);
-        order.setState(OrderStatusEnum.OFFERED);
-        budget.createOrderLineElementsForAssociatedOrder();
-    }
-
-    @Override
-    public Order getAssociatedOrder() {
-        return ((Budget) getTemplate()).getAssociatedOrder();
-    }
-
-    @Override
-    public boolean isReadOnly() {
-        return !getAssociatedOrder().getState().equals(OrderStatusEnum.BUDGET);
-    }
-}

libreplan-webapp/src/main/java/org/libreplan/web/templates/budgettemplates/BudgetTemplatesModel.java
@@ -33,6 +33,7 @@ import org.libreplan.business.advance.entities.AdvanceAssignmentTemplate;
 import org.libreplan.business.common.IAdHocTransactionService;
 import org.libreplan.business.common.IOnTransaction;
 import org.libreplan.business.common.exceptions.InstanceNotFoundException;
+import org.libreplan.business.common.exceptions.ValidationException;
 import org.libreplan.business.labels.daos.ILabelDAO;
 import org.libreplan.business.labels.entities.Label;
 import org.libreplan.business.orders.daos.IOrderDAO;
@@ -40,6 +41,7 @@ import org.libreplan.business.orders.daos.IOrderElementDAO;
 import org.libreplan.business.orders.entities.HoursGroup;
 import org.libreplan.business.orders.entities.Order;
 import org.libreplan.business.orders.entities.OrderElement;
+import org.libreplan.business.orders.entities.OrderStatusEnum;
 import org.libreplan.business.qualityforms.daos.IQualityFormDAO;
 import org.libreplan.business.qualityforms.entities.QualityForm;
 import org.libreplan.business.requirements.entities.DirectCriterionRequirement;
@@ -50,17 +52,23 @@ import org.libreplan.business.resources.entities.CriterionType;
 import org.libreplan.business.scenarios.IScenarioManager;
 import org.libreplan.business.scenarios.entities.Scenario;
 import org.libreplan.business.templates.daos.IOrderElementTemplateDAO;
+import org.libreplan.business.templates.entities.Budget;
 import org.libreplan.business.templates.entities.BudgetTemplate;
 import org.libreplan.business.templates.entities.OrderElementTemplate;
 import org.libreplan.web.common.concurrentdetection.OnConcurrentModification;
 import org.libreplan.web.orders.QualityFormsOnConversation;
 import org.libreplan.web.orders.labels.LabelsOnConversation;
+import org.libreplan.web.planner.budget.IBudgetModel;
+import org.libreplan.web.planner.order.PlanningStateCreator;
+import org.libreplan.web.planner.order.PlanningStateCreator.IActionsOnRetrieval;
+import org.libreplan.web.planner.order.PlanningStateCreator.PlanningState;
 import org.libreplan.web.templates.OrderElementsOnConversation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.zkoss.zk.ui.Desktop;
 
 /**
  * @author Óscar González Fernández <ogonzalez@igalia.com>
@@ -70,7 +78,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @OnConcurrentModification(goToPage = "/budgettemplates/templates.zul")
-public class BudgetTemplatesModel implements IBudgetTemplatesModel {
+public class BudgetTemplatesModel implements IBudgetTemplatesModel,
+        IBudgetModel {
 
     private static final Map<CriterionType, List<Criterion>> mapCriterions = new HashMap<CriterionType, List<Criterion>>();
 
@@ -108,6 +117,11 @@ public class BudgetTemplatesModel implements IBudgetTemplatesModel {
     private TemplatesTree treeModel;
 
     private LabelsOnConversation labelsOnConversation;
+
+    @Autowired
+    private PlanningStateCreator planningStateCreator;
+
+    private PlanningState planningState;
 
     private LabelsOnConversation getLabelsOnConversation() {
         if (labelsOnConversation == null) {
@@ -385,5 +399,51 @@ public class BudgetTemplatesModel implements IBudgetTemplatesModel {
             }
         }
         return true;
+    }
+
+    @Override
+    @Transactional
+    public void saveThroughPlanningState(Desktop desktop,
+            boolean showSaveMessage) {
+        if (!getAssociatedOrder().getState().equals(OrderStatusEnum.BUDGET)) {
+            throw new ValidationException(
+                    _("The project budget cannot be modified once it has been closed"));
+        }
+        this.planningState = planningStateCreator.retrieveOrCreate(desktop,
+                getAssociatedOrder(), new IActionsOnRetrieval() {
+
+                    @Override
+                    public void onRetrieval(PlanningState planningState) {
+                        planningState.reattach();
+                    }
+                });
+        if (showSaveMessage) {
+            this.planningState.getSaveCommand().save(null);
+        } else {
+            this.planningState.getSaveCommand().save(null, null);
+        }
+    }
+
+    @Override
+    @Transactional
+    public void closeBudget() {
+        Budget budget = (Budget) getTemplate();
+        Order order = budget.getAssociatedOrder();
+        if (!order.getState().equals(OrderStatusEnum.BUDGET)) {
+            throw new ValidationException(_("The budget is already closed"));
+        }
+        orderDAO.reattach(order);
+        order.setState(OrderStatusEnum.OFFERED);
+        budget.createOrderLineElementsForAssociatedOrder();
+    }
+
+    @Override
+    public Order getAssociatedOrder() {
+        return ((Budget) getTemplate()).getAssociatedOrder();
+    }
+
+    @Override
+    public boolean isReadOnly() {
+        return !getAssociatedOrder().getState().equals(OrderStatusEnum.BUDGET);
     }
 }