LibrePlan/libreplan

commit 896096272c2b3ee5ccf229726b42cb4f88dd8bd1

Remove unneeded code in LimitingResourcesTabCreator
Allowing create tabs only handling one ModeType, like the case of limiting
resources tab that is only used in global view.

FEA: ItEr76S04BugFixing

libreplan-webapp/src/main/java/org/libreplan/web/planner/tabs/TabOnModeType.java
@@ -20,7 +20,6 @@
  */
 package org.libreplan.web.planner.tabs;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.EnumMap;
 
@@ -67,9 +66,8 @@ public class TabOnModeType implements ITab {
 
     public TabOnModeType(Mode mode, EnumMap<ModeType, ITab> tabs) {
         Validate.notNull(mode);
-        Validate.isTrue(handleAllCases(tabs), "must handle all ModeTypes: "
-                + Arrays.toString(ModeType.values()) + ". It only handles: "
-                + tabs.keySet());
+        Validate.isTrue(handleAtLeatOneCase(tabs),
+                "must handle at least one ModeType");
         this.mode = mode;
         this.tabs = new EnumMap<ModeType, ITab>(tabs);
         this.mode.addListener(new ModeTypeChangedListener() {
@@ -91,13 +89,13 @@ public class TabOnModeType implements ITab {
         newTab.show();
     }
 
-    private boolean handleAllCases(EnumMap<ModeType, ITab> tabs) {
+    private boolean handleAtLeatOneCase(EnumMap<ModeType, ITab> tabs) {
         for (ModeType modeType : ModeType.values()) {
-            if (tabs.get(modeType) == null) {
-                return false;
+            if (tabs.get(modeType) != null) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
     private ITab getCurrentTab() {
@@ -114,12 +112,14 @@ public class TabOnModeType implements ITab {
 
     @Override
     public String getName() {
-        return getCurrentTab().getName();
+        ITab currentTab = getCurrentTab();
+        return currentTab == null ? "" : currentTab.getName();
     }
 
     @Override
     public String getCssClass() {
-        return getCurrentTab().getCssClass();
+        ITab currentTab = getCurrentTab();
+        return currentTab == null ? "hidden" : currentTab.getCssClass();
     }
 
     @Override

libreplan-webapp/src/main/java/org/libreplan/web/planner/tabs/LimitingResourcesTabCreator.java
@@ -43,9 +43,6 @@ import org.zkoss.zul.Label;
  */
 public class LimitingResourcesTabCreator {
 
-    /* Unnecesary */
-    private String ORDER_LIMITING_RESOURCES_VIEW = _("Queue-based resources (project)");
-
     public static ITab create(Mode mode,
             LimitingResourcesController LimitingResourcesController,
             LimitingResourcesController LimitingResourcesControllerGlobal,
@@ -76,44 +73,8 @@ public class LimitingResourcesTabCreator {
 
     private ITab build() {
         return TabOnModeType.forMode(mode)
-.forType(ModeType.GLOBAL,
-                createGlobalLimitingResourcesTab()).forType(ModeType.ORDER,
-                createOrderLimitingResourcesTab())
-            .create();
-    }
-
-    private ITab createOrderLimitingResourcesTab() {
-        IComponentCreator componentCreator = new IComponentCreator() {
-
-            @Override
-            /* Should never be called */
-            public org.zkoss.zk.ui.Component create(
-                    org.zkoss.zk.ui.Component parent) {
-                Map<String, Object> arguments = new HashMap<String, Object>();
-                arguments.put("LimitingResourcesController",
-                        limitingResourcesController);
-                return Executions.createComponents(
-                        "/limitingresources/_limitingresources.zul", parent,
-                        arguments);
-            }
-
-        };
-        return new CreatedOnDemandTab(ORDER_LIMITING_RESOURCES_VIEW,
-                "order-limiting-resources",
-                componentCreator) {
-
-            @Override
-            protected void afterShowAction() {
-                breadcrumbs.getChildren().clear();
-                breadcrumbs.appendChild(new Image(BREADCRUMBS_SEPARATOR));
-                breadcrumbs.appendChild(new Label(getSchedulingLabel()));
-                breadcrumbs.appendChild(new Image(BREADCRUMBS_SEPARATOR));
-                breadcrumbs
-                        .appendChild(new Label(ORDER_LIMITING_RESOURCES_VIEW));
-                breadcrumbs.appendChild(new Image(BREADCRUMBS_SEPARATOR));
-                breadcrumbs.appendChild(new Label(mode.getOrder().getName()));
-            }
-        };
+                .forType(ModeType.GLOBAL, createGlobalLimitingResourcesTab())
+                .create();
     }
 
     private ITab createGlobalLimitingResourcesTab() {

libreplan-webapp/src/main/webapp/common/css/libreplan_zk.css
@@ -1049,7 +1049,7 @@ span.perspective, span.perspective-active {
 .perspective-active.limiting-resources .z-button-cm {
     background-image: url(../img/ico_limiting-resources.png);
 }
-.perspective.order-limiting-resources {
+.perspective.hidden {
     display: none;
 }
 .perspective.order-data .z-button-cm,

commit c132309a89f47143921c5d1906c082d72862c469

Fix problem introduced in 896096272c2b3ee5ccf229726b42cb4f88dd8bd1
FEA: ItEr76S04BugFixing

libreplan-webapp/src/main/java/org/libreplan/web/planner/tabs/TabOnModeType.java
@@ -125,13 +125,19 @@ public class TabOnModeType implements ITab {
     @Override
     public void hide() {
         beingShown = false;
-        getCurrentTab().hide();
+        ITab currentTab = getCurrentTab();
+        if (currentTab != null) {
+            currentTab.hide();
+        }
     }
 
     @Override
     public void show() {
         beingShown = true;
-        getCurrentTab().show();
+        ITab currentTab = getCurrentTab();
+        if (currentTab != null) {
+            currentTab.show();
+        }
     }
 
 }