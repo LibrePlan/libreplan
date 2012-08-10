848a8b5c128dc5e480cd62e414d0282e49c27105
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2012-08-10 10:42:02 +0200

    Bug #1528: Check if name is null before truncating it.
    FEA: ItEr76S04BugFixing

--- libreplan-business/src/main/java/org/libreplan/business/orders/entities/OrderElement.java
+++ libreplan-business/src/main/java/org/libreplan/business/orders/entities/OrderElement.java
@@ -524,7 +524,7 @@ public abstract class OrderElement extends IntegrationEntity implements
     }
 
     public void setName(String name) {
-        if (name.length() > 255) {
+        if (name != null && name.length() > 255) {
             name = name.substring(0, 255);
         }
         this.getInfoComponent().setName(name);
