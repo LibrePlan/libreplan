3a90a8f34a36a4dbc637510854ec3bab6e36f96a
Manuel Rego Casasnovas <rego@igalia.com>
2012-09-06 17:35:29 +0200

    Add basic controller and zul for budget report
    FEA: AnA03S02InformeOrzamento

---
+++ libreplan-webapp/src/main/java/org/libreplan/web/reports/BudgetReportController.java
@@ -0,0 +1,52 @@
+/*
+ * This file is part of LibrePlan
+ *
+ * Copyright (C) 2012 Igalia, S.L.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.libreplan.web.reports;
+
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+
+import org.zkoss.zk.ui.Component;
+
+/**
+ * Controller for UI operations of Budget report.
+ *
+ * @author Manuel Rego Casasnovas <rego@igalia.com>
+ */
+public class BudgetReportController extends LibrePlanReportController {
+
+    private static final String REPORT_NAME = "budgetReport";
+
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        comp.setAttribute("controller", this);
+    }
+
+    @Override
+    protected String getReportName() {
+        return REPORT_NAME;
+    }
+
+    @Override
+    protected JRDataSource getDataSource() {
+        return new JREmptyDataSource();
+    }
+
+}
