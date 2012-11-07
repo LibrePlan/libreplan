dfe39ab7cbc2360df137eba016cfaf32379755c4
Manuel Rego Casasnovas <rego@igalia.com>
2012-11-07 07:01:20 +0100

    Add missing class ErrorDTO used in commit 7306b124deafa60a701b236eb9c9176a40733ed4
    FEA: ItEr77S13AllowDeleteOrderElements

---
+++ libreplan-webapp/src/main/java/org/libreplan/ws/common/api/ErrorDTO.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of LibrePlan
+ *
+ * Copyright (C) 2012 Igalia, S.L.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.libreplan.ws.common.api;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * DTO for representing any basic error in the web services.
+ *
+ * @author Manuel Rego Casasnovas <rego@igalia.com>
+ */
+@XmlRootElement(name = "error")
+public class ErrorDTO {
+
+    @XmlAttribute
+    public String message;
+
+    public ErrorDTO() {
+    }
+
+    public ErrorDTO(String message) {
+        this.message = message;
+    }
+
+}
