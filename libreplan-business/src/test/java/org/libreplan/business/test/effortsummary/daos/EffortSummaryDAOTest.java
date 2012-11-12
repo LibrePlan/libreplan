c2700f10dad1764fa15c5169073b3e86140d16a6
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2012-11-12 19:17:49 +0100

    Implemented test for EffortSummaryDAO.saveOrUpdate.
    FEA: ItEr77S11AllocationDataPerformance

--- libreplan-business/src/test/java/org/libreplan/business/test/effortsummary/daos/EffortSummaryDAOTest.java
+++ libreplan-business/src/test/java/org/libreplan/business/test/effortsummary/daos/EffortSummaryDAOTest.java
@@ -20,24 +20,46 @@
 package org.libreplan.business.test.effortsummary.daos;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.libreplan.business.BusinessGlobalNames.BUSINESS_SPRING_CONFIG_FILE;
 import static org.libreplan.business.test.BusinessGlobalNames.BUSINESS_SPRING_CONFIG_TEST_FILE;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
+import org.hibernate.SessionFactory;
 import org.joda.time.LocalDate;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.libreplan.business.IDataBootstrap;
+import org.libreplan.business.common.daos.IConfigurationDAO;
+import org.libreplan.business.common.exceptions.ValidationException;
 import org.libreplan.business.effortsummary.daos.IEffortSummaryDAO;
 import org.libreplan.business.effortsummary.entities.EffortSummary;
+import org.libreplan.business.orders.daos.IOrderDAO;
+import org.libreplan.business.orders.entities.HoursGroup;
+import org.libreplan.business.orders.entities.Order;
+import org.libreplan.business.orders.entities.OrderLine;
+import org.libreplan.business.orders.entities.SchedulingDataForVersion;
+import org.libreplan.business.orders.entities.TaskSource;
+import org.libreplan.business.orders.entities.TaskSource.TaskSourceSynchronization;
+import org.libreplan.business.planner.daos.ITaskElementDAO;
+import org.libreplan.business.planner.daos.ITaskSourceDAO;
+import org.libreplan.business.planner.entities.Task;
 import org.libreplan.business.resources.daos.IResourceDAO;
 import org.libreplan.business.resources.entities.Resource;
 import org.libreplan.business.resources.entities.Worker;
+import org.libreplan.business.scenarios.IScenarioManager;
+import org.libreplan.business.scenarios.bootstrap.IScenariosBootstrap;
+import org.libreplan.business.scenarios.entities.OrderVersion;
+import org.libreplan.business.test.planner.daos.ResourceAllocationDAOTest;
+import org.libreplan.business.workingday.EffortDuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -55,12 +77,34 @@ public class EffortSummaryDAOTest {
     @Autowired
     private IResourceDAO resourceDAO;
 
+    @Autowired
+    private ITaskElementDAO taskElementDAO;
+
+    @Autowired
+    private ITaskSourceDAO taskSourceDAO;
+
+    @Autowired
+    private IOrderDAO orderDAO;
+
+    @Autowired
+    private IConfigurationDAO configurationDAO;
+
+    @Autowired
+    private IScenarioManager scenarioManager;
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
     @javax.annotation.Resource
     private IDataBootstrap configurationBootstrap;
+
+    @Autowired
+    private IScenariosBootstrap scenariosBootstrap;
 
     @Before
     public void loadRequiredaData() {
         configurationBootstrap.loadRequiredData();
+        scenariosBootstrap.loadRequiredData();
     }
 
     public static Worker generateValidWorker() {
@@ -73,6 +117,12 @@ public class EffortSummaryDAOTest {
 
     public EffortSummary generateValidEffortSummary(int numberOfItems,
             Resource resource, LocalDate startDate) {
+        return generateValidEffortSummaryForTask(numberOfItems, resource, null,
+                startDate);
+    }
+
+    public EffortSummary generateValidEffortSummaryForTask(int numberOfItems,
+            Resource resource, Task task, LocalDate startDate) {
         LocalDate startDate2 = new LocalDate(startDate);
         LocalDate endDate = startDate.plusDays(numberOfItems - 1);
         int[] availableEffort = new int[numberOfItems];
@@ -84,8 +134,52 @@ public class EffortSummaryDAOTest {
             assignedEffort[i] = generator.nextInt(86400);
         }
         EffortSummary summary = EffortSummary.create(startDate2, endDate,
-                availableEffort, assignedEffort, resource);
+                availableEffort, assignedEffort, resource, task);
         return summary;
+    }
+
+    private Task generateValidTask() {
+        HoursGroup associatedHoursGroup = new HoursGroup();
+        associatedHoursGroup.setCode("hours-group-code-" + UUID.randomUUID());
+        OrderLine orderLine = generateValidOrderLine();
+        orderLine.addHoursGroup(associatedHoursGroup);
+        OrderVersion orderVersion = ResourceAllocationDAOTest
+                .setupVersionUsing(scenarioManager, orderLine.getOrder());
+        orderLine.useSchedulingDataFor(orderVersion);
+        SchedulingDataForVersion schedulingDataForVersion = orderLine
+                .getCurrentSchedulingDataForVersion();
+        TaskSource taskSource = TaskSource.create(schedulingDataForVersion,
+                Arrays.asList(associatedHoursGroup));
+        TaskSourceSynchronization mustAdd = TaskSource.mustAdd(taskSource);
+        mustAdd.apply(TaskSource.persistTaskSources(taskSourceDAO));
+        Task task = (Task) taskSource.getTask();
+        return task;
+    }
+
+    private OrderLine generateValidOrderLine() {
+        OrderLine orderLine = OrderLine.create();
+        orderLine.setName("bla");
+        orderLine.setCode("code-" + UUID.randomUUID());
+        HoursGroup hoursGroup = new HoursGroup();
+        hoursGroup.setCode("hours-group-code-" + UUID.randomUUID());
+        orderLine.addHoursGroup(hoursGroup);
+        Order order = Order.create();
+        OrderVersion orderVersion = ResourceAllocationDAOTest
+                .setupVersionUsing(scenarioManager, order);
+        order.setName("bla-" + UUID.randomUUID());
+        order.setInitDate(new Date());
+        order.setCode("code-" + UUID.randomUUID());
+        order.useSchedulingDataFor(orderVersion);
+        order.add(orderLine);
+        order.setCalendar(configurationDAO.getConfiguration()
+                .getDefaultCalendar());
+        try {
+            orderDAO.save(order);
+            sessionFactory.getCurrentSession().flush();
+        } catch (ValidationException e) {
+            throw new RuntimeException(e);
+        }
+        return orderLine;
     }
 
     @Test
@@ -140,5 +234,66 @@ public class EffortSummaryDAOTest {
         resourceDAO.save(worker);
         EffortSummary effort = effortSummaryDAO.findGlobalInformationForResource(worker);
         assertNull(effort);
+    }
+
+    @Test
+    public void testSaveOrUpdate() {
+        LocalDate date = new LocalDate();
+        final int numberOfItems = 1000;
+
+        // create global effort
+        Worker worker = generateValidWorker();
+        resourceDAO.save(worker);
+        resourceDAO.flush();
+        worker.dontPoseAsTransientObjectAnymore();
+        EffortSummary global = generateValidEffortSummary(numberOfItems,
+                worker, date);
+        effortSummaryDAO.save(global);
+
+        // store original assigned effort for two days to compare later
+        EffortDuration effortDay1 = global.getAssignedEffortForDate(date);
+        EffortDuration effortDay5 = global.getAssignedEffortForDate(date
+                .plusDays(5));
+
+        // create task effort
+        Task task = generateValidTask();
+        taskElementDAO.save(task);
+        taskElementDAO.flush();
+        task.dontPoseAsTransientObjectAnymore();
+        EffortSummary taskEffort = generateValidEffortSummaryForTask(
+                numberOfItems, worker, task, date);
+        effortSummaryDAO.saveOrUpdate(Collections.singleton(taskEffort));
+
+        // store new assigned effort for the same days
+        EffortDuration taskEffortDay1 = taskEffort
+                .getAssignedEffortForDate(date);
+        EffortDuration taskEffortDay5 = taskEffort
+                .getAssignedEffortForDate(date.plusDays(5));
+
+        // retrieve updated global effort
+        EffortSummary globalEffort = effortSummaryDAO
+                .findGlobalInformationForResource(worker);
+        assertNotNull(globalEffort);
+
+        // check that the two efforts have been added
+        assertEquals(effortDay1.plus(taskEffortDay1),
+                globalEffort.getAssignedEffortForDate(date));
+        assertEquals(effortDay5.plus(taskEffortDay5),
+                globalEffort.getAssignedEffortForDate(date.plusDays(5)));
+
+        // replace taskEffort with an empty object
+        taskEffort = EffortSummary.create(date, date, new int[0], new int[0],
+                worker, task);
+        effortSummaryDAO.saveOrUpdate(Collections.singleton(taskEffort));
+
+        // retrieve updated global effort
+        globalEffort = effortSummaryDAO
+                .findGlobalInformationForResource(worker);
+
+        // check that the two efforts have been added and the old one was
+        // substracted
+        assertEquals(effortDay1, globalEffort.getAssignedEffortForDate(date));
+        assertEquals(effortDay5,
+                globalEffort.getAssignedEffortForDate(date.plusDays(5)));
     }
 }
