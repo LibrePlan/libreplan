8869325b60a536d92f02ae4cd9aa1a09b9374268
Miciele Ghiorghis <m.ghiorghis@antoniusziekenhuis.nl>
2013-01-16 17:15:15 +0100

    Tim-connector: JobSchedulerConfiguration entity

---
+++ libreplan-business/src/main/java/org/libreplan/business/common/entities/JobSchedulerConfiguration.java
@@ -0,0 +1,89 @@
+/*
+ * This file is part of LibrePlan
+ *
+ * Copyright (C) 2013 St. Antoniusziekenhuis
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.libreplan.business.common.entities;
+
+import org.libreplan.business.common.BaseEntity;
+
+/**
+ * JobSchedulerConfiguration entity
+ *
+ * @author Miciele Ghiorghis <m.ghiorghis@antoniusziekenhuis.nl>
+ */
+public class JobSchedulerConfiguration extends BaseEntity {
+
+    public static JobSchedulerConfiguration create() {
+        return create(new JobSchedulerConfiguration());
+    }
+
+    private String jobGroup;
+    private String jobName;
+    private String triggerGroup;
+    private String triggerName;
+    private String cronExpression;
+    private String jobClassName;
+
+    public String getJobGroup() {
+        return jobGroup;
+    }
+
+    public void setJobGroup(String jobGroup) {
+        this.jobGroup = jobGroup;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public String getTriggerGroup() {
+        return triggerGroup;
+    }
+
+    public void setTriggerGroup(String triggerGroup) {
+        this.triggerGroup = triggerGroup;
+    }
+
+    public String getTriggerName() {
+        return triggerName;
+    }
+
+    public void setTriggerName(String triggerName) {
+        this.triggerName = triggerName;
+    }
+
+    public String getCronExpression() {
+        return cronExpression;
+    }
+
+    public void setCronExpression(String cronExpression) {
+        this.cronExpression = cronExpression;
+    }
+
+    public String getJobClassName() {
+        return jobClassName;
+    }
+
+    public void setJobClassName(String jobClassName) {
+        this.jobClassName = jobClassName;
+    }
+}
