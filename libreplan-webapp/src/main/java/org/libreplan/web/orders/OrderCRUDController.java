5138f4046a159129bbbd638f0d9ef4ada9a2dda8
Manuel Rego Casasnovas <rego@igalia.com>
2013-02-08 13:21:51 +0100

    Revert "Apply filter when entering project details view"
    This reverts commit 800cf8b50d40680a894a6c689b51a6f66ab90bdd.
    
    This patch was breaking the possibility to select a task in the WBS.
    
    FEA: ItEr77S15FilteringEnhancements

--- libreplan-webapp/src/main/java/org/libreplan/web/orders/OrderCRUDController.java
+++ libreplan-webapp/src/main/java/org/libreplan/web/orders/OrderCRUDController.java
@@ -51,6 +51,7 @@ import org.libreplan.business.orders.entities.Order.SchedulingMode;
 import org.libreplan.business.orders.entities.OrderElement;
 import org.libreplan.business.orders.entities.OrderStatusEnum;
 import org.libreplan.business.planner.entities.PositionConstraintType;
+import org.libreplan.business.planner.entities.TaskElement;
 import org.libreplan.business.resources.entities.Criterion;
 import org.libreplan.business.templates.entities.OrderTemplate;
 import org.libreplan.business.users.entities.User;
@@ -67,6 +68,7 @@ import org.libreplan.web.common.components.bandboxsearch.BandboxMultipleSearch;
 import org.libreplan.web.common.components.bandboxsearch.BandboxSearch;
 import org.libreplan.web.common.components.finders.FilterPair;
 import org.libreplan.web.common.components.finders.OrderFilterEnum;
+import org.libreplan.web.common.components.finders.TaskElementFilterEnum;
 import org.libreplan.web.common.components.finders.TaskGroupFilterEnum;
 import org.libreplan.web.orders.criterionrequirements.AssignedCriterionRequirementToOrderElementController;
 import org.libreplan.web.orders.labels.AssignedLabelsToOrderElementController;
@@ -535,7 +537,7 @@ public class OrderCRUDController extends GenericForwardComposer {
             tree.setModel(null);
             tree.setTreeitemRenderer(orderElementTreeController.getRenderer());
 
-            orderElementTreeController.onApplyFilter();
+            reloadTree(orderElementsTree);
         }
     }
 
